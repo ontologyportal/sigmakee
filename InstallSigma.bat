b005bdba256d3b2b2f665da8f1a5c5f7df8bbca9
abu_banaat <abu_banaat>
2007-09-24 15:02:56 +0000

    Update the Windows and Linux install scripts to use SIGMA_HOME and CATALINA_HOME.

--- InstallSigma.bat
+++ InstallSigma.bat
@@ -4,19 +4,23 @@ echo.
 echo Installing Sigma files ...
 echo.
 
-set EXPECTED_TOMCAT_VERSION=5
 set EXPECTED_TOMCAT_ROOT=C:\Program Files\Apache Software Foundation\
 set TOMCAT_ROOT=
-set TOMCAT_KBs_DIR=
 set TOMCAT_WEBAPPs_DIR=
+set SIGMA_ROOT=
+set SIGMA_KBS_DIR=
 
-for /D %%R in ( "%EXPECTED_TOMCAT_ROOT%Tomcat*%EXPECTED_TOMCAT_VERSION%*" ) do (
-  set TOMCAT_ROOT=%%R
+if exist "%CATALINA_HOME%" (
+  set TOMCAT_ROOT=%CATALINA_HOME%
+) else (
+  for /D %%R in ( "%EXPECTED_TOMCAT_ROOT%Tomcat*" ) do (
+    set TOMCAT_ROOT=%%R
+  )
 )
 
 if not exist "%TOMCAT_ROOT%" (
   echo.
-  echo Tomcat is not in %EXPECTED_TOMCAT_ROOT%.
+  echo Tomcat is not in %TOMCAT_ROOT%.
   echo.
   echo Please install Tomcat or edit %0, and then try again.
   pause
@@ -32,13 +36,31 @@ if not exist "%TOMCAT_WEBAPPs_DIR%" (
   goto quit
 )
 
-set TOMCAT_KBs_DIR=%TOMCAT_ROOT%\KBs
+if not "%SIGMA_HOME%"=="" (
+  set SIGMA_ROOT=%SIGMA_HOME%
+) else (
+  set SIGMA_ROOT=%TOMCAT_ROOT%
+)
 
-if not exist "%TOMCAT_KBs_DIR%" (
-  mkdir "%TOMCAT_KBs_DIR%"
-  if not exist "%TOMCAT_KBs_DIR%" (
+if not exist "%SIGMA_ROOT%" (
+  mkdir "%SIGMA_ROOT%"
+  if not exist "%SIGMA_ROOT%" (
     echo.
-    echo The directory %TOMCAT_KBs_DIR% could not be created.
+    echo The directory %SIGMA_ROOT% could not be created.
+    echo.
+    echo Perhaps you do not have permission to create it.
+    pause
+    goto quit
+  )
+)
+
+set SIGMA_KBS_DIR=%SIGMA_ROOT%\KBs
+
+if not exist "%SIGMA_KBS_DIR%" (
+  mkdir "%SIGMA_KBS_DIR%"
+  if not exist "%SIGMA_KBS_DIR%" (
+    echo.
+    echo The directory %SIGMA_KBS_DIR% could not be created.
     echo.
     echo Perhaps you do not have permission to create it.
     pause
@@ -73,15 +95,15 @@ if exist "%TOMCAT_WEBAPPs_DIR%\sigma" (
 )
 
 if exist "%TOMCAT_WEBAPPs_DIR%\sigma.war" (
-  copy "%TOMCAT_WEBAPPs_DIR%\sigma.war" "%TOMCAT_WEBAPPs_DIR%\sigma.war.old"
+  copy /B "%TOMCAT_WEBAPPs_DIR%\sigma.war" "%TOMCAT_WEBAPPs_DIR%\sigma.war.old"
   if exist "%TOMCAT_WEBAPPs_DIR%\sigma.war.old" (
-    echo Created sigma.war.old
+    echo Wrote sigma.war.old.
     del "%TOMCAT_WEBAPPs_DIR%\sigma.war"
   )
 )
 
 for %%X in ( "sigma*.war" ) do (
-  copy "%%X" "%TOMCAT_WEBAPPs_DIR%\sigma.war"
+  copy /B "%%X" "%TOMCAT_WEBAPPs_DIR%\sigma.war"
   if not exist "%TOMCAT_WEBAPPs_DIR%\sigma.war" (
     echo.
     echo Could not copy %%X.
@@ -92,13 +114,13 @@ for %%X in ( "sigma*.war" ) do (
 )
 
 for %%I in ( "KBs\*.kif" "KBs\*.txt" "KBs\*.exc" "KBs\index.*" ) do (
-  if exist "%TOMCAT_KBs_DIR%\%%~nxI" (
-    copy "%TOMCAT_KBs_DIR%\%%~nxI" "%TOMCAT_KBs_DIR%\%%~nxI.old"
-    echo Created %%~nxI.old
-    del "%TOMCAT_KBs_DIR%\%%~nxI"
+  if exist "%SIGMA_KBS_DIR%\%%~nxI" (
+    copy /B "%SIGMA_KBS_DIR%\%%~nxI" "%SIGMA_KBS_DIR%\%%~nxI.old"
+    echo Wrote %%~nxI.old
+    del "%SIGMA_KBS_DIR%\%%~nxI"
   )
-  copy "%%I" "%TOMCAT_KBs_DIR%\%%~nxI"
-  if not exist "%TOMCAT_KBs_DIR%\%%~nxI" (
+  copy /B "%%I" "%SIGMA_KBS_DIR%\%%~nxI"
+  if not exist "%SIGMA_KBS_DIR%\%%~nxI" (
     echo.
     echo %%~nxI could not be copied.
     pause
@@ -107,23 +129,48 @@ for %%I in ( "KBs\*.kif" "KBs\*.txt" "KBs\*.exc" "KBs\index.*" ) do (
   echo Wrote %%~nxI.
 )
 
-set CFGFILE=%TOMCAT_KBs_DIR%\config.xml
+if not exist "%SIGMA_KBS_DIR%\tests" (
+  mkdir "%SIGMA_KBS_DIR%\tests"
+)
+
+if not exist "%SIGMA_KBS_DIR%\tests" (
+  echo.
+  echo The directory %SIGMA_KBS_DIR%\tests could not be created.
+  echo.
+  echo The inference test files will not be copied.
+) else (
+  for %%I in ( "KBs\tests\*" ) do (
+    if exist "%SIGMA_KBS_DIR%\tests\%%~nxI" (
+      copy /B "%SIGMA_KBS_DIR%\tests\%%~nxI" "%SIGMA_KBS_DIR%\tests\%%~nxI.old"
+      echo Wrote %%~nxI.old
+      del "%SIGMA_KBS_DIR%\tests\%%~nxI"
+    )
+    copy /B "%%I" "%SIGMA_KBS_DIR%\tests\%%~nxI"
+    if not exist "%SIGMA_KBS_DIR%\tests\%%~nxI" (
+      echo.
+      echo %%~nxI could not be copied.
+    )
+    echo Wrote %%~nxI.
+  )
+)
+
+set CFGFILE=%SIGMA_KBS_DIR%\config.xml
 if exist "%CFGFILE%" (
-  copy "%CFGFILE%" "%CFGFILE%.old"
-  echo Created config.xml.old
+  copy /B "%CFGFILE%" "%CFGFILE%.old"
+  echo Wrote config.xml.old
 )
 
 for %%X in ( "inference\*.exe" ) do (
   if exist "%%~fX" (
     echo ^<configuration^> > "%CFGFILE%"
-    echo    ^<preference value^="%TOMCAT_ROOT%" name^="baseDir" ^/^> >> "%CFGFILE%"
-    echo    ^<preference value^="%TOMCAT_KBs_DIR%" name^="kbDir" ^/^> >> "%CFGFILE%"
+    echo    ^<preference value^="%SIGMA_ROOT%" name^="baseDir" ^/^> >> "%CFGFILE%"
+    echo    ^<preference value^="%SIGMA_KBS_DIR%" name^="kbDir" ^/^> >> "%CFGFILE%"
     echo    ^<preference value^="%%~fX" name="inferenceEngine" ^/^> >> "%CFGFILE%"
-    echo    ^<preference value^="%TOMCAT_KBs_DIR%\tests" name^="inferenceTestDir" ^/^> >> "%CFGFILE%"
-    echo    ^<preference value^="%TOMCAT_KBs_DIR%\tests" name^="testResultsDir" ^/^> >> "%CFGFILE%"
+    echo    ^<preference value^="%SIGMA_KBS_DIR%\tests" name^="inferenceTestDir" ^/^> >> "%CFGFILE%"
+    echo    ^<preference value^="%TOMCAT_WEBAPPS_DIR%\sigma\tests" name^="testOutputDir" ^/^> >> "%CFGFILE%"
     echo    ^<preference value^="yes" name^="typePrefix" ^/^> >> "%CFGFILE%"
     echo    ^<preference value^="no" name^="holdsPrefix" ^/^> >> "%CFGFILE%"
-    echo    ^<preference value^="no" name^="cache" ^/^> >> "%CFGFILE%"
+    echo    ^<preference value^="yes" name^="cache" ^/^> >> "%CFGFILE%"
     echo    ^<preference value^="yes" name^="TPTP" ^/^> >> "%CFGFILE%"
     echo    ^<preference value^="" name^="celtdir" ^/^> >> "%CFGFILE%"
     echo    ^<preference value^="no" name^="loadCELT" ^/^> >> "%CFGFILE%"
@@ -135,12 +182,12 @@ for %%X in ( "inference\*.exe" ) do (
     echo    ^<preference value^="SUMO" name^="sumokbname" ^/^> >> "%CFGFILE%"
     echo    ^<preference value^="" name^="prolog" ^/^> >> "%CFGFILE%"
     echo    ^<kb name^="SUMO"^> >> "%CFGFILE%"
-    echo      ^<constituent filename^="%TOMCAT_KBs_DIR%\Merge.kif" ^/^> >> "%CFGFILE%"
-    echo      ^<constituent filename^="%TOMCAT_KBs_DIR%\english_format.kif" ^/^> >> "%CFGFILE%"
+    echo      ^<constituent filename^="%SIGMA_KBS_DIR%\Merge.kif" ^/^> >> "%CFGFILE%"
+    echo      ^<constituent filename^="%SIGMA_KBS_DIR%\english_format.kif" ^/^> >> "%CFGFILE%"
     echo    ^<^/kb^> >> "%CFGFILE%"
     echo ^<^/configuration^> >> "%CFGFILE%"
   ) else (
-    copy "KBs\config.xml" "%TOMCAT_KBs_DIR%\config.xml"
+    copy /B "KBs\config.xml" "%SIGMA_KBS_DIR%\config.xml"
   )
 )
 
