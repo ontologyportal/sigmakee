3ec0fe3b30cb917007c8f2e61d75e53b6819cb8c
steventrac <steventrac>
2008-03-14 05:19:17 +0000

    tptp_parser files

---
+++ src/java/tptp_parser/TptpTokenTypes.java
@@ -0,0 +1,46 @@
+// $ANTLR 2.7.5 (20050128): "tptp.g" -> "TptpLexer.java"$
+
+package tptp_parser;
+
+import java.util.List;              
+import java.util.LinkedList; 
+
+public interface TptpTokenTypes {
+	int EOF = 1;
+	int NULL_TREE_LOOKAHEAD = 3;
+	int INTEGER = 4;
+	int UNSIGNED_INTEGER = 5;
+	int REAL = 6;
+	int LOWER_WORD = 7;
+	int SINGLE_QUOTED = 8;
+	int DOLLAR_WORD = 9;
+	int DOLLAR_DOLLAR_WORD = 10;
+	int LPR = 11;
+	int RPR = 12;
+	int COMMA = 13;
+	int DISTINCT_OBJECT = 14;
+	int UPPER_WORD = 15;
+	int EQUAL = 16;
+	int NOTEQUAL = 17;
+	int AND = 18;
+	int VLINE = 19;
+	int EQUIVALENCE = 20;
+	int IMPLICATION = 21;
+	int REVERSE_IMPLICATION = 22;
+	int DISEQUIVALENCE = 23;
+	int NOT_OR = 24;
+	int NOT_AND = 25;
+	int TILDA = 26;
+	int LSB = 27;
+	int RSB = 28;
+	int COLON = 29;
+	int ALL = 30;
+	int EXIST = 31;
+	int PLUSPLUS = 32;
+	int MINUSMINUS = 33;
+	int DOT = 34;
+	int ONE_LINE_COMMENT = 35;
+	int MANY_LINE_COMMENT = 36;
+	int STAR = 37;
+	int WHITESPACE = 38;
+}
