b5caf4f7f0ba07dc056622d55d44022ca0afbd9d
abu_banaat <abu_banaat>
2009-04-28 14:50:19 +0000

    Added several more static utility methods for Strings.

--- src/java/com/articulate/sigma/StringUtil.java
+++ src/java/com/articulate/sigma/StringUtil.java
@@ -15,12 +15,13 @@ package com.articulate.sigma;
 
 import java.util.*;
 import java.io.*;
+import java.text.SimpleDateFormat;
 
 
-  /** ***************************************************************
-  *  A utility class that defines static methods for common string
-  *  manipulation operations.
-  */
+/** ***************************************************************
+ *  A utility class that defines static methods for common string
+ *  manipulation operations.
+ */
 public class StringUtil {
 
     private StringUtil() {
@@ -46,16 +47,24 @@ public class StringUtil {
     }
 
     /** ***************************************************************
-     *  Remove the quotes in the first and last character of a
-     *  String, if present.
+     *  Removes all balanced ASCII double-quote characters from each
+     *  end of the String s, if any are present.
+     *
      */
     public static String removeEnclosingQuotes(String s) {
-        if (isNonEmptyString(s) 
-            && (s.charAt(0) == '"')
-            && (s.charAt(s.length()-1) == '"')) {
-            return s.substring(1,s.length()-1);
+        StringBuilder sb = new StringBuilder();
+        if (isNonEmptyString(s)) {
+            sb.append(s);
+            int lastI = (sb.length() - 1);
+            while ((lastI > 0)
+                   && (sb.charAt(0) == '"')
+                   && (sb.charAt(lastI) == '"')) {
+                sb.deleteCharAt(lastI);
+                sb.deleteCharAt(0);
+                lastI = (sb.length() - 1);
+            }
         }
-        return s;
+        return sb.toString();
     }
 
     /** ***************************************************************
@@ -67,9 +76,12 @@ public class StringUtil {
     public static String normalizeSpaceChars(String str) {
         String ans = str;
         if (isNonEmptyString(ans)) {
-            ans = ans.replaceAll("\\\\n", " ");
-            ans = ans.replaceAll("\\s\\s*", " ");
-            ans = ans.replaceAll("\\.\\s+", ".  ");
+            // ans = ans.replaceAll("(?s)\\s", " ");
+            ans = ans.replaceAll("\\s+", " ");
+            // ans = ans.replaceAll("\\.\\s+", ".  ");
+            // ans = ans.replaceAll("\\?\\s+", "?  ");
+            // ans = ans.replaceAll("\\:\\s+", ":  ");
+            // ans = ans.replaceAll("\\!\\s+", "!  ");
         }
         return ans;
     }
@@ -195,6 +207,220 @@ public class StringUtil {
             while (ans.matches(".+[^\\p{Alnum}]$")) {
                 ans = ans.substring(0, ans.length() - 1);
             }
+        }
+        return ans;
+    }
+
+    /** ***************************************************************
+     * Returns a date/time string corresponding to pattern.  The
+     * date/time returned is the date/time of the method call.  The
+     * locale is UTC (Greenwich).
+     *
+     * @param pattern Examples: yyyy, yyyyMMdd.
+     *
+     */
+    public static String getDateTime(String pattern) {
+
+        String dateTime = "";
+        try {
+            if (isNonEmptyString(pattern)) {
+                SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+                sdf.setTimeZone(new SimpleTimeZone(0, "Greenwich"));
+                dateTime = sdf.format(new Date());
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return dateTime;
+    }
+
+    /** ***************************************************************
+     * Returns true if input appears to be a URI string, else returns
+     * false.
+     *
+     * @param input A String
+     *
+     */
+    public static boolean isUri(String input) {
+
+        boolean ans = false;
+        try {
+            ans = (isNonEmptyString(input)
+                   && (input.matches("^.?http://.+")
+                       || input.matches("^.?file://.+")));
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return ans;
+    }
+
+    /** ***************************************************************
+     * Returns true if input is a String containing some whitespace
+     * chars, else returns false.
+     *
+     * @param input A String
+     *
+     * @return true or false
+     */
+    public static boolean isStringWithSpaces(String input) {
+
+        return (isNonEmptyString(input) && input.matches(".*\\s+.*"));
+    }
+
+    /** ***************************************************************
+     * Returns true if input appears to be a quoted String, else
+     * returns false.
+     *
+     * @param input A String
+     *
+     */
+    public static boolean isQuotedString(String input) {
+
+        boolean ans = false;
+        try {
+            if (isNonEmptyString(input)) {
+                int ilen = input.length();
+                if (ilen > 2) {
+                    char fc = input.charAt(0);
+                    char lc = input.charAt(ilen - 1);
+                    ans = (((fc == '"') && (lc == '"'))
+                           || (((fc == '\'') || (fc == '`'))
+                               && (lc == '\'')));
+                }
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return ans;
+    }
+
+    /** ***************************************************************
+     * Returns a new String formed by adding quoteChar to each end of
+     * input.
+     *
+     * @param input A String
+     *
+     */
+    public static String makeQuotedString(String input, char quoteChar) {
+        String ans = input;
+        try {
+            if (isNonEmptyString(input)
+                && !isQuotedString(input)
+                && (input.charAt(0) != quoteChar)) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(quoteChar);
+                sb.append(input);
+                sb.append(quoteChar);
+                ans = sb.toString();
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return ans;
+    }
+
+    /** ***************************************************************
+     * Returns true if every char in input is a digit char, else
+     * returns false.
+     *
+     * @param input A String
+     *
+     */
+    public static boolean isDigitString(String input) {
+        boolean ans = false;
+        try {
+            if (isNonEmptyString(input)) {
+                ans = true;
+                char ch = '0';
+                int ilen = input.length();
+                for (int i = 0; i < ilen; i++) {
+                    ans = Character.isDigit(input.charAt(i));
+                    if (!ans) break;
+                }
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return ans;
+    }
+
+    /** ***************************************************************
+     * Performs a depth-first search of tree, replacing all terms
+     * matching oldPattern with newTerm.
+     *
+     * @param oldPattern A regular expression pattern to be matched
+     * against terms in tree
+     *
+     * @param newTerm A String to replace terms matching oldPattern
+     *
+     * @param tree A String representing a SUO-KIF Formula (list)
+     * 
+     * @return A new tree (String), with all occurrences of terms
+     * matching oldPattern replaced by newTerm
+     */
+    public static String treeReplace(String oldPattern, String newTerm, String tree) {
+        String result = tree;
+        try {
+            StringBuilder sb = new StringBuilder();
+            String flist = tree;;
+            if (flist.matches(oldPattern))
+                sb.append(newTerm);
+            else if (Formula.listP(flist)) {
+                if (Formula.empty(flist)) {
+                    sb.append(flist);
+                }
+                else {
+                    Formula f = new Formula();
+                    f.read(flist);
+                    List tuple = f.literalToArrayList();
+                    sb.append("(");
+                    int i = 0;
+                    for (Iterator it = tuple.iterator(); it.hasNext(); i++) {
+                        if (i > 0) sb.append(" ");
+                        sb.append(treeReplace(oldPattern,
+                                              newTerm,
+                                              (String) it.next()));
+                    }
+                    sb.append(")");
+                }
+            }
+            else {
+                sb.append(flist);
+            }
+            result = sb.toString();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    /** ***************************************************************
+     * Returns a new ArrayList formed by extracting in order the
+     * top-level members of kifListAsString, which is assumed to be
+     * the String representation of a SUO-KIF (LISP) list.
+     *
+     * @param kifListAsString A SUO-KIF list represented as a String
+     *
+     * @return ArrayList
+     *
+     */
+    public static ArrayList kifListToArrayList(String kifListAsString) {
+        ArrayList ans = new ArrayList();
+        try {
+            if (isNonEmptyString(kifListAsString)) {
+                Formula f = new Formula();
+                f.read(kifListAsString);
+                ans = f.literalToArrayList();
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
         }
         return ans;
     }
