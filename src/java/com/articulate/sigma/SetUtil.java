097e2989d44f0dbf8c626f690498afe058adbe90
abu_banaat <abu_banaat>
2009-04-28 14:49:18 +0000

    Added some static utility methods for Sets and Collections.

--- src/java/com/articulate/sigma/SetUtil.java
+++ src/java/com/articulate/sigma/SetUtil.java
@@ -1,5 +1,3 @@
-
-  
 /** This code is copyright Articulate Software (c) 2003.  Some portions
 copyright Teknowledge (c) 2003 and reused under the terms of the GNU license.
 This software is released under the GNU Public License <http://www.gnu.org/copyleft/gpl.html>.
@@ -19,8 +17,98 @@ import java.util.*;
 import java.io.*;
 
 
-  /** ***************************************************************
-  *  A class that handles basic set operations.
-  */
+/** *************************************************************** 
+ * A utility class that defines static methods for working with Sets
+ * and other Collections.
+ */
 public class SetUtil {
-}
+
+    private SetUtil() {
+        // This class should not have any instances.
+    }
+
+    /** ***************************************************************
+     * Removes duplicates from collection based on its natural
+     * comparator or equality operator.
+     *
+     * @param collection The collection from which duplicate elements
+     *                   are to be removed.
+     *
+     */
+    public static void removeDuplicates(Collection collection) {
+        try {
+            HashSet hs = new HashSet();
+            Object obj = null;
+            for (Iterator it = collection.iterator(); it.hasNext();) {
+                obj = (Object) it.next();
+                if (hs.contains(obj)) {
+                    it.remove();
+                }
+                else {
+                    hs.add(obj);
+                }
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return;
+    }
+
+    /** ***************************************************************
+     * Returns true if obj is not a Collection (including if obj ==
+     * null) or if obj is an empty Collection.  Returns false if obj
+     * is a non-empty Collection.
+     *
+     * @param obj The Object to be tested, presumably with the
+     * expectation that it could be a Collection
+     *
+     * @return true or false
+     */
+    public static boolean isEmpty(Object obj) {
+        return (!(obj instanceof Collection) || ((Collection) obj).isEmpty());
+    }
+
+    /** ***************************************************************
+     * Returns the first String value indexed by a String regex matching
+     * key in the property list plist, else returns an empty String
+     * if no regex in plist matches key.
+     *
+     * @param plist A property list (cf. LISP) containing an even
+     * number of Strings, which are understood to be consecutive
+     * regex/value pairs
+     *
+     * @param key A String that will be matched against the regular
+     * expressions in plist.
+     *
+     * @return A String
+     */
+    public static String plistMatch(List<String> plist, String key) {
+        String ans = "";
+        try {
+            int plistSize = plist.size();
+            if ((plistSize > 1) && ((plistSize % 2) == 0)) {
+                int valIdx = -1;
+                String str = null;
+                for (int i = 0; i < plistSize; i++) {
+                    str = plist.get(i);
+                    if (i == valIdx) {
+                        ans = str;
+                        break;
+                    }
+                    if ((i % 2) != 0) {
+                        continue;
+                    }
+                    if (key.matches(str)) {
+                        valIdx = (i + 1);
+                    }
+                }
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return ans;
+    }
+
+}  // SetUtil
