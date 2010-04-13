a1362a233584295d3bc3edf911585f1665e592d8
apease <apease>
2010-04-13 22:25:13 +0000

    Changes to remove duplicate proof step numbers.

--- src/java/com/articulate/sigma/ProofStep.java
+++ src/java/com/articulate/sigma/ProofStep.java
@@ -28,7 +28,7 @@ public class ProofStep {
     public String formulaType = null;
 
      /** A String of the role of the formula */
-  	public String formulaRole = null;
+    public String formulaRole = null;
 
      /** A String containing a valid KIF expression, that is the axiom 
       *  expressing the conclusion of this proof step. */
@@ -39,11 +39,10 @@ public class ProofStep {
     public Integer number = new Integer(0);
 
      /** An ArrayList of Integer(s), which reference prior proof steps from
-      *  which this axiom is derived. 0 signifies the axiom is directly
-      *  from the knowledge base, rather than deduced. Note that the 
-      *  numbering is what the ProofProcessor assigns, not necessarily
-      *  the proof numbers returned directly from Vampire. */
-    public ArrayList premises = new ArrayList();
+      *  which this axiom is derived. Note that the numbering is what
+      *  the ProofProcessor assigns, not necessarily the proof
+      *  numbers returned directly from the inference engine. */
+    public ArrayList<Integer> premises = new ArrayList();
     
     /** ***************************************************************
      * Take an ArrayList of ProofSteps and renumber them consecutively
@@ -52,16 +51,48 @@ public class ProofStep {
      */
     public static ArrayList normalizeProofStepNumbers(ArrayList proofSteps) {
 
-        HashMap numberingMap = new HashMap();
+        // old number, new number
+        HashMap<Integer,Integer> numberingMap = new HashMap();
 
+        //System.out.println("INFO in ProofStep.normalizeProofStepNumbers()");
+        int newIndex = 1;
         for (int i = 0; i < proofSteps.size(); i++) {
+            //System.out.println("INFO in ProofStep.normalizeProofStepNumbers(): numberingMap: " + numberingMap);
             ProofStep ps = (ProofStep) proofSteps.get(i);
-            numberingMap.put(ps.number,new Integer(i + 1));
-            ps.number = new Integer(i + 1);
+            //System.out.println("INFO in ProofStep.normalizeProofStepNumbers(): Checking proof step: " + ps);
+            Integer oldIndex = new Integer(ps.number);
+            if (numberingMap.containsKey(oldIndex)) 
+                ps.number = (Integer) numberingMap.get(oldIndex);
+            else {
+                //System.out.println("INFO in ProofStep.normalizeProofStepNumbers(): adding new step: " + newIndex);
+                ps.number = new Integer(newIndex);            
+                numberingMap.put(oldIndex,new Integer(newIndex++));
+            }
             for (int j = 0; j < ps.premises.size(); j++) {
-                ps.premises.set(j,(Integer) numberingMap.get(ps.premises.get(j)));
+                Integer premiseNum = ps.premises.get(j).intValue();
+                //System.out.println("INFO in ProofStep.normalizeProofStepNumbers(): old premise num: " + premiseNum);
+                Integer newNumber = null;
+                if (numberingMap.get(premiseNum) != null) 
+                    newNumber = new Integer((Integer) numberingMap.get(premiseNum));
+                else {
+                    newNumber = new Integer(newIndex++);
+                    numberingMap.put(premiseNum,new Integer(newNumber));
+                }
+                //System.out.println("INFO in ProofStep.normalizeProofStepNumbers(): new premise num: " + newNumber);
+                ps.premises.set(j,newNumber);
             }
         }
         return proofSteps;
+    }
+
+    /** ***************************************************************
+     */
+    public String toString() {
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("Proof step: " + number + " with " + 
+                  premises.size() + " premises: " + premises + "\n");
+        sb.append(axiom);
+        return sb.toString();
     }
 }
