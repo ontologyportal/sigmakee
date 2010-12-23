26038c205d759bd1c12718c9e9a47ae03aaa75cf
apease <apease>
2010-12-23 17:45:09 +0000

    Added routines to check the pictureList files for obsolete URLs

--- src/java/com/articulate/sigma/KButilities.java
+++ src/java/com/articulate/sigma/KButilities.java
@@ -16,7 +16,9 @@ http://sigmakee.sourceforge.net
 /*************************************************************************************************/
 package com.articulate.sigma;
 import java.io.*;
+import java.net.*;
 import java.util.*;
+import java.util.regex.*;
 
 /** *****************************************************************
  *  Contains utility methods for KBs
@@ -40,6 +42,90 @@ public class KButilities {
 
     /** *************************************************************
      */
+    public static boolean exists(String URLName){
+
+        try {
+            HttpURLConnection.setFollowRedirects(false);
+            // note : you may also need
+            //        HttpURLConnection.setInstanceFollowRedirects(false)
+            HttpURLConnection con =
+                (HttpURLConnection) new URL(URLName).openConnection();
+            con.setRequestMethod("HEAD");
+            return(con.getResponseCode() == HttpURLConnection.HTTP_OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /** *************************************************************
+     */
+    public static void checkURLs(KB kb) {
+
+        URL u = null;
+        ArrayList<Formula> results = kb.ask("arg",0,"externalImage");
+        for (int i = 0; i < results.size(); i++) {
+            Formula f = (Formula) results.get(i);
+            String url = StringUtil.removeEnclosingQuotes(f.getArgument(2));
+            if (!exists(url)) 
+                System.out.println(f + " doesn't exist");
+        }
+    }
+
+
+    /** *************************************************************
+     */
+    public static void validatePictureList() {
+
+        // (externalImage WaterVehicle "http://upload.wikimedia.org/wikipedia/commons/1/12/2003_LWGO_ubt.JPG") 
+        // http://commons.wikimedia.org/wiki/File:2003_LWGO_ubt.JPG
+        // http://upload.wikimedia.org/wikipedia/commons/f/ff/Fishing_boat_ORL-3_Gdynia_Poland_2003_ubt.JPG
+        // http://en.wikipedia.org/wiki/File:Reef.jpg
+        // (externalImage Reef "http://upload.wikimedia.org/wikipedia/en/3/33/Reef.jpg") 
+        // http://upload.wikimedia.org/wikipedia/commons/3/33/Reef.jpg
+        // 
+        URL u = null;
+        String line = null;
+
+        FileReader fr = null;
+        LineNumberReader lr = null;
+
+        try {
+            fr = new FileReader("pictureList.kif");
+            lr = new LineNumberReader(fr);
+            Pattern p = Pattern.compile("([^ ]+) ([^ ]+) \"([^\"]+)\"\\)");
+            while ((line = lr.readLine()) != null) {
+                Matcher m = p.matcher(line);
+                if (m.matches()) {
+                    String url = StringUtil.removeEnclosingQuotes(m.group(3));
+                    //System.out.println("the url: " + url);
+                    if (!exists(url)) 
+                        System.out.println(";; " + line);
+                    else
+                        System.out.println(line);
+                }
+                else
+                    System.out.println(line);
+            }
+        }
+        catch (java.io.IOException e) {
+            System.out.println("Error reading pictureList.kif\n" + e.getMessage());
+        }
+        finally {
+            try {
+                if (lr != null) 
+                    lr.close();            
+                if (fr != null) 
+                    fr.close(); 
+            }
+            catch (Exception ex) {
+            }
+        }
+    }
+
+
+    /** *************************************************************
+     */
     public static void main(String[] args) {
 
         try {
@@ -49,7 +135,9 @@ public class KButilities {
             System.out.println(e.getMessage());
         }
         KB kb = KBmanager.getMgr().getKB("SUMO");
-        countRelations(kb);
+        //countRelations(kb);
+        //checkURLs(kb);
+        validatePictureList();
     }
 }
 
