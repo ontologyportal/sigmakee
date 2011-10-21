e59a890aa6a8f8d0edc801231337bf41c505c945
knomorosa <knomorosa>
2011-10-21 22:36:26 +0000

    Changed SigmaCheck to be more flexible. Now accepts command line arguments for url and email.

--- src/java/com/articulate/sigma/SigmaCheck.java
+++ src/java/com/articulate/sigma/SigmaCheck.java
@@ -11,11 +11,19 @@ August 9, Acapulco, Mexico.  See also http://sigmakee.sourceforge.net
 */
 package com.articulate.sigma;
 
-import java.io.*;
-import java.util.*;
-import java.net.*;
-import javax.mail.*;
-import javax.mail.internet.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.MimeMessage;
 
 /** ***************************************************************
  *  This class, when scheduled as a "cron" job, can serve as a "heartbeat"
@@ -26,8 +34,10 @@ import javax.mail.internet.*;
  */
 public class SigmaCheck {
 
-      // Will create a file object called emailSent.txt located in the Sigma home directory
-    private static File emailSentFile = new File(System.getenv("SIGMA_HOME") + File.separator + "emailSent.txt"); 
+	// Will create a file object called emailSent.txt located in the Sigma home
+	// directory
+	// private static File emailSentFile = new File(System.getenv("SIGMA_HOME")
+	// + File.separator + "emailSent.txt");
 
     /** ***************************************************************
      *  checkURL checks whether an inputed URL contains the string "SUMO" somewhere in it's HTML body
@@ -60,26 +70,28 @@ public class SigmaCheck {
     /** ***************************************************************
      * @param args
      */
-    public static void main(String[] args) {
-  
-        if (!emailSentFile.exists() && !containsSUMO("http://localhost:8080/sigma/KBs.jsp")) {
+	public static void checkSite(String url, String email) {
+		File emailSentFile = new File(System.getenv("SIGMA_HOME")
+				+ File.separator + "emailSent-" + url.hashCode());
+
+        if (!emailSentFile.exists() && !containsSUMO(url)) {
             try {
                 emailSentFile.createNewFile(); //create empty File
                 //send email
                 Properties props = new Properties();
                 //props.put("mail.smtp.host", "owa.mygazoo.com");
                 //props.put("mail.from", "first.last@reardencommerce.com");
-                props.put("mail.smtp.host", "smtp.earthlink.net");
-                props.put("mail.from", "user@earthlink.net");
+				props.put("mail.smtp.host", "owa.mygazoo.com");
+				props.put("mail.from", "adam.pease@reardencommerce.com");
                 Session session = Session.getInstance(props, null);
 
                 MimeMessage msg = new MimeMessage(session);
                 msg.setFrom();
                 //msg.setRecipients(Message.RecipientType.TO,"first.last@reardencommerce.com");
-                msg.setRecipients(Message.RecipientType.TO,"user@earthlink.net");
+				msg.setRecipients(Message.RecipientType.TO, email);
                 msg.setSubject("SIGMA IS DOWN");
                 msg.setSentDate(new Date());
-                msg.setText("The Sigma main page at http://localhost:8080/sigma/KBs.jsp is down.");
+				msg.setText("The Sigma main page at " + url + " is down.");
                 Transport.send(msg);
                 System.out.println("Email Sent");
             } 
@@ -90,7 +102,7 @@ public class SigmaCheck {
                 System.out.println("File creation failed, exception: " + ioex);
             }
         }    
-        else if (emailSentFile.exists() && containsSUMO("http://localhost:8080/sigma/KBs.jsp")) {
+ else if (emailSentFile.exists() && containsSUMO(url)) {
             try {
                 //delete file
                 boolean deleted = emailSentFile.delete();
@@ -100,4 +112,22 @@ public class SigmaCheck {
             }
         }
     }
+
+	public static void main(String[] args) {
+		String url = "http://sigma.ontologyportal.org:4010/sigma/KBs.jsp";
+		String email = "adam.pease@reardencommerce.com";
+
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].contains("http://"))
+				url = args[i];
+			else if (args[i].contains("@"))
+				email = args[i];
+		}
+
+		try {
+			checkSite(url, email);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
