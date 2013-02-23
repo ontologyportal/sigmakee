f133b72b787e89c77fbeb66b9bb4bafb4c66f296
wyattxu <wyattxu>
2013-02-23 03:46:17 +0000

    modified normalized similarity procedure; four test cases are added

--- src/java/com/articulate/sigma/TreeKernel.java
+++ src/java/com/articulate/sigma/TreeKernel.java
@@ -10,6 +10,8 @@ import java.util.Map;
 import javax.swing.text.Utilities;
 import javax.swing.tree.TreeNode;
 
+import edu.uci.ics.jung.graph.DelegateForest;
+import edu.uci.ics.jung.graph.Forest;
 import edu.uci.ics.jung.graph.Tree;
 
 /**
@@ -70,13 +72,15 @@ public class TreeKernel {
 		ParseTreeNode root2 = tree2.getRoot();
 		
 		if(mNormalize){
+			double k_1_2 = continguousSubtreeKernel(tree1, tree2, root1, root2);
+			if(k_1_2 <= 0.0)
+				return 0.0;
+			
 			double k1 = continguousSubtreeKernel(tree1, tree1, root1, root1);
 			double k2 = continguousSubtreeKernel(tree2, tree2, root2, root2);
 			double normTerm = Math.sqrt(k1 * k2);
-			double k_1_2 = continguousSubtreeKernel(tree1, tree2, root1, root2);
 			
-			return k_1_2/normTerm;
-			
+			return k_1_2/normTerm;	
 		}else{
 			return continguousSubtreeKernel(tree1, tree2, root1, root2);
 		}
@@ -100,6 +104,7 @@ public class TreeKernel {
 
 		List<ParseTreeNode> children1 = collection2List(tree1.getChildren(n1));
 		List<ParseTreeNode> children2 = collection2List(tree2.getChildren(n2));
+		
 		int children1Cnt = children1.size();
 		int children2Cnt = children2.size();
 
@@ -205,5 +210,247 @@ public class TreeKernel {
 		}
 
 		return list;
+	}
+	/**
+	 * Test case one: tree with different number of children
+	 * Tree 1:
+	 * 		a
+	 *     /
+	 *    /
+	 *   b
+	 *   
+	 *   Tree 2:
+	 * 		a
+	 *     /\
+	 *    /  \
+	 *   b    c
+	 *   
+	 *   similarity between the two trees is 0, since they have different number of children.
+	 */
+	public static void testCaseOne(){
+		Forest<ParseTreeNode, ParseTreeEdge> tree = new DelegateForest<ParseTreeNode, ParseTreeEdge>();
+		ParseTreeNode node0 = new ParseTreeNode("a", 0);
+		ParseTreeNode node1 = new ParseTreeNode("b", 1);
+		
+		ParseTreeEdge edge1 = new ParseTreeEdge(1, node0, node1);
+		tree.addVertex(node0);
+		tree.addEdge(edge1, node0, node1);
+		
+		Forest<ParseTreeNode, ParseTreeEdge> tree2 = new DelegateForest<ParseTreeNode, ParseTreeEdge>();
+		
+		ParseTreeNode node20 = new ParseTreeNode("a", 0);
+		ParseTreeNode node21 = new ParseTreeNode("b", 1);
+		ParseTreeNode node22 = new ParseTreeNode("c", 2);
+		
+		ParseTreeEdge edge21 = new ParseTreeEdge(1, node20, node21);
+		ParseTreeEdge edge22 = new ParseTreeEdge(2, node20, node22);
+		tree2.addVertex(node20);
+		tree2.addEdge(edge21, node20, node21);
+		tree2.addEdge(edge22, node20, node22);
+		
+		TreeKernel tk = new TreeKernel();
+		
+		Tree<ParseTreeNode, ParseTreeEdge> t1 = tree.getTrees().iterator().next();
+		Tree<ParseTreeNode, ParseTreeEdge> t2 = tree2.getTrees().iterator().next();
+		double unnormalize = tk.treeKernelSim(t1, t2);
+		
+		TreeKernel tk_norm = new TreeKernel(true);
+		double normalize = tk_norm.treeKernelSim(t1, t2);
+		System.out.println("unnormalized sim: " + unnormalize + "\tnormalized sim: " + normalize);
+	}
+	/**
+	 * Test case two: tree with the same number of children, and the children are the same as well
+	 * Tree 1:
+	 * 	    a
+	 *     /\
+	 *    /  \
+	 *   b    c
+	 *   
+	 *   Tree 2:
+	 * 		a
+	 *     /\
+	 *    /  \
+	 *   b    c
+	 *   
+	 *   similarity between the two trees is 2.5 (un-normalized), and 1 (normalized).
+	 */
+	public static void testCaseTwo(){
+		Forest<ParseTreeNode, ParseTreeEdge> tree = new DelegateForest<ParseTreeNode, ParseTreeEdge>();
+		ParseTreeNode node0 = new ParseTreeNode("a", 0);
+		ParseTreeNode node1 = new ParseTreeNode("b", 1);
+		ParseTreeNode node2 = new ParseTreeNode("c", 2);
+		
+		ParseTreeEdge edge1 = new ParseTreeEdge(1, node0, node1);
+		ParseTreeEdge edge2 = new ParseTreeEdge(2, node0, node2);
+		
+		tree.addVertex(node0);
+		tree.addEdge(edge1, node0, node1);
+		tree.addEdge(edge2, node0, node2);
+		
+		Forest<ParseTreeNode, ParseTreeEdge> tree2 = new DelegateForest<ParseTreeNode, ParseTreeEdge>();
+		
+		ParseTreeNode node20 = new ParseTreeNode("a", 0);
+		ParseTreeNode node21 = new ParseTreeNode("b", 1);
+		ParseTreeNode node22 = new ParseTreeNode("c", 2);
+		
+		ParseTreeEdge edge21 = new ParseTreeEdge(1, node20, node21);
+		ParseTreeEdge edge22 = new ParseTreeEdge(2, node20, node22);
+		tree2.addVertex(node20);
+		tree2.addEdge(edge21, node20, node21);
+		tree2.addEdge(edge22, node20, node22);
+		
+		TreeKernel tk = new TreeKernel();
+		
+		Tree<ParseTreeNode, ParseTreeEdge> t1 = tree.getTrees().iterator().next();
+		Tree<ParseTreeNode, ParseTreeEdge> t2 = tree2.getTrees().iterator().next();
+		double unnormalize = tk.treeKernelSim(t1, t2);
+		
+		TreeKernel tk_norm = new TreeKernel(true);
+		double normalize = tk_norm.treeKernelSim(t1, t2);
+		System.out.println("unnormalized sim: " + unnormalize + "\tnormalized sim: " + normalize);
+	}
+	
+	/**
+	 * Test case three: root nodes have the same number of children, 
+	 * but children in two trees have different number of grand children.
+	 * 
+	 * Tree 1:
+	 * 	    a
+	 *     /\
+	 *    /  \
+	 *   b    c
+	 *       /
+	 *      /
+	 *     e
+	 *   Tree 2:
+	 * 		a
+	 *     /\
+	 *    /  \
+	 *   b    c
+	 *       /\
+	 *      /  \
+	 *     e    f
+	 *   
+	 *   similarity between the two trees is 1.75 (un-normalized), and 0.542 (normalized).
+	 */
+	public static void testCaseThree(){
+		Forest<ParseTreeNode, ParseTreeEdge> tree = new DelegateForest<ParseTreeNode, ParseTreeEdge>();
+		ParseTreeNode node0 = new ParseTreeNode("a", 0);
+		ParseTreeNode node1 = new ParseTreeNode("b", 1);
+		ParseTreeNode node2 = new ParseTreeNode("c", 2);
+		ParseTreeNode node3 = new ParseTreeNode("e", 3);
+		
+		ParseTreeEdge edge1 = new ParseTreeEdge(1, node0, node1);
+		ParseTreeEdge edge2 = new ParseTreeEdge(2, node0, node2);
+		ParseTreeEdge edge3 = new ParseTreeEdge(3, node2, node3);
+		
+		tree.addVertex(node0);
+		tree.addEdge(edge1, node0, node1);
+		tree.addEdge(edge2, node0, node2);
+		tree.addEdge(edge3, node2, node3);
+		
+		Forest<ParseTreeNode, ParseTreeEdge> tree2 = new DelegateForest<ParseTreeNode, ParseTreeEdge>();
+		
+		ParseTreeNode node20 = new ParseTreeNode("a", 0);
+		ParseTreeNode node21 = new ParseTreeNode("b", 1);
+		ParseTreeNode node22 = new ParseTreeNode("c", 2);
+		ParseTreeNode node23 = new ParseTreeNode("e", 3);
+		ParseTreeNode node24 = new ParseTreeNode("f", 4);
+		
+		ParseTreeEdge edge21 = new ParseTreeEdge(1, node20, node21);
+		ParseTreeEdge edge22 = new ParseTreeEdge(2, node20, node22);
+		ParseTreeEdge edge23 = new ParseTreeEdge(3, node22, node23);
+		ParseTreeEdge edge24 = new ParseTreeEdge(4, node22, node24);
+		tree2.addVertex(node20);
+		tree2.addEdge(edge21, node20, node21);
+		tree2.addEdge(edge22, node20, node22);
+		tree2.addEdge(edge23, node22, node23);
+		tree2.addEdge(edge24, node22, node24);
+		
+		TreeKernel tk = new TreeKernel();
+		Tree<ParseTreeNode, ParseTreeEdge> t1 = tree.getTrees().iterator().next();
+		Tree<ParseTreeNode, ParseTreeEdge> t2 = tree2.getTrees().iterator().next();
+		double unnormalize = tk.treeKernelSim(t1, t2);
+		
+		TreeKernel tk_norm = new TreeKernel(true);
+		double normalize = tk_norm.treeKernelSim(t1, t2);
+		System.out.println("unnormalized sim: " + unnormalize + "\tnormalized sim: " + normalize);
+	}
+	/**
+	 * Test case two: root have the same number of children, but one of their child is different.
+	 * Also, the child (c) has the same number of grand children in both trees.
+	 * 
+	 * Tree 1:
+	 * 	    a
+	 *     /\
+	 *    /  \
+	 *   j    c
+	 *       /\
+	 *      /  \
+	 *     e    f
+	 *   Tree 2:
+	 * 		a
+	 *     /\
+	 *    /  \
+	 *   b    c
+	 *       /\
+	 *      /  \
+	 *     e    f
+	 *   
+	 *   similarity between the two trees is 2.875 (un-normalized), and 0.79 (normalized).
+	 */
+	public static void testCaseFour(){
+		Forest<ParseTreeNode, ParseTreeEdge> tree = new DelegateForest<ParseTreeNode, ParseTreeEdge>();
+		ParseTreeNode node0 = new ParseTreeNode("a", 0);
+		ParseTreeNode node1 = new ParseTreeNode("j", 1);
+		ParseTreeNode node2 = new ParseTreeNode("c", 2);
+		ParseTreeNode node3 = new ParseTreeNode("e", 3);
+		ParseTreeNode node4 = new ParseTreeNode("f", 4);
+		
+		ParseTreeEdge edge1 = new ParseTreeEdge(1, node0, node1);
+		ParseTreeEdge edge2 = new ParseTreeEdge(2, node0, node2);
+		ParseTreeEdge edge3 = new ParseTreeEdge(3, node2, node3);
+		ParseTreeEdge edge4 = new ParseTreeEdge(4, node2, node4);
+		
+		tree.addVertex(node0);
+		tree.addEdge(edge1, node0, node1);
+		tree.addEdge(edge2, node0, node2);
+		tree.addEdge(edge3, node2, node3);
+		tree.addEdge(edge4, node2, node4);
+		
+		Forest<ParseTreeNode, ParseTreeEdge> tree2 = new DelegateForest<ParseTreeNode, ParseTreeEdge>();
+		
+		ParseTreeNode node20 = new ParseTreeNode("a", 0);
+		ParseTreeNode node21 = new ParseTreeNode("b", 1);
+		ParseTreeNode node22 = new ParseTreeNode("c", 2);
+		ParseTreeNode node23 = new ParseTreeNode("e", 3);
+		ParseTreeNode node24 = new ParseTreeNode("f", 4);
+		
+		ParseTreeEdge edge21 = new ParseTreeEdge(1, node20, node21);
+		ParseTreeEdge edge22 = new ParseTreeEdge(2, node20, node22);
+		ParseTreeEdge edge23 = new ParseTreeEdge(3, node22, node23);
+		ParseTreeEdge edge24 = new ParseTreeEdge(4, node22, node24);
+		tree2.addVertex(node20);
+		tree2.addEdge(edge21, node20, node21);
+		tree2.addEdge(edge22, node20, node22);
+		tree2.addEdge(edge23, node22, node23);
+		tree2.addEdge(edge24, node22, node24);
+		
+		TreeKernel tk = new TreeKernel();
+		
+		Tree<ParseTreeNode, ParseTreeEdge> t1 = tree.getTrees().iterator().next();
+		Tree<ParseTreeNode, ParseTreeEdge> t2 = tree2.getTrees().iterator().next();
+		double unnormalize = tk.treeKernelSim(t1, t2);
+		
+		TreeKernel tk_norm = new TreeKernel(true);
+		double normalize = tk_norm.treeKernelSim(t1, t2);
+		System.out.println("unnormalized sim: " + unnormalize + "\tnormalized sim: " + normalize);
+	}
+	
+	public static void main(String[] args){
+		testCaseOne();
+		testCaseTwo();
+		testCaseThree();
+		testCaseFour();
 	}
 }
