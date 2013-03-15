5378090a3f9e486c6595523c228ff344c3a849db
wyattxu <wyattxu>
2013-03-15 15:42:36 +0000

    simplified text annotator

--- src/java/com/articulate/sigma/TextAnnotator.java
+++ src/java/com/articulate/sigma/TextAnnotator.java
@@ -3,9 +3,14 @@ package com.articulate.sigma;
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.dcoref.CorefChain;
+import edu.stanford.nlp.dcoref.CorefChain.CorefMention;
+import edu.stanford.nlp.dcoref.CorefCoreAnnotations.CorefChainAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
@@ -13,10 +18,16 @@ import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokenEndAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
+import edu.stanford.nlp.trees.semgraph.SemanticGraph;
+import edu.stanford.nlp.trees.semgraph.SemanticGraphCoreAnnotations.BasicDependenciesAnnotation;
+import edu.stanford.nlp.trees.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
+import edu.stanford.nlp.trees.semgraph.SemanticGraphCoreAnnotations.CollapsedDependenciesAnnotation;
+import edu.stanford.nlp.trees.semgraph.SemanticGraphEdge;
 import edu.stanford.nlp.util.CoreMap;
 
 /**
@@ -30,170 +41,133 @@ public class TextAnnotator {
 	
 	public TextAnnotator(){
 		Properties props= new Properties();
-	    props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse");
+	    props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
 		pipeline = new StanfordCoreNLP(props);
 	}
 	
 	/**
-	 * Get the header of the annotated file
-	 * @return
+	 * Get texts contains triggers from a given corpus.
+	 * @param corpusDir the directory of a corpus
+	 * @param outDir the output directory
+	 * @param trigger the trigger word
+	 * @throws Exception
 	 */
-	public String getAnnotationHeader(){
-		String header = "";
-		header += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-		header += "<?xml-stylesheet href=\"CoreNLP-to-HTML.xsl\" type=\"text/xsl\"?>\n";
-		header += "<root>\n";
-		header += "  <document>\n";
-		header += "    <sentences>\n";
+	public static void getTextContainsTriggers(String corpusDir, String outDir, String trigger) throws Exception{
+		PrintWriter writer = null;
+		File[] fileList = new File(corpusDir).listFiles();
+		for( File file : fileList ){
+			String personName = removeExtension(file.getName());
+			if(isMessyWord(personName)) continue;
+			if(personName.contains("(")) continue;
+			if(personName.split("\\s").length > 3) continue;
+			if(personName.contains(" of ")) continue;
+			
+			String text = FileUtils.getTextFromFile(file);
+			if(text == null || text.isEmpty()) continue;
+			WikiArticleParser wap = new WikiArticleParser(text);
+			text = wap.getPlainText().trim();
+			
+			if(!isTermInSequence(text, trigger)) continue;
+			
+			String outfile = outDir + "/" + personName + ".txt";
+			writer = FileUtils.getWriter(outfile);
+			writer.write(text + "\n");
+			writer.flush();
+		}
 		
-		return header;
-	}
-	/**
-	 * Get the tail of the annoted
-	 * @return
-	 */
-	public String getAnnotationTail(){
-		String tail = "";
-		tail += "    </sentences>\n";
-		tail += "  </document>\n";
-		tail += "</root>\n";
-		
-		return tail;
+		writer.close();
 	}
 	
 	/**
-	 * Get annotated tokens.
-	 * @param sentence
+	 * Check if the term is in the sequence.
+	 * @param sequence
+	 * @param term
 	 * @return
 	 */
-	public String getAnnotedTokens(CoreMap sentence){
+	public static boolean isTermInSequence(String sequence, String term){
 		
-		String annotatedTokens = "";
-		annotatedTokens += "        <tokens>\n";
-		for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+		if(sequence == null || sequence.isEmpty() || term == null || term.isEmpty()) return false;
+		
+		int index = sequence.indexOf(term);
+		while(index >= 0){
+			//if it is an independent term, it cannot have English character preceeding or succeeding this term
+			if(index == 0){//at the beginning of the sequence
+				if(index + term.length() == sequence.length()){//they are of the same length
+					if(sequence.equalsIgnoreCase(term)) return true;
+				}else{
+					char succeedingChar = sequence.charAt(index + term.length());
+					if(!Character.isAlphabetic(succeedingChar))
+						return true;
+				}
+			}else if((index + term.length()) == sequence.length()){//at the end of the sequence
+				char preceedingChar = sequence.charAt( index - 1 );
+				if(!Character.isAlphabetic(preceedingChar))
+					return true;
+			}else{//term is inbetween the sequence
+				char preceedingChar = sequence.charAt( index - 1 );
+				char succeedingChar = sequence.charAt(index + term.length());
+				if(!Character.isAlphabetic(preceedingChar) && !Character.isAlphabetic(succeedingChar))
+					return true;
+			}
 			
-			Integer index = token.get(TokenEndAnnotation.class);
-			String word = token.get(TextAnnotation.class);
-			String pos = token.get(PartOfSpeechAnnotation.class);
-			String lemma = token.get(LemmaAnnotation.class);
-			String ne = token.get(NamedEntityTagAnnotation.class); 
-			
-			annotatedTokens +="          <token id=\"" + index + "\">\n";
-			annotatedTokens +="            <word>" + word + "</word>\n";
-			annotatedTokens +="            <lemma>" + lemma + "</lemma>\n";
-			annotatedTokens += "            <POS>" + pos + "</POS>\n";
-			annotatedTokens += "            <NER>" + ne + "</NER>\n";
-			annotatedTokens += "          </token>\n";
+			index = sequence.indexOf(term, index + term.length());
 		}
 		
-		annotatedTokens += "        </tokens>\n";
-		
-		return annotatedTokens;
-	}
-	/**
-	 * Annotate the text
-	 * @param text the plain Wikipedia text
-	 * @param triggerWord the trigger word used for selecting sentences
-	 * @return annotated text
-	 */
-	public String annotateText(String text, String personName, String triggerWord){
-		Annotation annotation = new Annotation(text);
-		pipeline.annotate(annotation);
-		List<CoreMap> sentences = annotation.get(SentencesAnnotation.class);
-		
-		String annotatedText = "";
-		for( CoreMap sentence : sentences ){
-			if( !sentenceHasTerm(sentence, triggerWord) ) continue;
-			if( !sentenceHasTerm(sentence, personName) ) continue;
-			
-			annotatedText += "      <sentence person=\"" + personName + "\">\n";
-			
-			String annotatedTokens = getAnnotedTokens(sentence);
-			annotatedText += annotatedTokens;
-			
-			Tree tree = sentence.get(TreeAnnotation.class);
-			String tree_string = tree.toString();
-			annotatedText += "        <parse>" + tree_string + "</parse>\n";
-			
-			annotatedText += "      </sentence>\n";
-		}
-		
-		return annotatedText;
-	}
-	/**
-	 * Check if the sentence contains the trigger word.
-	 * @param sentence
-	 * @param triggerWord trigger word used for filtering sentences
-	 * @return
-	 */
-	public boolean hasTriggerWord(CoreMap sentence, String triggerWord){
-		for (CoreLabel token : sentence.get(TokensAnnotation.class)) {	
-			
-			String word = token.get(TextAnnotation.class);
-			String lemma = token.get(LemmaAnnotation.class);
-			if(word.equalsIgnoreCase(triggerWord) || lemma.equalsIgnoreCase(triggerWord))
-				return true;
-		}
 		return false;
 	}
 	
-	
-	public boolean sentenceHasTerm(CoreMap sentence, String term){
-		boolean hasTerm = true;
-		String parts[] = term.split("\\s");
-		for( String part : parts ){
-			boolean flag = false;
-			
-			for (CoreLabel token : sentence.get(TokensAnnotation.class)) {	
-				
-				String word = token.get(TextAnnotation.class);
-				String lemma = token.get(LemmaAnnotation.class);
-				if(word.equalsIgnoreCase(part) || lemma.equalsIgnoreCase(part)){
-					flag = true;
-					break;
-				}
-			}
-			
-			if( !flag ){
-				hasTerm = false;
-				break;
-			}
-		}
-		
-		return hasTerm;
-	}
-	
 	/**
-	 * Batch annotate texts.
-	 * @param corpusDir the directory of a corpus
-	 * @param annoFile  the result annotation file (in XML format)
-	 * @param triggerWord  the trigger word for selecting sentences
+	 * Annotate texts in a corpus, including tokenization, part-of-speech tagging,
+	 * lemmatization, named entity recognition, co-reference resolution
+	 * @param corpusDir the corpus directory
+	 * @param outDir the output directory
 	 */
-	public void batchAnnotate(String corpusDir, String annoFile, String triggerWord){
+	public void annotateTexts(String corpusDir, String outDir){
 		try{
-			PrintWriter writer = FileUtils.getWriter(annoFile);
-			String header = getAnnotationHeader();
-			writer.write(header);
-			
+			PrintWriter writer = null;
 			File[] fileList = new File(corpusDir).listFiles();
 			for( File file : fileList ){
 				String text = FileUtils.getTextFromFile(file);
 				if(text == null || text.isEmpty()) continue;
-				String personName = removeExtension(file.getName());
-				String annotatedText = annotateText(text, personName, triggerWord);
 				
-				if( annotatedText == null || annotatedText.isEmpty() ) continue;
-				writer.write(annotatedText);
-				writer.flush();
+				String personName = removeExtension(file.getName());
+				String outfile = outDir + "/" + personName + ".xml";
+				writer = new PrintWriter(outfile);
+				
+				Annotation annotation = new Annotation(text);
+				pipeline.annotate(annotation);
+				pipeline.xmlPrint(annotation, writer);
 			}
 			
-			String tail = getAnnotationTail();
-			writer.write(tail);
-			writer.flush();
 			writer.close();
 		}catch(Exception e){}
 	}
 	
+	/**
+	 * Check the word is messy word or not
+	 * @param word
+	 */
+	public static boolean isMessyWord(String word){
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			if (!isPuncLetterSpace(ch)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	/**
+	 * Check if the char is puncuation, letter or space or not.
+	 * @param ch
+	 * @return
+	 */
+	public static boolean isPuncLetterSpace(char ch) {
+		Pattern pattern = Pattern.compile("[\\p{Alnum}\\p{Punct}\\p{Space}]");
+		Matcher match = pattern.matcher(Character.toString(ch));
+		if (match.find())
+			return true;
+		return false;
+	}
 	/**
 	 * Remove file name extension
 	 * @param filename
@@ -203,13 +177,14 @@ public class TextAnnotator {
 		int index = filename.lastIndexOf(".");
 	    if (index == -1)
 	        return filename;
+	    
 	    return filename.substring(0, index);
 	}
 	
-	public static void main(String[] args){
-		String corpusDir = args[0]; String annoFile = args[1];
-		String triggerWord = args[2];
+	public static void main(String[] args) throws Exception{
+		String corpusDir = args[0]; 
+		String outDir = args[1];
 		TextAnnotator ta = new TextAnnotator();
-		ta.batchAnnotate(corpusDir, annoFile, triggerWord);
+		ta.annotateTexts(corpusDir, outDir);
 	}
 }
