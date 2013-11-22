0f8c2f14c09385cdbf05cea3ce48076e76f729f0
apease <apease>
2013-11-22 01:56:07 +0000

    Added some tests: isRelation(), isAttribute() etc.  Changed exists() to uRLexists()

--- src/java/com/articulate/sigma/KButilities.java
+++ src/java/com/articulate/sigma/KButilities.java
@@ -27,6 +27,70 @@ public class KButilities {
 
     /** *************************************************************
      */
+    public static boolean isRelation(KB kb, String term) {
+        
+        return kb.isInstanceOf(term,"Relation");
+    }
+
+    /** *************************************************************
+     */
+    public static boolean isFunction(KB kb, String term) {
+        return kb.isInstanceOf(term,"Function");        
+    }
+    
+    /** *************************************************************
+     */
+    public static boolean isAttribute(KB kb, String term) {
+        return kb.isInstanceOf(term,"Attribute");
+    }
+    
+    /** *************************************************************
+     */
+    public static boolean isClass(KB kb, String term) {
+        return kb.isInstanceOf(term,"Class");
+    }
+    
+    /** *************************************************************
+     */
+    public static boolean isInstance(KB kb, String term) {
+        return !kb.isInstanceOf(term,"Class");
+    }
+    
+    /** *************************************************************
+     * Get all formulas that contain both terms. 
+     */
+    public static ArrayList<Formula> termIntersection(KB kb, String term1, String term2) {
+  	
+    	ArrayList<Formula> ant1 = kb.ask("ant",0,term1);
+    	ArrayList<Formula> ant2 = kb.ask("ant",0,term2);
+        ArrayList<Formula> cons1 = kb.ask("cons",0,term1);
+        ArrayList<Formula> cons2 = kb.ask("cons",0,term2);
+        HashSet<Formula> hrule1 = new HashSet<Formula>();
+        hrule1.addAll(ant1);
+        hrule1.addAll(cons1);
+        HashSet<Formula> hrule2 = new HashSet<Formula>();
+        hrule2.addAll(ant2);
+        hrule2.addAll(cons2);
+        ArrayList<Formula> result = new ArrayList<Formula>();
+        result.addAll(hrule1);
+        result.retainAll(hrule2);
+        ArrayList<Formula> stmt1 = kb.ask("stmt",0,term1);
+        ArrayList<Formula> stmt2 = kb.ask("stmt",0,term2);
+        stmt1.retainAll(stmt2);
+        result.addAll(stmt1);
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+            	if (j != i) {
+                    ArrayList<Formula> stmt = kb.askWithRestriction(i,term1,j,term2);
+                    result.addAll(stmt);
+            	}
+            }
+        }
+        return result;
+    }
+
+    /** *************************************************************
+     */
     public static void countRelations(KB kb) {
 
         System.out.println("Relations: " + kb.getCountRelations());
@@ -42,7 +106,7 @@ public class KButilities {
 
     /** *************************************************************
      */
-    public static boolean exists(String URLName){
+    private static boolean uRLexists(String URLName){
 
         try {
             HttpURLConnection.setFollowRedirects(false);
@@ -67,7 +131,7 @@ public class KButilities {
         for (int i = 0; i < results.size(); i++) {
             Formula f = (Formula) results.get(i);
             String url = StringUtil.removeEnclosingQuotes(f.getArgument(2));
-            if (!exists(url)) 
+            if (!uRLexists(url)) 
                 System.out.println(f + " doesn't exist");
         }
     }
@@ -99,7 +163,7 @@ public class KButilities {
                 if (m.matches()) {
                     String url = StringUtil.removeEnclosingQuotes(m.group(3));
                     //System.out.println("the url: " + url);
-                    if (!exists(url)) 
+                    if (!uRLexists(url)) 
                         System.out.println(";; " + line);
                     else
                         System.out.println(line);
