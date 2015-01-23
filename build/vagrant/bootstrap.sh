0ce751133008752a1a72214f5aa06f5a59572586
kyjohnsonipsoft <kyjohnsonipsoft>
2015-01-23 19:48:31 +0000

    update for building sigma from /vagrant

--- build/vagrant/bootstrap.sh
+++ build/vagrant/bootstrap.sh
@@ -4,19 +4,40 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install cvs
 
-cd ~/
-#cvs -d :ext:kyjohnsonipsoft@sigmakee.cvs.sourceforge.net:/cvsroot/sigmakee checkout sigma
-cvs -d :ext:anonymous@sigmakee.cvs.sourceforge.net:/cvsroot/sigmakee checkout sigma
+sudo apt-get -y install cvs ant openjdk-7-jdk tomcat7
 
-cd /home/vagrant/sigma
-sudo apt-get -y install ant openjdk-7-jdk
-mkdir ~/KBs
+# this should have been made already
+if [ ! -d /vagrant/sigma ]
+then
+  mkdir /vagrant/sigma
+fi
+
+cd /vagrant/sigma
+
+# KBs dir req'd for Sigma build
+if [ ! -d /vagrant/KBs ]
+then
+  mkdir /vagrant/KBs
+fi
+
 ant createDEB
 
 sudo dpkg -i native-installers/debian/temp/repository/sigma_1.0-1_amd64.deb
-sudo apt-get -y install tomcat7
-sudo apt-get -f install
-sigma start
-sleep 5
+#sudo apt-get -f install
+
+#sigma start
+#sleep 5
+#response=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8080)
+#[ $response == 200 ]; echo $?
+
+
+# test if Sigma is running on guest
 response=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8080)
-[ $response == 200 ]; echo $?
+if [ $response == 200 ]
+then
+  echo Sigma build and installation successful! To use Sigma, navigate to: http://localhost:8888/sigma/login.html
+else
+  echo Sigma startup failed
+fi
+
+
