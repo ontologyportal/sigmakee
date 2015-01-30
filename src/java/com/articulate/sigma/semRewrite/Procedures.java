497fa4057d8049c546e20208f631705996c0ac9d
apease <apease>
2015-01-30 15:16:44 +0000

    Added procedures for type checking during rule processing.

--- src/java/com/articulate/sigma/semRewrite/Procedures.java
+++ src/java/com/articulate/sigma/semRewrite/Procedures.java
@@ -3,19 +3,37 @@ package com.articulate.sigma.semRewrite;
 import com.articulate.sigma.*;
 
 public class Procedures {
-
-    public static String execProc(Clause c) {
-        
-        if (c.pred.equals("isCELTclass")) {
-            return isCELTclass(c);         
-        }
-        return "";
-    }
     
+    // CELT classes which are not SUMO classes like "person"
+    // but not implemented yet, just using SUMO classes
     public static String isCELTclass(Clause c) {
     
         KB kb = KBmanager.getMgr().getKB("SUMO");  
+        //System.out.println("INFO in Procedures.isCELTclass(): " + c);
+        //System.out.println("INFO in Procedures.isCELTclass(): " + kb.isSubclass(c.arg1, c.arg2));
         if (kb.isSubclass(c.arg1, c.arg2))
+            return "true";
+        else
+            return "false";
+    }
+   
+    public static String isSubclass(Clause c) {
+        
+        KB kb = KBmanager.getMgr().getKB("SUMO");  
+        //System.out.println("INFO in Procedures.isSubclass(): " + c);
+        //System.out.println("INFO in Procedures.isSubclass(): " + kb.isSubclass(c.arg1, c.arg2));
+        if (kb.isSubclass(c.arg1, c.arg2))
+            return "true";
+        else
+            return "false";
+    }
+    
+    public static String isInstanceOf(Clause c) {
+        
+        KB kb = KBmanager.getMgr().getKB("SUMO");
+        //System.out.println("INFO in Procedures.isInstanceOf(): " + c);
+        //System.out.println("INFO in Procedures.isInstanceOf(): " + kb.isInstanceOf(c.arg1, c.arg2));
+        if (kb.isInstanceOf(c.arg1, c.arg2))
             return "true";
         else
             return "false";
