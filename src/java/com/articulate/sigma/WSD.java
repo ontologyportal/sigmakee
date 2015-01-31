a4a70a345ed1ff410105f020eb85db9c0f0a6d4f
apease <apease>
2015-01-31 19:34:34 +0000

    Set a threshold so that limited word co-occurance data doesn't predominate over sense frequency data.

--- src/java/com/articulate/sigma/WSD.java
+++ src/java/com/articulate/sigma/WSD.java
@@ -22,46 +22,6 @@ public class WSD {
             if (!StringUtil.emptyString(SUMO))
                 result.add(SUMO);
         }
-        /*
-        String result = "";
-        ArrayList<String> al = WordNet.splitToArrayList(sentence);
-        if (al == null || al.size() < 1)
-            return "";
-        for (int i = 0; i < al.size(); i++) {
-            String word = (String) al.get(i);
-            String SUMO = findSUMOFromWordInContext(word,al);
-            if (!StringUtil.emptyString(SUMO)) {
-                if (SUMO.indexOf("&%") >= 0) {
-                    System.out.println("INFO in WordNet.collectSUMOFromWords(): found in context: " + SUMO);
-                    SUMO = WordNetUtilities.getBareSUMOTerm(SUMO);
-                    //System.out.println("INFO in WordNet.collectSUMOFromWords(): after: " + SUMO);                    
-                }
-                if (result == "")
-                    result = SUMO;
-                else
-                    result = result + " " + SUMO;
-            }
-            else {                                    // assume it's a noun
-                SUMO = getBestDefaultSUMO(word);
-                if (!StringUtil.emptyString(SUMO)) {
-                    if (SUMO.indexOf("&%") >= 0) {
-                        System.out.println("INFO in WordNet.collectSUMOFromWords(): no context: " + SUMO);
-                        SUMO = WordNetUtilities.getBareSUMOTerm(SUMO);
-                        //System.out.println("INFO in WordNet.collectSUMOFromWords(): after: " + SUMO); 
-                    }
-                    if (result == "")
-                        result = SUMO;
-                    else
-                        result = result + " " + SUMO;
-                }
-            }
-            //if (SUMO == null || SUMO == "")
-            //   System.out.println("INFO in findSUMOWordSense(): word not found: " + word);
-           //    else
-           //    System.out.println("INFO in findSUMOWordSense(): word, term: " + word + ", " + SUMO);
-        }
-        System.out.println("INFO in WordNet.collectSUMOFromWords(): result: " + result);
-        */
         return result;
     }
 
@@ -73,7 +33,7 @@ public class WSD {
      */
     public static ArrayList<String> collectWordSenses(String text) {
 
-        System.out.println("INFO in WordNet.collectWordSenses(): " + text);
+        //System.out.println("INFO in WordNet.collectWordSenses(): " + text);
         String newtext = StringUtil.removeHTML(text);
         newtext = StringUtil.removePunctuation(newtext);
         String context = newtext;
@@ -107,56 +67,19 @@ public class WSD {
                 }
             }
         }
-        System.out.println("INFO in WordNet.collectWordSenses(): result: " + result);
+        //System.out.println("INFO in WordNet.collectWordSenses(): result: " + result);
         return result;
     }
-
-    /** ***************************************************************
-     * Return the best guess at the SUMO term for the given word in the
-     * context of the sentence.  Returns a SUMO term.
-     
-    public static String findSUMOFromWordInContext(String word, ArrayList<String> words) {
-
-        //System.out.println("INFO in findSUMOFromWordInContext(): word: " + word);
-        int bestScore = -1;
-        //int POS = 0;
-        String bestTerm = "";
-        for (int i = 1; i < 4; i++) {
-            String newWord = "";
-            if (i == 1)
-                newWord = WordNet.wn.nounRootForm(word,word.toLowerCase());
-            if (i == 2)
-                newWord = WordNet.wn.verbRootForm(word,word.toLowerCase());
-            //System.out.println("INFO in findSUMOFromWordInContext(): root form: " + newWord + " for POS " + i);
-            if (!StringUtil.emptyString(newWord))
-                word = newWord;
-            ArrayList<String> al = findSUMOWordSenseArray(word, words, i);
-            if (al != null && al.size() > 0) {
-                //String synset = (String) al.get(0); // 9-digit
-                String SUMOterm = (String) al.get(1);
-                //System.out.println("INFO in findSUMOFromWordInContext(): SUMO candidate: " + SUMOterm);
-                String bestTotal = (String) al.get(2);
-                int total = (new Integer(bestTotal)).intValue();
-                if (total > bestScore) {
-                    bestScore = total;
-                    //POS = i;
-                    bestTerm = SUMOterm;
-                }
-            }
-        }
-        //System.out.println("INFO in findSUMOFromWordInContext(): result: " + bestTerm);
-        return bestTerm;
-    }
-    */
     
     /** ***************************************************************
      * Return the best guess at the synset for the given word in the
-     * context of the sentence.  @return the 9-digit synset.
+     * context of the sentence.  @return the 9-digit synset but only
+     * if there's a reasonable amount of data.
      */
     public static String findWordSenseInContext(String word, ArrayList<String> words) {
 
-        System.out.println("INFO in findWordSenseInContext(): word, words: " + 
-                word + ", " + words);
+        //System.out.println("INFO in findWordSenseInContext(): word, words: " + 
+        //        word + ", " + words);
         int bestScore = -1;
         String bestSynset = "";
         for (int i = 1; i <= 4; i++) {
@@ -178,8 +101,12 @@ public class WSD {
                 }
             }
         }
-        //System.out.println("INFO in findWordSenseInContext(): best synset: " + bestSynset);
-        return bestSynset;
+        System.out.println("INFO in findWordSenseInContext(): best synset: " + bestSynset);
+        System.out.println("INFO in findWordSenseInContext(): best score: " + bestScore);
+        if (bestScore > 5)
+            return bestSynset;
+        else
+            return "";
     }
         
     /** ***************************************************************
@@ -241,85 +168,6 @@ public class WSD {
         return result;
     }
     
-    /** ***************************************************************
-     * Return the best guess at the synset for the given word.  
-     * Returns an ArrayList consisting of
-     * a 9-digit WordNet synset, and the corresponding SUMO term.
-     
-    private static ArrayList<String> findSUMOWordSensePOS(String word, int POS) {
-
-        //System.out.println("WordNet.findSUMOWordSensePOS(): word: " + word);
-        String SUMOterm = null;
-        ArrayList<String> senses = WordNet.wn.wordsToSenses.get(word.intern());
-        if (senses == null) 
-            return new ArrayList<String>();        
-        int firstSense = -1;
-        for (int i = 0; i < senses.size(); i++) {
-            String sense = (String) senses.get(i);
-            if (WordNetUtilities.sensePOS(sense) == POS) {
-                if (firstSense == -1)
-                    firstSense = i;
-            }
-        }
-        if (firstSense == -1)         // if there were no words of the right part of speech
-            return new ArrayList<String>();            
-        
-        String senseValue = (String) senses.get(firstSense);
-        String synset = WordNet.wn.senseIndex.get(senseValue.intern());
-        switch (POS) {
-            case WordNet.NOUN : SUMOterm = WordNet.wn.nounSUMOHash.get(synset.intern()); break;
-            case WordNet.VERB : SUMOterm = WordNet.wn.verbSUMOHash.get(synset.intern()); break;
-            case WordNet.ADJECTIVE : SUMOterm = WordNet.wn.adjectiveSUMOHash.get(synset.intern()); break;
-            case WordNet.ADVERB : SUMOterm = WordNet.wn.adverbSUMOHash.get(synset.intern()); break;
-        }
-        if (SUMOterm != null) {                                                // Remove SUMO-WordNet mapping characters
-            SUMOterm = SUMOterm.replaceAll("&%","");
-            SUMOterm = SUMOterm.replaceAll("[+=@]","");
-        }
-        ArrayList<String> result = new ArrayList<String>();
-        result.add((new Integer(POS)).toString()+synset);
-        result.add(SUMOterm);
-        //System.out.println("WordNet.findSUMOWordSensePOS(): result: " + result);
-        return result;
-    }
-*/
-
-    /** ***************************************************************
-     * Return the best guess at the synset for the given word.
-     * @return a 9 digit synset number
-     
-    public static String findWordSense(String word) {
-
-        //System.out.println("INFO in WordNet.findWordSense(): " + word);
-        int bestScore = 0;
-        //int POS = 0;
-        String bestSynset = "";
-        for (int i = 1; i <= 4; i++) {
-            String newWord = "";
-            if (i == 1)
-                newWord = WordNet.wn.nounRootForm(word,word.toLowerCase());
-            if (i == 2)
-                newWord = WordNet.wn.verbRootForm(word,word.toLowerCase());
-            if (newWord != null && newWord != "")
-                word = newWord;
-            //System.out.println("INFO in WordNet.findWordSense(): newWord: " + word);
-            ArrayList<String> al = findSUMOWordSensePOS(word, i);
-            if (al != null && al.size() > 0) {
-                String synset = al.get(0); // 9-digit
-                //String SUMOterm = al.get(1);
-                String bestTotal = Integer.toString(4-i);  // give priority to nouns
-                //System.out.println("INFO in WordNet.findWordSense(): synset,SUMO " + synset + "," + SUMOterm);
-                int total = (new Integer(bestTotal)).intValue();
-                if (total > bestScore) {
-                    bestScore = total;
-                    //POS = i;
-                    bestSynset = synset;
-                }
-            }
-        }
-        return bestSynset;
-    }
-*/
     /** ***************************************************************
      * Get the SUMO term that represents the best guess at
      * meaning for a word.  This method attempts to convert to root form.
@@ -412,7 +260,7 @@ public class WSD {
             }
         }
         if (bestSense == "") {
-            //System.out.println("WSD.getBestDefaultSense(): no frequencies for " + word);
+            System.out.println("WSD.getBestDefaultSense(): no frequencies for " + word);
             ArrayList<String> al = WordNet.wn.wordsToSenses.get(word);
             if (al != null) {
                 Iterator<String> it = al.iterator();
@@ -448,7 +296,7 @@ public class WSD {
             newWord = WordNet.wn.verbRootForm(word,word.toLowerCase());
         if (StringUtil.emptyString(newWord))
             newWord = word;
-        System.out.println("WSD.getBestDefaultSense(): word: " + newWord);
+        //System.out.println("WSD.getBestDefaultSense(): word: " + newWord);
         TreeSet<AVPair> senseKeys = WordNet.wn.wordFrequencies.get(newWord);
         //System.out.println("WSD.getBestDefaultSense(): sensekeys: " + senseKeys);
         if (senseKeys != null) {
@@ -501,66 +349,29 @@ public class WSD {
      *  A method used only for testing.  It should not be called
      *  during normal operation.
      */
-    public static void testWSD () {
+    public static void testWSD() {
         
-        //System.out.println("INFO in WordNet.main(): " + WordNet.wn.multiWord.get("four"));
-        //System.out.println((new ArrayList(WordNet.wn.senseIndex.keySet()).get(0)));
-        //System.out.println(WordNet.wn.senseIndex.get("coffee_NN_1"));
-
-        // 107827896 dill
-        // 107811416 herb
-        // 107816296 bay leaf
-        // 100021265 food, nutrient
-        
-        //ArrayList<String> al = WordNet.wn.collectWordSenses("A computer is a general purpose device that can be programmed to carry out a finite set of arithmetic or logical operations.");
-        //ArrayList<String> al = WordNet.wn.collectWordSenses("A four stroke engine is a beautiful thing.");
-        ArrayList<String> al = collectWordSenses("Bob likes all living things.");
-        System.out.println("in main(): " + al);
-        String synString = al.get(1);
-        String[] synsets = synString.split(" ");
-        for (int i = 0; i < synsets.length; i++) {
-            System.out.println("in main(): synset,words: " + 
-                    synsets[i] + " " + WordNet.wn.synsetsToWords.get(synsets[i]));
-        }        
-    }
-
-    /** ***************************************************************
-     *  A method used only for testing.  It should not be called
-     *  during normal operation.
-     */
-    public static void oldTestWSD (String[] args) {
-
-        StringBuffer textBuffer = new StringBuffer();
         try {
-            FileInputStream fileStream = new FileInputStream(args[0]);
-            DataInputStream dataStream = new DataInputStream(fileStream);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(dataStream));
-            String line;
-    
-            while ((line = reader.readLine()) != null) {
-                textBuffer.append(line + " ");
-            }
-            dataStream.close();
+            KBmanager.getMgr().initializeOnce();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
-        catch (IOException ioe) {
-            System.out.println("Error in WSD.oldTestWSD(): " + ioe.getMessage());
-            ioe.printStackTrace();
-        }
-        String text = textBuffer.toString();
-        ArrayList<String> al = WSD.collectWordSenses(text);
-           
-        System.out.println("in main(): " + al);
-        String synString = al.get(1);
-        String[] synsets = synString.split(" ");
-        for (int i = 0; i < synsets.length; i++) {
-            System.out.println("in WSD.oldTestWSD(): synset,words,SUMO: " + 
-                    synsets[i] + " " + WordNet.wn.synsetsToWords.get(synsets[i]) + " " + 
-                    WordNetUtilities.getBareSUMOTerm(WordNet.wn.getSUMOMapping(synsets[i])));
-        }     
-        /*String sentence = "Bob likes all living things.";
-        String params = "flang=KIF&lang=EnglishLanguage&kb=SUMO";
-        WordNet.wn.sumoSentenceDisplay(sentence, sentence, params);
-        */
+        WordNet.initOnce();
+        System.out.println("INFO in WSD.testWSD(): done initializing");
+
+        String sentence = "John walks.";
+        System.out.println("INFO in WSD.testWSD(): " + sentence);
+        System.out.println("INFO in WSD.testWSD(): " + WSD.collectWordSenses(sentence));
+        sentence = "Bob runs around the track.";
+        System.out.println("INFO in WSD.testWSD(): " + sentence);
+        System.out.println("INFO in WSD.testWSD(): " + WSD.collectWordSenses(sentence));
+        sentence = "A computer is a general purpose device that can be programmed to carry out a finite set of arithmetic or logical operations.";
+        System.out.println("INFO in WSD.testWSD(): " + sentence);
+        System.out.println("INFO in WSD.testWSD(): " + WSD.collectWordSenses(sentence));
+        sentence = "A four stroke engine is a beautiful thing.";
+        System.out.println("INFO in WSD.testWSD(): " + sentence);
+        System.out.println("INFO in WSD.testWSD(): " + WSD.collectWordSenses(sentence));     
+        System.out.println("INFO in WSD.testWSD(): " + WordNet.wn.sumoSentenceDisplay(sentence,sentence,""));
     }
     
     /** ***************************************************************
@@ -569,24 +380,6 @@ public class WSD {
      */
     public static void main (String[] args) {
 
-        try {
-            KBmanager.getMgr().initializeOnce();
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-        WordNet.initOnce();
-        System.out.println("INFO in WSD.main(): done initializing");
-
-        //String sentence = "Bob runs around the track.";
-        //ArrayList<String> al = WordNet.wn.collectWordSenses("A computer is a general purpose device that can be programmed to carry out a finite set of arithmetic or logical operations.");
-        //ArrayList<String> al = WordNet.wn.collectWordSenses("A four stroke engine is a beautiful thing.");
-
-        //ArrayList<String> al = WordNet.splitToArrayList(sentence);
-        //String synset = findWordSenseInContext("runs",al);
-        //System.out.println("INFO in WSD.main(): " + synset);
-        //System.out.println("INFO in WSD.main(): sense for 'pin': " + WSD.getBestDefaultSUMOsense("pin",1));
-        String sentence = "John walks.";
-        //System.out.println("INFO in WSD.main(): " + WSD.collectWordSenses(sentence));
-        System.out.println("INFO in WSD.main(): " + WordNet.wn.sumoSentenceDisplay(sentence,sentence,""));
+        testWSD();
     }
 }
