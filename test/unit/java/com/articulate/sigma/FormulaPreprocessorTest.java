ae5949815c875e8c8f4165c83d4506413a445c70
reedandrew49 <reedandrew49>
2015-02-05 17:17:38 +0000

    move tests to correct place

--- test/unit/java/com/articulate/sigma/FormulaPreprocessorTest.java
+++ test/unit/java/com/articulate/sigma/FormulaPreprocessorTest.java
@@ -276,32 +276,6 @@ public class FormulaPreprocessorTest extends UnitTestBase  {
 
     //FIXME: Rename me
     @Test
-    public void testFindTypes2() {
-        Map<String, HashSet<String>> expected = Maps.newHashMap();
-        expected.put("?NOTPARTPROB", Sets.newHashSet("Quantity", "Entity"));
-        expected.put("?PART", Sets.newHashSet("SetOrClass", "Object+"));
-        expected.put("?PARTPROB", Sets.newHashSet("Quantity", "Entity"));
-        expected.put("?X", Sets.newHashSet("Object", "Entity"));
-        expected.put("?WHOLE", Sets.newHashSet("SetOrClass", "Object+"));
-        expected.put("?Y", Sets.newHashSet("Object", "Entity"));
-        expected.put("?Z", Sets.newHashSet("Object", "Entity"));
-
-        String strf = "(=> (and (typicalPart ?PART ?WHOLE) (instance ?X ?PART) " +
-                "(equal ?PARTPROB (ProbabilityFn (exists (?Y) (and " +
-                "(instance ?Y ?WHOLE) (part ?X ?Y))))) (equal ?NOTPARTPROB " +
-                "(ProbabilityFn (not (exists (?Z) (and (instance ?Z ?WHOLE) " +
-                "(part ?X ?Z))))))) (greaterThan ?PARTPROB ?NOTPARTPROB))";
-        Formula f = new Formula();
-        f.read(strf);
-        FormulaPreprocessor fp = new FormulaPreprocessor();
-
-        HashMap<String, HashSet<String>> actualMap = fp.computeVariableTypes(f, kb);
-
-        assertEquals(expected, actualMap);
-    }
-
-    //FIXME: Rename me
-    @Test
     public void testFindTypes3() {
         Map<String, HashSet<String>> expected = Maps.newHashMap();
         expected.put("?REL", Sets.newHashSet("Entity"));
@@ -399,26 +373,4 @@ public class FormulaPreprocessorTest extends UnitTestBase  {
         assertEquals(expected, actual);
     }
 
-    @Test
-    public void testAddTypes3() {
-        String strf = "(=> (and (typicalPart ?PART ?WHOLE) (instance ?X ?PART) " +
-                "(equal ?PARTPROB (ProbabilityFn (exists (?Y) (and " +
-                "(instance ?Y ?WHOLE) (part ?X ?Y))))) (equal (?NOTPARTPROB " +
-                "(ProbabilityFn (not (exists (?Z) (and (instance ?Z ?WHOLE) " +
-                "(part ?X ?Z))))))) (greaterThan ?PARTPROB ?NOTPARTPROB))";
-        Formula f = new Formula();
-        f.read(strf);
-        FormulaPreprocessor fp = new FormulaPreprocessor();
-
-        Formula expected = new Formula();
-        String expectedString = "(=> (and (instance ?PART SetOrClass) (subclass ?PART Object) (instance ?PARTPROB Entity) (instance ?X Object) (instance ?WHOLE SetOrClass) (subclass ?WHOLE Object) (instance ?Y Object)) " +
-                "(=> (and (typicalPart ?PART ?WHOLE) (instance ?X ?PART) " +
-                "(equal ?PARTPROB (ProbabilityFn (exists (?Y) (and (instance ?Y ?WHOLE) (part ?X ?Y)))))" +
-                "(equal (?NOTPARTPROB (ProbabilityFn (not (exists (?Z) (and (instance ?Z ?WHOLE) (part ?X ?Z))))))) " +
-                "(greaterThan ?PARTPROB ?NOTPARTPROB))) ";
-        expected.read(expectedString);
-        Formula actual = fp.addTypeRestrictionsNew(f, kb);
-        //assertTrue("expected: " + expected.toString() + ", but was: " + actual.toString(), expected.equals(actual));
-        assertEquals(expected, actual);
-    }
 }
