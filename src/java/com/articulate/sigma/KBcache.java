a989bf8e105407a5b45fe56356ddd766631afa4f
apease <apease>
2015-02-07 22:21:45 +0000

    Comment changes

--- src/java/com/articulate/sigma/KBcache.java
+++ src/java/com/articulate/sigma/KBcache.java
@@ -1,3 +1,31 @@
+/** This code is copyright Articulate Software (c) 2003.  Some
+portions copyright Teknowledge (c) 2003 and reused under the termsof the GNU
+license.  This software is released under the GNU Public License
+<http://www.gnu.org/copyleft/gpl.html>.  Users of this code also consent,
+by use of this code, to credit Articulate Software and Teknowledge in any
+writings, briefings, publications, presentations, or other representations
+of any software which incorporates, builds on, or uses this code.  Please
+cite the following article in any publication with references:
+
+Pease, A., (2003). The Sigma Ontology Development Environment, in Working
+Notes of the IJCAI-2003 Workshop on Ontology and Distributed Systems,
+August 9, Acapulco, Mexico. see also
+http://sigmakee.sourceforge.net
+
+Note that this class, and therefore, Sigma, depends upon several terms
+being present in the ontology in order to function as intended.  They are:
+  domain
+  domainSubclass
+  Entity
+  instance
+  Relation
+  subclass
+  subrelation
+  TransitiveRelation
+*/
+
+/*************************************************************************************************/
+
 package com.articulate.sigma;
 
 import java.io.*;
