48ef5f0cbc539166eb2de0255e08d2f961b25229
apease <apease>
2015-02-07 22:25:45 +0000

    Comment changes.

--- src/java/com/articulate/sigma/TPTP3ProofProcessor.java
+++ src/java/com/articulate/sigma/TPTP3ProofProcessor.java
@@ -1,3 +1,16 @@
+/* This code is copyright Articulate Software (c) 2003.  
+This software is released under the GNU Public License <http://www.gnu.org/copyleft/gpl.html>.
+Users of this code are also requested, to credit Articulate Software in any
+writings, briefings, publications, presentations, or 
+other representations of any software which incorporates,
+builds on, or uses this code. Please cite the following
+article in any publication with references:
+
+Pease, A., (2003). The Sigma Ontology Development Environment, 
+in Working Notes of the IJCAI-2003 Workshop on Ontology and Distributed Systems,
+August 9, Acapulco, Mexico.  See also http://sigmakee.sourceforge.net
+*/
+
 package com.articulate.sigma;
 
 import TPTPWorld.TPTPFormula;
