417417c996500b9a195167eabefcc0c71ff9c43d
qingqingcai <qingqingcai>
2015-02-13 14:02:56 +0000

    Qingqing: changes in TPTP3ProofProcessor - use InferenceType for postprocessing

--- src/java/com/articulate/sigma/TPTP3ProofProcessor.java
+++ src/java/com/articulate/sigma/TPTP3ProofProcessor.java
@@ -46,9 +46,9 @@ public class TPTP3ProofProcessor {
 			sb.append(proof.get(i));
 		return sb.toString();
 	}
-	
-    /** ***************************************************************
-     */
+
+	/** ***************************************************************
+	 */
 	public static String trimBrackets (String line) {
 
 		// System.out.println("Info in trimBrackets: " + line);
@@ -59,9 +59,9 @@ public class TPTP3ProofProcessor {
 			return null;
 		}
 	}
-     
-    /** ***************************************************************
-     */
+
+	/** ***************************************************************
+	 */
 	public static String trimParens (String line) {
 
 		// line does not always starts with "("; eg: ?[X1]:(s__instance(X1,s__SetOrClass)=>s__subclass(X1,s__Object))
@@ -73,11 +73,11 @@ public class TPTP3ProofProcessor {
 			return null;
 		}
 	}
-	
+
 	/** ***************************************************************
-     */
+	 */
 	public ArrayList<Integer> parseInferenceObject(String supportId) {
-		
+
 		//System.out.println("Info in TPTP3ProofProcessor.parseInferenceObject(): " + supportId);
 
 		ArrayList<Integer> prems = new ArrayList<Integer>();
@@ -90,10 +90,23 @@ public class TPTP3ProofProcessor {
 			supports = "[" + supportId + "]";
 		supports = supportId.substring(secondComma+1,secondParen);
 
-		if (supports.startsWith("inference")) 
+		if (supports.startsWith("inference"))
 			return parseInferenceObject(supports);
-		else 
-			return parseSupports(supports.trim()); 
+		else
+			return parseSupports(supports.trim());
+	}
+
+	/** ***************************************************************
+	 */
+	public String getInferenceType(String supportId) {
+
+		String inferenceType = null;
+		if (supportId.startsWith("inference(")) {
+			int firstParen = supportId.indexOf("(");
+			int firstComma = supportId.indexOf(",");
+			inferenceType = supportId.substring(firstParen+1, firstComma);
+		}
+		return inferenceType;
 	}
 
 	/** ***************************************************************
@@ -106,14 +119,14 @@ public class TPTP3ProofProcessor {
 		if (supportId.startsWith("[")) {
 			supportId = trimBrackets(supportId).trim();
 			//System.out.println("Info in TPTP3ProofProcessor.parseSupports()2: " + supportId);
-			if (supportId.startsWith("inference(")) 
+			if (supportId.startsWith("inference("))
 				return parseInferenceObject(supportId);
 			String[] supportSet = supportId.split(",");
 			for (int i = 0; i < supportSet.length; i++) {
 				//System.out.println("Info in TPTP3ProofProcessor.parseSupports(): support element: " + supportSet[i]);
 				if (supportSet[i].indexOf("(") == -1) {
 					Integer stepnum = idTable.get(supportSet[i].trim());
-					if (stepnum == null) 
+					if (stepnum == null)
 						System.out.println("Error in TPTP3ProofProcessor.parseSupports() no id: " + stepnum +
 								" for premises at step " + supportSet[i]);
 					else
@@ -133,27 +146,27 @@ public class TPTP3ProofProcessor {
 		else {
 			Integer stepnum = idTable.get(supportId);
 			if (stepnum == null) {
-				System.out.println("Error in TPTP3ProofProcessor.parseSupports() no id: " + stepnum +
-						" for premises at step " + supportId);
+	//			System.out.println("Error in TPTP3ProofProcessor.parseSupports() no id: " + stepnum +
+	//					" for premises at step " + supportId);
 				return prems;
 			}
 			else {
 				prems.add(stepnum);
 				return prems;
 			}
-		}    		
+		}
 		return prems;
 	}
-	
-    /** ***************************************************************
-     * Parse a step like the following into its constituents
-     *   fof(c_0_5, axiom, (s__subclass(s__Artifact,s__Object)), c_0_3).
-     *   
-     *   fof(c_0_2, 
-     *       negated_conjecture, 
-     *       (~(?[X1]:(s__subclass(X1,s__Object)&~$answer(esk1_1(X1))))), 
-     *       inference(assume_negation,[status(cth)],[inference(add_answer_literal,[status(thm)],[c_0_0, theory(answers)])])).
-     */
+
+	/** ***************************************************************
+	 * Parse a step like the following into its constituents
+	 *   fof(c_0_5, axiom, (s__subclass(s__Artifact,s__Object)), c_0_3).
+	 *
+	 *   fof(c_0_2,
+	 *       negated_conjecture,
+	 *       (~(?[X1]:(s__subclass(X1,s__Object)&~$answer(esk1_1(X1))))),
+	 *       inference(assume_negation,[status(cth)],[inference(add_answer_literal,[status(thm)],[c_0_0, theory(answers)])])).
+	 */
 	public ProofStep parseProofStep (String line) {
 
 		if (StringUtil.emptyString(line))
@@ -211,14 +224,16 @@ public class TPTP3ProofProcessor {
 		}
 		// System.out.println("KIF stmnt : " + stmnt);
 		ps.axiom = stmnt;
-		String supportId = rest.substring(statementEnd+2,rest.length()).trim();     	 
+		String supportId = rest.substring(statementEnd+2,rest.length()).trim();
 		// System.out.println("supportID: " + supportId);
+		// add an inference type
+		ps.inferenceType = getInferenceType(supportId.trim());
 		ps.premises.addAll(parseSupports(supportId.trim()));
 		return ps;
 	}
-      
-    /** ***************************************************************
-     */
+
+	/** ***************************************************************
+	 */
 	public void processAnswers (String line) {
 
 		//System.out.println("Info in TPTP3ProofProcessor.processAnswers(): " + line);
@@ -268,136 +283,138 @@ public class TPTP3ProofProcessor {
 		}
 		return answerString.trim();
 	}
-     
-     /** ***************************************************************
-      */
-     public void printAnswers () {
 
-    	 System.out.println("Answers:");
-    	 for (int i = 0; i < bindings.size(); i++)
-    		 System.out.println(bindings.get(i));
-     }
+	/** ***************************************************************
+	 */
+	public void printAnswers () {
 
-      /** ***************************************************************
-       */
-     public static void testParseProofStep () {
+		System.out.println("Answers:");
+		for (int i = 0; i < bindings.size(); i++)
+			System.out.println(bindings.get(i));
+	}
 
-    	 String ps1 = "fof(c_0_5, axiom, (s__subclass(s__Artifact,s__Object)), c_0_3).";
-    	 String ps2 = "fof(c_0_2, negated_conjecture,(~(?[X1]:(s__subclass(X1,s__Object)&~$answer(esk1_1(X1)))))," + 
-    			 "inference(assume_negation,[status(cth)],[inference(add_answer_literal,[status(thm)],[c_0_0, theory(answers)])])).";
-    	 String ps3 = "cnf(c_0_14,negated_conjecture,($false), " +
-    			 "inference(eval_answer_literal,[status(thm)], [inference(spm,[status(thm)],[c_0_12, c_0_13, theory(equality)]), theory(answers)]), ['proof']).";
-    	 TPTP3ProofProcessor tpp = new TPTP3ProofProcessor();
-    	 tpp.idTable.put("c_0_0", new Integer(0));
-    	 tpp.idTable.put("c_0_3", new Integer(1));    	 
-    	 tpp.idTable.put("c_0_12", new Integer(2));
-    	 tpp.idTable.put("c_0_13", new Integer(3));
-    	 System.out.println(tpp.parseProofStep(ps1));
-    	 System.out.println();
-    	 System.out.println(tpp.parseProofStep(ps1));
-    	 System.out.println();
-    	 System.out.println(tpp.parseProofStep(ps3));
-     }
+	/** ***************************************************************
+	 */
+	public static void testParseProofStep () {
 
-     /** ***************************************************************
-      */
-    public static TPTP3ProofProcessor parseProofOutput (LineNumberReader lnr) {
-    	
-       	TPTP3ProofProcessor tpp = new TPTP3ProofProcessor();
-    	try {
-    		boolean inProof = false;
-    		String line;
-    		while ((line = lnr.readLine()) != null) {
-    			if (line.indexOf("SZS output start") != -1) {
-    				inProof = true;
-    				line = lnr.readLine();
-    			}
-    			if (line.indexOf("SZS status") != -1) {
-    				tpp.status = line.substring(15);
-    			}
-    			if (line.indexOf("SZS answers") != -1) {
-    				tpp.processAnswers(line.substring(20).trim());
-    				tpp.printAnswers();
-    			}
-    			if (inProof) {
-    				if (line.indexOf("SZS output end") != -1) {
-    					inProof = false;
-    				}
-    				else {
-    					ProofStep ps = tpp.parseProofStep(line);
-    					if (ps != null) {
-    						tpp.proof.add(ps);
-    					}
-    				}
-    			}
-    		}
-    	} 
-    	catch (Exception ex) {
-    		System.out.println(ex.getMessage());
-    	}
+		String ps1 = "fof(c_0_5, axiom, (s__subclass(s__Artifact,s__Object)), c_0_3).";
+		String ps2 = "fof(c_0_2, negated_conjecture,(~(?[X1]:(s__subclass(X1,s__Object)&~$answer(esk1_1(X1)))))," +
+				"inference(assume_negation,[status(cth)],[inference(add_answer_literal,[status(thm)],[c_0_0, theory(answers)])])).";
+		String ps3 = "cnf(c_0_14,negated_conjecture,($false), " +
+				"inference(eval_answer_literal,[status(thm)], [inference(spm,[status(thm)],[c_0_12, c_0_13, theory(equality)]), theory(answers)]), ['proof']).";
+		TPTP3ProofProcessor tpp = new TPTP3ProofProcessor();
+		tpp.idTable.put("c_0_0", new Integer(0));
+		tpp.idTable.put("c_0_3", new Integer(1));
+		tpp.idTable.put("c_0_12", new Integer(2));
+		tpp.idTable.put("c_0_13", new Integer(3));
+		System.out.println(tpp.parseProofStep(ps1));
+		System.out.println();
+		System.out.println(tpp.parseProofStep(ps1));
+		System.out.println();
+		System.out.println(tpp.parseProofStep(ps3));
+	}
+
+	/** ***************************************************************
+	 */
+	public static TPTP3ProofProcessor parseProofOutput (LineNumberReader lnr) {
+
+		TPTP3ProofProcessor tpp = new TPTP3ProofProcessor();
+		try {
+			boolean inProof = false;
+			String line;
+			while ((line = lnr.readLine()) != null) {
+				if (line.indexOf("SZS output start") != -1) {
+					inProof = true;
+					line = lnr.readLine();
+				}
+				if (line.indexOf("SZS status") != -1) {
+					tpp.status = line.substring(15);
+				}
+				if (line.indexOf("SZS answers") != -1) {
+					tpp.processAnswers(line.substring(20).trim());
+					tpp.printAnswers();
+				}
+				if (inProof) {
+					if (line.indexOf("SZS output end") != -1) {
+						inProof = false;
+					}
+					else {
+						ProofStep ps = tpp.parseProofStep(line);
+						if (ps != null) {
+							tpp.proof.add(ps);
+						}
+					}
+				}
+			}
+		}
+		catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		// remove unnecessary steps, eg: conjectures, duplicate trues
+		tpp.proof = ProofStep.removeUnnecessary(tpp.proof);
 		tpp.proof = ProofStep.removeDuplicates(tpp.proof);
-    	return tpp;
-    }
-    
-     /** ***************************************************************
-      */
-    public static TPTP3ProofProcessor parseProofOutput (String st) {
-    	
-    	StringReader sr = new StringReader(st);
-    	LineNumberReader lnr = new LineNumberReader(sr);
-    	return parseProofOutput(lnr);
- 
-    }
-    
-     /** ***************************************************************
-      */
-    public static void testParseProofFile () {
+		return tpp;
+	}
 
-    	TPTP3ProofProcessor tpp = new TPTP3ProofProcessor();
-    	try {
-    		FileReader r = new FileReader("/home/apease/Programs/E/PROVER/eltb_out.txt");
-    		LineNumberReader lnr = new LineNumberReader(r);
-    		tpp = parseProofOutput(lnr);
-    	} 
-    	catch (Exception ex) {
-    		System.out.println(ex.getMessage());
-    	}
-    	System.out.println("-------------------------------------------------");
-    	System.out.println(tpp.proof);
-    }
-      
-    /** ***************************************************************
-     */
-    public static void testE () {
+	/** ***************************************************************
+	 */
+	public static TPTP3ProofProcessor parseProofOutput (String st) {
 
-    	try {
-    		System.out.println("INFO in EProver.main()");
-    		//KBmanager.getMgr().initializeOnce();
-    		//KB kb = KBmanager.getMgr().getKB("SUMO");
-    		KB kb = null;
-    		System.out.println("------------- INFO in EProver.main() completed initialization--------");
-    		EProver eprover = new EProver("/home/apease/Programs/E/PROVER/e_ltb_runner",
-    				"/home/apease/Sigma/KBs/SUMO.tptp");
+		StringReader sr = new StringReader(st);
+		LineNumberReader lnr = new LineNumberReader(sr);
+		return parseProofOutput(lnr);
 
-    		String result = eprover.submitQuery("(subclass Patio Object)",kb);
-    		StringReader sr = new StringReader(result);
-    		LineNumberReader lnr = new LineNumberReader(sr);
-    		TPTP3ProofProcessor tpp = parseProofOutput(lnr);
-    		System.out.println("-------------------------------------------------");
-        	System.out.println(tpp.proof);
-    		eprover.terminate();
-    	} 
-    	catch (Exception e) {
-    		System.out.println(e.getMessage());
-    	}
-    }
-   
-    /** ***************************************************************
-     */
-    public static void main (String[] args) {
+	}
 
-    	//testParseProofStep();
-    	//testParseProofFile();
-    	testE();
-    }
+	/** ***************************************************************
+	 */
+	public static void testParseProofFile () {
+
+		TPTP3ProofProcessor tpp = new TPTP3ProofProcessor();
+		try {
+			FileReader r = new FileReader("/home/apease/Programs/E/PROVER/eltb_out.txt");
+			LineNumberReader lnr = new LineNumberReader(r);
+			tpp = parseProofOutput(lnr);
+		}
+		catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		System.out.println("-------------------------------------------------");
+		System.out.println(tpp.proof);
+	}
+
+	/** ***************************************************************
+	 */
+	public static void testE () {
+
+		try {
+			System.out.println("INFO in EProver.main()");
+			//KBmanager.getMgr().initializeOnce();
+			//KB kb = KBmanager.getMgr().getKB("SUMO");
+			KB kb = null;
+			System.out.println("------------- INFO in EProver.main() completed initialization--------");
+			EProver eprover = new EProver("/home/apease/Programs/E/PROVER/e_ltb_runner",
+					"/home/apease/Sigma/KBs/SUMO.tptp");
+
+			String result = eprover.submitQuery("(subclass Patio Object)",kb);
+			StringReader sr = new StringReader(result);
+			LineNumberReader lnr = new LineNumberReader(sr);
+			TPTP3ProofProcessor tpp = parseProofOutput(lnr);
+			System.out.println("-------------------------------------------------");
+			System.out.println(tpp.proof);
+			eprover.terminate();
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/** ***************************************************************
+	 */
+	public static void main (String[] args) {
+
+		//testParseProofStep();
+		//testParseProofFile();
+		testE();
+	}
 }
