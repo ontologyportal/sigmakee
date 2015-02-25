c16bab24eba53b98b8d9b1b4f3c6c064a2d3743f
qingqingcai <qingqingcai>
2015-02-25 21:52:21 +0000

    Test buildParents() and buildChildren() in KBcache;

--- test/integration/java/com/articulate/sigma/KBcacheTest.java
+++ test/integration/java/com/articulate/sigma/KBcacheTest.java
@@ -10,6 +10,48 @@ import static org.junit.Assert.assertEquals;
 public class KBcacheTest extends IntegrationTestBase {
 
     @Test
+    public void testbuildParents() {
+
+        KBcache cache = SigmaTestBase.kb.kbCache;
+
+        String child = "IrreflexiveRelation";
+        HashSet<String> expected = new HashSet<>(Arrays.asList("Entity", "Relation", "InheritableRelation", "Abstract", "BinaryRelation"));
+        HashSet<String> actual = cache.getParentClasses(child);
+        assertEquals(expected, actual);
+
+        child = "City";
+        expected = new HashSet<>(Arrays.asList("Entity", "Physical", "Object", "Region", "GeographicArea", "Agent", "GeopoliticalArea", "LandArea"));
+        actual = cache.getParentClasses(child);
+        assertEquals(expected, actual);
+
+        child = "AsymmetricRelation";
+        expected = new HashSet<>(Arrays.asList("Entity", "Abstract", "Relation", "InheritableRelation", "BinaryRelation", "AntisymmetricRelation", "IrreflexiveRelation"));
+        actual = cache.getParentClasses(child);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testbuildChildren() {
+
+        KBcache cache = SigmaTestBase.kb.kbCache;
+
+        String parent = "BiologicalAttribute";
+        HashSet<String> expected = new HashSet<>(Arrays.asList("AnimacyAttribute", "BodyPosition", "DevelopmentalAttribute",
+                "DiseaseOrSyndrome", "Fingerprint", "PsychologicalAttribute", "SexAttribute", "VisualAcuityAttribute",
+                "AquiredImmunoDeficiencySyndrome", "Disability", "PhysicalDisability", "SensoryDisability",
+                "InfectiousDisease", "BacterialDisease", "VaccinatableDisease", "Influenza", "TickBorneEncephalitis",
+                "ViralDisease", "EmotionalState", "HemorrhagicFever", "Hepatitis", "RiftValleyFever", "LiteracyAttribute",
+                "VenezuelanEquineEncephalitis", "ConsciousnessAttribute", "LifeThreateningDisease", "NonspecificDisease",
+                "PsychologicalDysfunction", "Neurosis", "Psychosis", "TyphoidFever", "StateOfMind", "TraitAttribute"));
+        HashSet<String> actual = cache.getChildClasses(parent);
+        assertEquals(expected, actual);
+        parent = "AsymmetricRelation";
+        expected = new HashSet<>(Arrays.asList("PropositionalAttitude", "CaseRole"));
+        actual = cache.getChildClasses(parent);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void testbuildTransInstOf() {
 
         KBcache cache = SigmaTestBase.kb.kbCache;
@@ -21,16 +63,14 @@ public class KBcacheTest extends IntegrationTestBase {
         assertEquals(expected, actual);
 
         relation = "during";
-        expected = new HashSet<>(Arrays.asList("Entity", "TransitiveRelation", "AntisymmetricRelation",
-                "Abstract", "Predicate", "TemporalRelation", "Relation", "InheritableRelation", "ReflexiveRelation",
-                "IrreflexiveRelation", "BinaryPredicate", "PartialOrderingRelation", "BinaryRelation", "SymmetricRelation"));
+        expected = new HashSet<>(Arrays.asList("Entity", "TransitiveRelation", "Abstract", "Relation", "InheritableRelation", "IrreflexiveRelation", "BinaryRelation"));
         actual = cache.getParentClassesOfInstance(relation);
         assertEquals(expected, actual);
 
         relation = "temporalPart";
         expected = new HashSet<>(Arrays.asList("Entity", "TransitiveRelation", "AntisymmetricRelation",
-                "Abstract", "Predicate", "TemporalRelation", "Relation", "InheritableRelation", "ReflexiveRelation",
-                "BinaryPredicate", "PartialOrderingRelation", "BinaryRelation"));
+                "Abstract", "TemporalRelation", "Relation", "InheritableRelation", "ReflexiveRelation",
+                "PartialOrderingRelation", "BinaryRelation", "BinaryPredicate", "Predicate"));
         actual = cache.getParentClassesOfInstance(relation);
         assertEquals(expected, actual);
     }
