184d508f3ba72fd08de63eaa024ad2278fc36fe7
apease <apease>
2015-03-09 18:14:42 +0000

    Added comment headers

--- src/java/com/articulate/sigma/nlp/pipeline/Pipeline.java
+++ src/java/com/articulate/sigma/nlp/pipeline/Pipeline.java
@@ -1,5 +1,26 @@
 package com.articulate.sigma.nlp.pipeline;
 
+/*
+Copyright 2014-2015 IPsoft
+
+Author: Andrei Holub andrei.holub@ipsoft.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program ; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+MA  02111-1307 USA 
+*/
+
 import edu.stanford.nlp.pipeline.*;
 
 import java.util.*;
@@ -8,7 +29,10 @@ public class Pipeline {
 
     final StanfordCoreNLP pipeline;
 
+    /** ***************************************************************
+     */
     public Pipeline() {
+        
         Properties props = new Properties();
         props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref, entitymentions");
         props.setProperty("parse.kbest", "2");
@@ -17,7 +41,10 @@ public class Pipeline {
         pipeline = new StanfordCoreNLP(props);
     }
 
+    /** ***************************************************************
+     */
     public Annotation annotate(String text) {
+        
         // create an empty Annotation just with the given text
         Annotation document = new Annotation(text);
 
