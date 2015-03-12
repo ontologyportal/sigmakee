a811b5a4ce49f944786ee9184b60937bacb3238f
apease <apease>
2015-03-12 14:39:29 +0000

    Fixed formatting.  Added imports and test output format.

--- test/corpus/java/com/articulate/sigma/semRewrite/InterpreterTest.java
+++ test/corpus/java/com/articulate/sigma/semRewrite/InterpreterTest.java
@@ -1,11 +1,16 @@
 package com.articulate.sigma.semRewrite;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import com.articulate.sigma.Formula;
 import com.articulate.sigma.IntegrationTestBase;
 import com.articulate.sigma.test.JsonReader;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -30,6 +35,7 @@ public class InterpreterTest extends IntegrationTestBase {
      */
     @BeforeClass
     public static void initInterpreter() {
+        
         interp = new Interpreter();
         KBmanager.getMgr().initializeOnce();
         interp.loadRules();
@@ -39,7 +45,9 @@ public class InterpreterTest extends IntegrationTestBase {
      */
     @Parameters(name="{0}")
     public static Collection<Object[]> prepare() {
-        return JsonReader.transform("resources/translation_tests.json", new Function<JSONObject, Object[]>() {
+        
+        //return JsonReader.transform("resources/translation_tiny.json", new Function<JSONObject, Object[]>() {
+            return JsonReader.transform("resources/translation_tests.json", new Function<JSONObject, Object[]>() {
             @Override
             public Object[] apply(JSONObject jo) {
                 String text = (String) jo.get("text");
@@ -51,7 +59,10 @@ public class InterpreterTest extends IntegrationTestBase {
         });
     }
 
+    /** ***************************************************************
+     */
     private String unify(String data) {
+        
         return data.replaceAll("\\n", "").replaceAll("\\s*\\)", ")").replaceAll("\\s*\\(", "(").trim();
     }
     
@@ -59,10 +70,25 @@ public class InterpreterTest extends IntegrationTestBase {
      */
     @Test
     public void test() {
+        
         String actual = interp.interpretSingle(fInput);
         // Just to have beautiful output
-        if(!Objects.equals(unify(fExpected), unify(actual))) {
-            assertEquals(fExpected, actual);
+        if (!Objects.equals(unify(fExpected), unify(actual))) {
+            boolean passed = (new Formula(fExpected)).logicallyEquals(new Formula(actual));
+            System.out.println("Input: " + fInput);
+            System.out.println("Expected: " + fExpected);
+            System.out.println("Actual: " + actual);
+            if (!passed)
+                System.out.println("****** FAIL ******");
+            else
+                System.out.println("pass");
+            if (passed) {
+                assertTrue("The following should be equal: \n" + fExpected + "\n and \n" + actual, passed);
+            } 
+            else {
+                assertFalse("The following should be equal: \n" + fExpected + "\n and \n" + actual, !passed);
+            }
+            //assertEquals(passed,true);
         }
     }
 }
