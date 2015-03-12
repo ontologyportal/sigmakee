bdb2b790dd83500ec37bff10a973205de97f0626
qingqingcai <qingqingcai>
2015-03-12 21:01:11 +0000

    Update inference unit test;

--- test/corpus/java/com/articulate/sigma/inference/InferenceTest.java
+++ test/corpus/java/com/articulate/sigma/inference/InferenceTest.java
@@ -38,20 +38,44 @@ public class InferenceTest {
     @Parameterized.Parameters(name="{0}")
     public static <T> Collection<T> prepare() {
 
-        return getTestFiles("test/corpus/java/resources/InferenceTestData");
+        String testDataDirectoryPath = "test/corpus/java/resources/InferenceTestData";
+        boolean enableIncludeTestsList = false;   // If enableIncludeTestsList=true, only run test files in includeTestsList
+        boolean enableExcludeTestsList = false;   // If enableIncludeTestsList=false & enableExcludeTestsList=true, only run test files NOT in excludeTestsLists
+                                                  // If enableIncludeTestsList=false & enableExcludeTestsList=false, run all test files in InferenceTestData
+        ArrayList<String> includeTestsList = Lists.newArrayList("QA1");
+        ArrayList<String> excludeTestsList = Lists.newArrayList("TQG2", "TQG4", "TQG10");
+        return getTestFiles(testDataDirectoryPath, includeTestsList, enableIncludeTestsList,
+                excludeTestsList, enableExcludeTestsList);
     }
 
     /** ***************************************************************
      */
-    public static <T> Collection<T> getTestFiles(String testDataDirectoryPath) {
+    public static <T> Collection<T> getTestFiles(String testDataDirectoryPath,
+                  ArrayList<String> includeTestsList, boolean enableIncludeTestsList,
+                  ArrayList<String> excludeTestsList, boolean enableExcludeTestsList) {
 
         Collection<T> result = Lists.newArrayList();
         File folder = new File(testDataDirectoryPath);
         try {
             for (File fileEntry : folder.listFiles()) {
                 if (fileEntry.getName().endsWith(".kif.tq")) {
-                    String path = fileEntry.getCanonicalPath();
-                    result.add((T) new Object[]{path});
+                    String filename = fileEntry.getName().substring(0, fileEntry.getName().indexOf(".kif.tq"));
+                    if (enableIncludeTestsList) {       // only consider files in includeTestsList
+                        if (includeTestsList.contains(filename)) {
+                            String path = fileEntry.getCanonicalPath();
+                            result.add((T) new Object[]{path});
+                        }
+                    }
+                    else if (enableExcludeTestsList) {  // only consider files NOT in excludeTestsList
+                        if (!excludeTestsList.contains(filename)) {
+                            String path = fileEntry.getCanonicalPath();
+                            result.add((T) new Object[]{path});
+                        }
+                    }
+                    else {                              // consider all files in InferenceTestData directory
+                        String path = fileEntry.getCanonicalPath();
+                        result.add((T) new Object[]{path});
+                    }
                 }
             }
         } catch (IOException e) {
@@ -70,6 +94,7 @@ public class InferenceTest {
         ArrayList<String> expectedAnswers = new ArrayList<>();
         ArrayList<String> actualAnswers = new ArrayList<>();
         InferenceTestSuite.inferenceUnitTest(fInput, kb, expectedAnswers, actualAnswers);
+
         assertEquals(expectedAnswers, actualAnswers);
     }
 }
