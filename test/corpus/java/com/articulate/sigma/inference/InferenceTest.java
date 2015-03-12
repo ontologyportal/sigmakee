2a9a72b6817d46db41b6879d745295e128352795
qingqingcai <qingqingcai>
2015-03-12 14:07:52 +0000

    update InferenceTest using Parameterized.Parameter;

--- test/corpus/java/com/articulate/sigma/inference/InferenceTest.java
+++ test/corpus/java/com/articulate/sigma/inference/InferenceTest.java
@@ -3,20 +3,29 @@ package com.articulate.sigma.inference;
 import com.articulate.sigma.InferenceTestSuite;
 import com.articulate.sigma.KB;
 import com.articulate.sigma.KBmanager;
+import com.google.common.collect.Lists;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by qingqingcai on 3/9/15.
- */
+@RunWith(Parameterized.class)
 public class InferenceTest {
 
     private static KB kb;
 
+    @Parameterized.Parameter(value= 0)
+    public String fInput;
+
+    /** ***************************************************************
+     */
     @BeforeClass
     public static void setKB() {
 
@@ -24,40 +33,43 @@ public class InferenceTest {
         kb = KBmanager.getMgr().getKB("SUMO");
     }
 
-    @Test
-    public void testTQG1() {
-        String testpath = "test/corpus/java/resources/InferenceTestData/TQG1.kif.tq";
-        ArrayList<String> expectedAnswers = new ArrayList<>();
-        ArrayList<String> actualAnswers = new ArrayList<>();
-        System.out.println(kb.eprover);
-        InferenceTestSuite.inferenceUnitTest(testpath, kb, expectedAnswers, actualAnswers);
-        assertEquals(expectedAnswers, actualAnswers);
+    /** ***************************************************************
+     */
+    @Parameterized.Parameters(name="{0}")
+    public static <T> Collection<T> prepare() {
+
+        return getTestFiles("test/corpus/java/resources/InferenceTestData");
     }
 
-    @Test
-    public void testTQG5() {
-        String testpath = "test/corpus/java/resources/InferenceTestData/TQG5.kif.tq";
-        ArrayList<String> expectedAnswers = new ArrayList<>();
-        ArrayList<String> actualAnswers = new ArrayList<>();
-        InferenceTestSuite.inferenceUnitTest(testpath, kb, expectedAnswers, actualAnswers);
-        assertEquals(expectedAnswers, actualAnswers);
+    /** ***************************************************************
+     */
+    public static <T> Collection<T> getTestFiles(String testDataDirectoryPath) {
+
+        Collection<T> result = Lists.newArrayList();
+        File folder = new File(testDataDirectoryPath);
+        try {
+            for (File fileEntry : folder.listFiles()) {
+                if (fileEntry.getName().endsWith(".kif.tq")) {
+                    String path = fileEntry.getCanonicalPath();
+                    result.add((T) new Object[]{path});
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
+    /** ***************************************************************
+     */
     @Test
-    public void testTQG8() {
-        String testpath = "test/corpus/java/resources/InferenceTestData/TQG8.kif.tq";
-        ArrayList<String> expectedAnswers = new ArrayList<>();
-        ArrayList<String> actualAnswers = new ArrayList<>();
-        InferenceTestSuite.inferenceUnitTest(testpath, kb, expectedAnswers, actualAnswers);
-        assertEquals(expectedAnswers, actualAnswers);
-    }
+    public void test() {
 
-    @Test
-    public void testTQG9() {
-        String testpath = "test/corpus/java/resources/InferenceTestData/TQG9.kif.tq";
+        System.out.println(fInput);
         ArrayList<String> expectedAnswers = new ArrayList<>();
         ArrayList<String> actualAnswers = new ArrayList<>();
-        InferenceTestSuite.inferenceUnitTest(testpath, kb, expectedAnswers, actualAnswers);
+        InferenceTestSuite.inferenceUnitTest(fInput, kb, expectedAnswers, actualAnswers);
         assertEquals(expectedAnswers, actualAnswers);
     }
 }
