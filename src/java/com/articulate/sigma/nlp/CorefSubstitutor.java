68b0375eee917760acf67ce82ccc9427fb9aaae7
andrei_holub <andrei_holub>
2015-03-16 20:53:30 +0000

    fix sources headers

--- src/java/com/articulate/sigma/nlp/CorefSubstitutor.java
+++ src/java/com/articulate/sigma/nlp/CorefSubstitutor.java
@@ -1,5 +1,26 @@
 package com.articulate.sigma.nlp;
 
+/*
+Copyright 2014-2015 IPsoft
+
+Author: Andrei Holub andrei.holub@ipsoft.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program ; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+MA  02111-1307 USA
+*/
+
 import com.articulate.sigma.nlp.pipeline.Pipeline;
 import com.google.common.collect.Lists;
 import edu.stanford.nlp.dcoref.CorefChain;
@@ -13,9 +34,6 @@ import edu.stanford.nlp.util.CoreMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by aholub on 3/9/15.
- */
 public class CorefSubstitutor {
 
     final Annotation document;
--- test/corpus/java/com/articulate/sigma/nlp/pipeline/CorefParam.java
+++ test/corpus/java/com/articulate/sigma/nlp/pipeline/CorefParam.java
@@ -1,10 +1,28 @@
 package com.articulate.sigma.nlp.pipeline;
 
+/*
+Copyright 2014-2015 IPsoft
+
+Author: Andrei Holub andrei.holub@ipsoft.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program ; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+MA  02111-1307 USA
+*/
+
 import edu.stanford.nlp.dcoref.CorefChain;
 
-/**
- * Created by aholub on 3/9/15.
- */
 public class CorefParam {
     static final String NO_VALUE = "N/A";
     static final Long NO_INDEX = Long.MIN_VALUE;
