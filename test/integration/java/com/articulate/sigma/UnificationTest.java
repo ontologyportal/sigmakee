5ee8bf2b5d031490cdce8905bef8d6887ac887d3
qingqingcai <qingqingcai>
2015-04-09 14:45:09 +0000

    add test examples.

--- test/integration/java/com/articulate/sigma/UnificationTest.java
+++ test/integration/java/com/articulate/sigma/UnificationTest.java
@@ -120,6 +120,8 @@ public class UnificationTest extends IntegrationTestBase {
     }
 
     /** *************************************************************
+     * When was Amelia Mary Earhart born?
+     * advmod(bear*,when-1), auxpass(bear*,be*), nsubjpass(bear*,?HUM), sumo(?C,bear*) ==> {(birthdate ?HUM ?WH)}.
      */
     @Test
     public void testWhenWasAmeliaMaryEarhartBorn() {
@@ -146,6 +148,8 @@ public class UnificationTest extends IntegrationTestBase {
     }
 
     /** *************************************************************
+     * When did Amelia Mary Earhart die?
+     * advmod(die*,when-1), nsubj(die*,?HUM), sumo(?C,die*), sumo(?C2,do*) ==> {(deathdate ?HUM ?WH)}.
      */
     @Test
     public void testWhenDidAmeliaMaryEarhartDie() {
@@ -172,6 +176,8 @@ public class UnificationTest extends IntegrationTestBase {
     }
 
     /** *************************************************************
+     * What was Amelia Mary Earhart interested in?
+     * prep(interested*,in*), sumo(?C,interested*), nsubj(?R,?S), cop(interested*,be*) ==> {(inScopeOfInterest ?S ?O)}.
      */
     @Test
     public void testWhatWasAmeliaMaryEarhartInterestedIn() {
@@ -198,6 +204,8 @@ public class UnificationTest extends IntegrationTestBase {
     }
 
     /** *************************************************************
+     * Was Amelia Mary Earhart interested in airplanes?
+     * prep_in(?R,?O), sumo(inScopeOfInterest,interested*), nsubj(?R,?S), cop(interested*,be*) ==> {(inScopeOfInterest ?S ?O)}.
      */
     @Test
     public void testWasAmeliaMaryEarhartInterestedInAirplanes() {
@@ -218,6 +226,66 @@ public class UnificationTest extends IntegrationTestBase {
                 "(names AmeliaMaryEarhart-2 \"Amelia Mary Earhart\")",
                 "(instance airplane-7 Airplane)",
                 "(instance AmeliaMaryEarhart-2 Human)"
+        };
+
+        List<String> results = interp.interpretCNF(inputs);
+        assertThat(results, hasItems(expected));
+    }
+
+    /** *************************************************************
+     * Where did she fly?
+     * advmod(?X,where*), aux(?X,do*) ==> {(traverses ?X ?WH)}.
+     */
+    @Test
+    public void testWhereDidSheFly() {
+
+        Interpreter interp = new Interpreter();
+        interp.initialize();
+
+        String input = "root(ROOT-0,fly-6), advmod(fly-6,where-1), aux(fly-6,do-2), nsubj(fly-6,AmeliaMaryEarhart-3), attribute(AmeliaMaryEarhart-3,Female), sumo(IntentionalProcess,do-2), names(AmeliaMaryEarhart-3,\"Amelia Mary Earhart\"), sumo(Human,AmeliaMaryEarhart-3), sumo(Flying,fly-6), number(SINGULAR,Amelia-3), number(SINGULAR,Mary-4), number(SINGULAR,Earhart-5)";
+        Lexer lex = new Lexer(input);
+        CNF cnfInput = CNF.parseSimple(lex);
+        ArrayList<CNF> inputs = new ArrayList<CNF>();
+        inputs.add(cnfInput);
+
+        // Where did she fly?
+        String[] expected = {
+                "(traverses fly-6 ?WH)",
+                "(agent fly-6 AmeliaMaryEarhart-3)",
+                "(attribute AmeliaMaryEarhart-3 Female)",
+                "(names AmeliaMaryEarhart-3 \"Amelia Mary Earhart\")",
+                "(instance AmeliaMaryEarhart-3 Human)",
+                "(instance fly-6 Flying)"
+        };
+
+        List<String> results = interp.interpretCNF(inputs);
+        assertThat(results, hasItems(expected));
+    }
+
+    /** *************************************************************
+     * What language did Amelia speak?
+     * det(?L,what*), dobj(?S,?L), sumo(?LT,?L), isSubclass(?LT,Physical), +sumo(?ST,?S), isSubclass(?ST,Process) ==> {(patient ?S ?WH)}.
+     */
+    @Test
+    public void testWhatLanguageDidAmeliaSpeak() {
+
+        Interpreter interp = new Interpreter();
+        interp.initialize();
+
+        String input = "root(ROOT-0,speak-7), det(language-2,what-1), dobj(speak-7,language-2), aux(speak-7,do-3), nsubj(speak-7,AmeliaMaryEarhart-4), sumo(Speaking,speak-7), sumo(Language,language-2), sumo(IntentionalProcess,do-3), sumo(Human,AmeliaMaryEarhart-4), names(AmeliaMaryEarhart-4,\"Amelia Mary Earhart\"), attribute(AmeliaMaryEarhart-4,Female), number(SINGULAR,language-2), number(SINGULAR,Amelia-4), number(SINGULAR,Mary-5), number(SINGULAR,Earhart-6)";
+        Lexer lex = new Lexer(input);
+        CNF cnfInput = CNF.parseSimple(lex);
+        ArrayList<CNF> inputs = new ArrayList<CNF>();
+        inputs.add(cnfInput);
+
+        // Where did she fly?
+        String[] expected = {
+                "(patient speak-7 ?WH)",
+                "(agent speak-7 AmeliaMaryEarhart-4)",
+                "(attribute AmeliaMaryEarhart-4 Female)",
+                "(names AmeliaMaryEarhart-4 \"Amelia Mary Earhart\")",
+                "(instance AmeliaMaryEarhart-4 Human)",
+                "(instance speak-7 Speaking)"
         };
 
         List<String> results = interp.interpretCNF(inputs);
