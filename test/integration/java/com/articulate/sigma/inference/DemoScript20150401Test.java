cad4b0a8bd4ab4ef2ba376056c6db2c8b71c6152
jkurlandski01 <jkurlandski01>
2015-04-21 13:02:01 +0000

    Fix broken test.

--- test/integration/java/com/articulate/sigma/inference/DemoScript20150401Test.java
+++ test/integration/java/com/articulate/sigma/inference/DemoScript20150401Test.java
@@ -73,8 +73,8 @@ public class DemoScript20150401Test extends IntegrationTestBase {
 
         input = "Where did she disappear?";
         actualAnswer = interpreter.interpret(input).get(0);
-        // FIXME: at one time we got this more precise answer; assertEquals("SouthPacificOcean.", actualAnswer);
-        assertEquals("PacificOcean.", actualAnswer);
+        assertEquals("SouthPacificOcean.", actualAnswer);
+        //assertEquals("PacificOcean.", actualAnswer);
 
         input = "When did she disappear?";
         actualAnswer = interpreter.interpret(input).get(0);
