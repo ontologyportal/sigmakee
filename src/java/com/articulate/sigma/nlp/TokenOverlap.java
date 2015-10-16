3353e3a9e98a8fb4aff801e32471341156148f9d
apease <adam.pease@ipsoft.com>
2015-10-16 16:30:09 -0400

    Remove obsolete methods now in NGramOverlap

--- src/java/com/articulate/sigma/nlp/TokenOverlap.java
+++ src/java/com/articulate/sigma/nlp/TokenOverlap.java
@@ -23,62 +23,6 @@ public class TokenOverlap {
     }
 
     /** ***************************************************************
-     * @return an integer score of the number of shared ngrams (minus
-     * stopwords and punctuation)
-     */
-    public int nGramOverlap(String x, String y, int n) {
-
-        //System.out.println("TokenOverlap.overlap(): testing: " + x + " \nand:\n" + y);
-        int overlap = 0;
-        String str1 = tfidf.removePunctuation(x);
-        str1 = tfidf.removeStopWords(str1);
-        ArrayList<String> s1 = new ArrayList<String>();
-        String[] sspl = str1.split(" ");
-        s1.addAll(Arrays.asList(sspl));
-        String str2 = tfidf.removePunctuation(y);
-        str2 = tfidf.removeStopWords(str2);
-        ArrayList<String> s2 = new ArrayList<String>();
-        s2.addAll(Arrays.asList(str2.split(" ")));
-
-        for (int i = 0; i < s1.size() - n; i++) {
-            StringBuffer s1tok = new StringBuffer();
-            for (int z = 0; z < n; z++)
-                s1tok.append(s1.get(i + z));
-            for (int j = 0; j < s2.size() - n; j++) {
-                StringBuffer s2tok = new StringBuffer();
-                for (int z = 0; z < n; z++)
-                    s2tok.append(s2.get(j+z));
-                if (s1tok.equals(s2tok)) {
-                    overlap++;
-                    System.out.println("TokenOverlap.nGramOverlap(): match: " + s1tok);
-                }
-            }
-        }
-        //if (s1.size() > 0)
-        //    System.out.println("TokenOverlap.overlap(): common tokens: " + s1);
-        return overlap;
-    }
-
-    /** ***************************************************************
-     * @return an integer score of the number of shared ngrams (minus
-     * stopwords and punctuation)
-     */
-    public int cachedNGramOverlap(HashMap<Integer,HashSet<String>> questions,
-                                  HashMap<Integer,HashSet<String>> answers, int n) {
-
-        //System.out.println("TokenOverlap.cachedNGramOverlap():  " + questions + " \nand:\n" + answers);
-        HashSet<String> qs = questions.get(n);
-        if (qs == null) return 0;
-        HashSet<String> as = answers.get(n);
-        HashSet<String> result = new HashSet<>(qs);
-        result.retainAll(as);
-        int overlap = result.size();
-        if (debug)
-            System.out.println("TokenOverlap.cachedNGramOverlap(): common tokens: " + result);
-        return overlap;
-    }
-
-    /** ***************************************************************
      * @return an integer score of the number of shared tokens (minus
      * stopwords and punctuation)
      */
@@ -149,33 +93,6 @@ public class TokenOverlap {
      * @return a map of scores and the set of document IDs that have that
      * score, which is a count of token overlap with the question
      */
-    public TreeMap<Float,ArrayList<Integer>> computeNGramOverlap(String question) {
-
-        ProgressPrinter pp = new ProgressPrinter(10);
-        System.out.print("TokenOverlap.computerNGramOverlap(): ");
-        TreeMap<Float,ArrayList<Integer>> result = new TreeMap<>();
-        for (String line : tfidf.lines) {
-            pp.tick();
-            //if (tfidf.lines.indexOf(line) == 8362)
-            //    System.out.println("TokenOverlap.computeOverlap(): " + line);
-            int score = nGramOverlap(question, line, 2);
-            if (score == 0)
-                continue;
-            float fscore = (float) score;
-            ArrayList<Integer> al = new ArrayList<Integer>();
-            if (result.containsKey(fscore))
-                al = result.get(fscore);
-            al.add(tfidf.lines.indexOf(line));
-            result.put(fscore,al);
-        }
-        System.out.println();
-        return result;
-    }
-
-    /** ***************************************************************
-     * @return a map of scores and the set of document IDs that have that
-     * score, which is a count of token overlap with the question
-     */
     public TreeMap<Float,ArrayList<Integer>> rank(String question,
                                                   List<String> toScoreIDs,
                                                   TreeMap<Float,ArrayList<Integer>> scoredIDs) {
@@ -198,36 +115,6 @@ public class TokenOverlap {
             result.put(fscore,al);
         }
 
-        return result;
-    }
-
-    /** ***************************************************************
-     * @return a map of scores and the set of document IDs that have that
-     * score, which is a count of token overlap with the question
-     */
-    public TreeMap<Float,ArrayList<Integer>> nGramRank(ShuZiInsQA.Dev dev,
-                                                  List<String> toScoreIDs,
-                                                  ArrayList<HashMap<Integer,HashSet<String>>> answerNgrams,
-                                                  TreeMap<Float,ArrayList<Integer>> scoredIDs) {
-
-        TreeMap<Float,ArrayList<Integer>> result = new TreeMap<>();
-        result.putAll(scoredIDs);
-        for (String id : toScoreIDs) {
-            int intID = Integer.parseInt(id);
-            int score = cachedNGramOverlap(dev.questionNgrams, answerNgrams.get(intID), 2);
-            if (debug)
-                System.out.println("TokenOverlap.nGramRank(): id: " + id + " as int: " + intID + " with score: " + score);
-            if (score == 0)
-                continue;
-            float fscore = (float) score;
-            ArrayList<Integer> al = new ArrayList<Integer>();
-            if (result.containsKey(fscore))
-                al = result.get(fscore);
-            al.add(intID);
-            result.put(fscore,al);
-        }
-        if (debug)
-            System.out.println("TokenOverlap.nGramRank(): result " + result);
         return result;
     }
 
