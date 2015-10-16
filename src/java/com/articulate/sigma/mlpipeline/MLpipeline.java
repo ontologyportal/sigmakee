ad4f9fb506caaaea44320a526e46b1f204fa184f
apease <adam.pease@ipsoft.com>
2015-10-16 16:28:21 -0400

    Start creating a shell for generalized processing steps

--- src/java/com/articulate/sigma/mlpipeline/MLpipeline.java
+++ src/java/com/articulate/sigma/mlpipeline/MLpipeline.java
@@ -1,5 +1,8 @@
 package com.articulate.sigma.mlpipeline;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.IntStream;
@@ -17,118 +20,40 @@ public class MLpipeline {
         ArrayList<Map<String,String>> tokens = new ArrayList<>();
     }
 
-    public static void pipeline() {
+    public void readPreambles() { // vocab, stopwords etc
+        // read vocab
+        // read stopwords
+    }
+
+    public void readTraining() {  // read and generate features from training file
 
     }
 
-    public static void main(String[] args) {
+    // read a file of features
+    public void processHeaderLine(String s) { // put column names and types into collection
 
-        List<String> myList =
-                Arrays.asList("a1", "a2", "b1", "c2", "c1");
+    }
 
-        myList
-                .stream()
-                .filter(s -> s.startsWith("a"))
-                .map(String::toUpperCase)
-                .sorted()
-                .forEach(System.out::println);
-        System.out.println();
-// C1
-// C2
+    public void processDataLine(String s) { // put column names and types into collection, assuming conformance to header order
 
-        Arrays.asList("a1", "a2", "a3")
-                .stream()
-                .findFirst()
-                .ifPresent(System.out::println);  // a1
-        System.out.println();
+    }
 
-        Stream.of("a1", "a2", "a3")
-                .findFirst()
-                .ifPresent(System.out::println);  // a1
-        System.out.println();
+    public void pipeline() {
 
-        IntStream.range(1, 4)
-                .forEach(System.out::println);
-        System.out.println();
-
-        Arrays.stream(new int[] {1, 2, 3})
-                .map(n -> 2 * n + 1)
-                .average()
-                .ifPresent(System.out::println);  // 5.0
-        System.out.println();
-
-        Stream.of("a1", "a2", "a3")
-                .map(s -> s.substring(1))
-                .mapToInt(Integer::parseInt)
-                .max()
-                .ifPresent(System.out::println);  // 3
-        System.out.println();
-
-        Stream.of("d2", "a2", "b1", "b3", "c")
-                .filter(s -> {
-                    System.out.println("filter: " + s);
-                    return true;
-                })
-                .forEach(s -> System.out.println("forEach: " + s));
-        System.out.println();
-
-        Stream.of("d2", "a2", "b1", "b3", "c")
-                .map(s -> {
-                    System.out.println("map: " + s);
-                    return s.toUpperCase();
-                })
-                .anyMatch(s -> {
-                    System.out.println("anyMatch: " + s);
-                    return s.startsWith("B");
-                });
-        System.out.println();
-
-        Stream.of("d2", "a2", "b1", "b3", "c")
-                .filter(s -> s.startsWith("a"))
-                .forEach(s -> System.out.println("forEach: " + s));
-        System.out.println();
-
-        class Person {
-            String name;
-            int age;
-
-            Person(String name, int age) {
-                this.name = name;
-                this.age = age;
-            }
-
-            @Override
-            public String toString() {
-                return name;
-            }
+        readPreambles();
+        readTraining();
+        try {
+            Files.lines(Paths.get(".", "data.txt"))
+                    .forEach(s -> {if (s.startsWith("#")) processHeaderLine(s); else processDataLine(s); return;});
         }
+        catch (IOException ioe) {
+            System.out.println("Error in MLpipeline.pipeline()");
+            System.out.println(ioe.getMessage());
+            ioe.printStackTrace();
+        }
+    }
 
-        List<Person> persons =
-                Arrays.asList(
-                        new Person("Max", 18),
-                        new Person("Peter", 23),
-                        new Person("Pamela", 23),
-                        new Person("David", 12));
-
-        Collector<Person, StringJoiner, String> personNameCollector =
-                Collector.of(
-                        () -> new StringJoiner(" | "),          // supplier
-                        (j, p) -> j.add(p.name.toUpperCase()),  // accumulator
-                        (j1, j2) -> j1.merge(j2),               // combiner
-                        StringJoiner::toString);                // finisher
-
-        String names = persons
-                .stream()
-                .collect(personNameCollector);
-
-        System.out.println(names);  // MAX | PETER | PAMELA | DAVID
-        System.out.println();
-
-        persons
-                .stream()
-                .reduce((p1, p2) -> p1.age > p2.age ? p1 : p2)
-                .ifPresent(System.out::println);    // Pamela
-        System.out.println();
+    public static void main(String[] args) {
 
     }
 }
