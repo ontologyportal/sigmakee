e8c76e13c84a1bd4950fb025f0ce1290e9b72315
Adam Pease <apease@articulatesoftware.com>
2016-06-12 18:47:38 -0700

    Added debugging statements, unit test

--- src/java/com/articulate/sigma/semRewrite/substitutor/SubstitutionUtil.java
+++ src/java/com/articulate/sigma/semRewrite/substitutor/SubstitutionUtil.java
@@ -21,11 +21,12 @@
 
 package com.articulate.sigma.semRewrite.substitutor;
 
+import com.articulate.sigma.semRewrite.Clause;
 import com.google.common.collect.Lists;
+import edu.stanford.nlp.ling.CoreLabel;
+import sun.java2d.pipe.SpanShapeRenderer;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -37,6 +38,7 @@ public class SubstitutionUtil {
     // predicate(word-1, word)
     public static final Pattern CLAUSE_SPLITTER = Pattern.compile("([^\\(]+)\\((.+?(-\\d+)?),\\s*(.+?(-\\d+)?)\\)");
     public static final Pattern CLAUSE_PARAM = Pattern.compile("(.+)-(\\d+)");
+    public static final boolean debug = false;
 
     /** **************************************************************
      * Takes the list of clauses and substitutes them using provided substitutor.
@@ -45,26 +47,26 @@ public class SubstitutionUtil {
      */
     public static List<String> groupClauses(ClauseSubstitutor substitutor, List<String> clauses) {
 
-        //System.out.println("INFO in SubstitutionUtil.groupClauses(): clauses: " + clauses);
-        //System.out.println("INFO in SubstitutionUtil.groupClauses(): substitutor: " + substitutor);
+        if (debug) System.out.println("INFO in SubstitutionUtil.groupClauses(): clauses: " + clauses);
+        if (debug)  System.out.println("INFO in SubstitutionUtil.groupClauses(): substitutor: " + substitutor);
         Iterator<String> clauseIterator = clauses.iterator();
         List<String> modifiedClauses = Lists.newArrayList();
         while (clauseIterator.hasNext()) {
             String clause = clauseIterator.next();
-            //System.out.println("INFO in SubstitutionUtil.groupClauses(): clause: " + clause);
+            if (debug) System.out.println("INFO in SubstitutionUtil.groupClauses(): clause: " + clause);
             Matcher m = CLAUSE_SPLITTER.matcher(clause);
             if (m.matches()) {
                 // FIXME LOW: Still waiting for optimization
                 String attr1 = m.group(2);
                 String attr2 = m.group(4);
-                //System.out.println("INFO in SubstitutionUtil.groupClauses(): attr1: " + attr1);
-                //System.out.println("INFO in SubstitutionUtil.groupClauses(): attr2: " + attr2);
+                if (debug) System.out.println("INFO in SubstitutionUtil.groupClauses(): attr1: " + attr1);
+                if (debug) System.out.println("INFO in SubstitutionUtil.groupClauses(): attr2: " + attr2);
                 if ((m.group(3) != null && substitutor.containsKey(attr1.toUpperCase()))
                         || (m.group(5) != null && substitutor.containsKey(attr2.toUpperCase()))) {
                     CoreLabelSequence attr1Grouped = substitutor.getGrouped(attr1.toUpperCase());
                     CoreLabelSequence attr2Grouped = substitutor.getGrouped(attr2.toUpperCase());
-                    //System.out.println("INFO in SubstitutionUtil.groupClauses(): attr1Grouped: " + attr1Grouped);
-                    //System.out.println("INFO in SubstitutionUtil.groupClauses(): attr2Grouped: " + attr2Grouped);
+                    if (debug) System.out.println("INFO in SubstitutionUtil.groupClauses(): attr1Grouped: " + attr1Grouped);
+                    if (debug) System.out.println("INFO in SubstitutionUtil.groupClauses(): attr2Grouped: " + attr2Grouped);
                    // if (attr1Grouped.toString() != null && attr2Grouped != null &&
                     if (!Objects.equals(attr1Grouped, attr2Grouped)) {
                             // delete clauses like amod() and nn() that have parts of a compound noun as args
@@ -77,7 +79,7 @@ public class SubstitutionUtil {
                         if (attr2Grouped != null)
                             arg2 = attr2Grouped.toStringWithNumToken();
                         String modClause = label + "(" + arg1  + "," + arg2 + ")";
-                        //System.out.println("INFO in SubstitutionUtil.groupClauses(): modClause: " + modClause);
+                        if (debug) System.out.println("INFO in SubstitutionUtil.groupClauses(): modClause: " + modClause);
                         modifiedClauses.add(modClause);
                     }
                 }
@@ -88,8 +90,51 @@ public class SubstitutionUtil {
                 System.out.println("Error in SubstitutionUtil.groupClauses(): unmatched clause: " + clause);
         }
 
-        //System.out.println("INFO in SubstitutionUtil.groupClauses(2): " + modifiedClauses);
+        if (debug) System.out.println("INFO in SubstitutionUtil.groupClauses(2): " + modifiedClauses);
         clauses.addAll(modifiedClauses);
         return modifiedClauses;
+    }
+
+    /** **************************************************************
+     */
+    public static void test() {
+
+        CoreLabel cl1 = new CoreLabel();
+        cl1.setValue("C.");
+        cl1.setIndex(4);
+        List<CoreLabel>  lcl = new ArrayList<>();
+        List<CoreLabel>  lcl2 = new ArrayList<>();
+        lcl.add(cl1);
+        lcl2.add(cl1);
+        CoreLabel cl2 = new CoreLabel();
+        cl2.setValue("S.");
+        cl2.setIndex(5);
+        lcl.add(cl2);
+        lcl2.add(cl2);
+        CoreLabel cl3 = new CoreLabel();
+        cl3.setValue("Lewis".toUpperCase());
+        cl3.setIndex(6);
+        lcl.add(cl3);
+        cl3 = new CoreLabel();
+        cl3.setValue("Lewis");
+        cl3.setIndex(6);
+        lcl2.add(cl3);
+        CoreLabelSequence cls = new CoreLabelSequence(lcl);
+        CoreLabelSequence cls2 = new CoreLabelSequence(lcl2);
+        //subst =  [{[C.-4, S.-5, Lewis-6]=[C.-4, S.-5, Lewis-6]}, {}];
+        String clause = "number(SINGULAR, Lewis-6)";
+        List<String> clauses = new ArrayList<>();
+        clauses.add(clause);
+        SimpleSubstitutorStorage sss = new SimpleSubstitutorStorage();
+        Map<CoreLabelSequence,CoreLabelSequence> m = new HashMap<>();
+        m.put(cls,cls2);
+        sss.addGroups(m);
+        System.out.println("SubstitutionUtil.test(): " + groupClauses(sss,clauses));
+    }
+
+    /****************************************************************
+     */
+    public static void main(String[] args) {
+        test();
     }
 }
