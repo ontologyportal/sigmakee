3efa67c62bc1bf2c9b192706ac68f29483d78b99
apease <apease@articulatesoftware.com>
2017-05-20 14:07:27 -0700

    Added a jUnit "suite" class to call all tests, which makes it possible to run jUnit from the command line

---
+++ test/unit/java/com/articulate/sigma/UnitTestSuite.java
@@ -0,0 +1,36 @@
+package com.articulate.sigma;
+
+import com.google.common.collect.Sets;
+import org.junit.Test;
+import com.articulate.sigma.nlg.UnitNLGTestSuite;
+import com.articulate.sigma.wordNet.MultiWordsTest;
+import java.util.ArrayList;
+import java.util.Set;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import static org.junit.Assert.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        FormulaArityCheckTest.class,
+        FormulaDeepEqualsTest.class,
+        FormulaLogicalEqualityTest.class,
+        FormulaPreprocessorComputeVariableTypesTest.class,
+        FormulaPreprocessorFindExplicitTypesTest.class,
+        FormulaPreprocessorTest.class,
+        FormulaTest.class,
+        FormulaUnificationTest.class,
+        KBcacheUnitTest.class,
+        KBmanagerInitTest.class,
+        PredVarInstTest.class,
+        StringUtilTest.class,
+        SUMOformulaToTPTPformulaTest.class,
+        MultiWordsTest.class,
+        WordNetTest.class,
+        KBTest.class,
+        UnitNLGTestSuite.class,
+})
+public class UnitTestSuite extends UnitTestBase {
+
+}
