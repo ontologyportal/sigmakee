fc9d31f0f40282e46c2dfb033deee0007f8f8aba
apease <apease@articulatesoftware.com>
2017-05-20 14:06:04 -0700

    made resource path relative to SIGMA_SRC. Since it's reading from the file system rather than from within an IDE like IntelliJ, reading has to be done from a FileReader and not a StringReader.  Also formatting changes

--- test/unit/java/com/articulate/sigma/SigmaTestBase.java
+++ test/unit/java/com/articulate/sigma/SigmaTestBase.java
@@ -10,6 +10,7 @@ import java.util.Collection;
 import java.util.List;
 
 public class SigmaTestBase {
+
     static final String SIGMA_HOME = System.getenv("SIGMA_HOME");
     protected static final String KB_PATH = (new File(SIGMA_HOME, "KBs")).getAbsolutePath();
 
@@ -19,16 +20,17 @@ public class SigmaTestBase {
      * Performs the KB load.
      * @param reader
      */
-    protected static void doSetUp(BufferedReader reader)    {
+    protected static void doSetUp(BufferedReader reader) {
+
         KBmanager manager = KBmanager.getMgr();
-
         SimpleElement configuration = null;
-
-        if(! manager.initialized) {
+        if (! manager.initialized) {
             try {
                 SimpleDOMParser sdp = new SimpleDOMParser();
+                //sdp.setSkipProlog(false);
                 configuration = sdp.parse(reader);
-            } catch (IOException e) {
+            }
+            catch (IOException e) {
                 e.printStackTrace();
             }
 
@@ -37,31 +39,30 @@ public class SigmaTestBase {
             manager.initialized = true;
             manager.initializing = false;
         }
-
         kb = KBmanager.getMgr().getKB("SUMO");
-
         checkConfiguration();
     }
 
-    /****************************************************************/
+    /***************************************************************
+     * */
     protected static void checkConfiguration() {
+
         List<String> problemList = Lists.newArrayList();
-        if(NLGUtils.getKeywordMap() == null || NLGUtils.getKeywordMap().isEmpty()) {
+        if (NLGUtils.getKeywordMap() == null || NLGUtils.getKeywordMap().isEmpty()) {
             problemList.add("LanguageFormatter.keywordMap is empty.");
         }
-        if(WordNet.wn.synsetsToWords.isEmpty()) {
+        if (WordNet.wn.synsetsToWords.isEmpty()) {
             problemList.add("WordNet mappings are empty.");
         }
 
         if (! problemList.isEmpty()) {
             StringBuilder sBuild = new StringBuilder();
-            for (String problem : problemList)  {
+            for (String problem : problemList) {
                 final String NEWLINE_AND_SPACES = "\n   ";
                 sBuild.append(NEWLINE_AND_SPACES).append(problem);
             }
             throw new IllegalStateException("Configuration failed. Problems:" + sBuild.toString());
         }
-
     }
 
     /****************************************************************
@@ -71,33 +72,39 @@ public class SigmaTestBase {
      * @return
      */
     protected static BufferedReader getXmlReader(String path, Class theClass)  {
+
         BufferedReader xmlReader = null;
         try {
-            URI uri = theClass.getClassLoader().getResource(path).toURI();
+            //URI uri = theClass.getClassLoader().getResource(path).toURI();
             //URI uri = theClass.getResource(path).toURI();
-            File configFile = new File(uri);
-            String contents = StringUtil.getContents(configFile);
-            contents = contents.replaceAll("\\$SIGMA_HOME", SIGMA_HOME);
-            xmlReader = new BufferedReader(new StringReader(contents));
-
+            //File configFile = new File(uri);
+           // String contents = StringUtil.getContents(configFile);
+            //contents = contents.replaceAll("\\$SIGMA_HOME", SIGMA_HOME);
+            //xmlReader = new BufferedReader(new StringReader(path));
+            xmlReader = new BufferedReader(new FileReader(path));
             //xmlReader = new BufferedReader(new InputStreamReader(theClass.getResourceAsStream(path)));
         }
         catch (Exception ex)  {
-            try {
-                URI uri = theClass.getClassLoader().getResource(".").toURI();
+            //try {
+                //URI uri = theClass.getClassLoader().getResource(".").toURI();
                 //URI uri = theClass.getResource(".").toURI();
-                String msg = "Could not find " + path + " in " + uri.toString();
-                throw new IllegalStateException(msg);
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
-            }
-
+                //String msg = "Could not find " + path + " in " + uri.toString();
+                ex.printStackTrace();
+                System.out.println(ex.getMessage());
+                System.out.println("SigmaTestBase.getXmlReader(): Could not find " + path);
+                //throw new IllegalStateException(msg);
+            //}
+            //catch (URISyntaxException e) {
+            //    e.printStackTrace();
+            //}
         }
         return xmlReader;
     }
 
-    /****************************************************************/
+    /***************************************************************
+     * */
     public static <T> void displayCollectionStringDiffs(Collection<T> coll1, Collection<T> coll2) {
+
         for (T obj : coll1) {
             if (!coll2.contains(obj)) {
                 System.out.println("Found in parameter 1 but not 2: " + obj.toString());
