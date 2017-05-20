2d673fefbaa608038a99057d2be400d80282ab37
apease <apease@articulatesoftware.com>
2017-05-20 14:21:19 -0700

    Formatting changes

--- test/unit/java/com/articulate/sigma/nlg/SigmaMockTestBase.java
+++ test/unit/java/com/articulate/sigma/nlg/SigmaMockTestBase.java
@@ -13,11 +13,12 @@ import org.junit.BeforeClass;
 import java.io.File;
 import java.util.*;
 
-/**
+/****************************************************************
  * Class mocks KB and KBcache so that unit tests can be run independently and without long initialization.
  * Add processes and case roles to the static final lists as necessary.
  */
 public class SigmaMockTestBase {
+
     private static final String SIGMA_HOME = System.getenv("SIGMA_HOME");
     private static final String KB_PATH = (new File(SIGMA_HOME, "KBs")).getAbsolutePath();
     private static Hashtable<String, String> oldWordNetSynSetTable;
@@ -46,9 +47,11 @@ public class SigmaMockTestBase {
     // FIXME: not sure if instance should be here, or not
     private static final ImmutableList<String> RECOGNIZED_VARIABLE_ARITY_RELATIONS = ImmutableList.of("");
 
-    // Add an item to one of these two lists--RECOGNIZED_SUBSTANCES or RECOGNIZED_CORPUSCULAR_OBJECTS--if it is a "participant" in a process and
+    // Add an item to one of these two lists--RECOGNIZED_SUBSTANCES or RECOGNIZED_CORPUSCULAR_OBJECTS--
+    // if it is a "participant" in a process and
     // the NL output has it capitalized.
-    // Add an item to RECOGNIZED_SUBSTANCES if it is a "participant" in a process and the NL output precedes it with an indefinite article (i.e. "a" or "an").
+    // Add an item to RECOGNIZED_SUBSTANCES if it is a "participant" in a process and the NL output
+    // precedes it with an indefinite article (i.e. "a" or "an").
     private static final ImmutableList<String> RECOGNIZED_SUBSTANCES = ImmutableList.of(
             "Coffee",
             "Food",
@@ -66,7 +69,6 @@ public class SigmaMockTestBase {
 
     private static final Map<String, List<String>> recognizedMap = Maps.newHashMap();
 
-
     private static final ArrayList<String> INSTANCE_SIGNATURES = Lists.newArrayList(
             "",
             "Entity",
@@ -83,7 +85,6 @@ public class SigmaMockTestBase {
             "Entity");
 
     private static final HashMap<String, ArrayList<String>> signaturesMap = Maps.newHashMap();
-
 
     private static final ArrayList<Formula> LANG_FORMAT_MAP_COLS = Lists.newArrayList(
             new Formula("(format EnglishLanguage agent \"%2 is %n an &%agent of %1\")"),
@@ -103,7 +104,8 @@ public class SigmaMockTestBase {
 
     private static final HashMap<String, String> termFormatMap = Maps.newHashMap();
 
-
+    /****************************************************************
+     */
     static  {
         termFormatMap.put("Automobile", "automobile");
         termFormatMap.put("City", "city");
@@ -124,7 +126,7 @@ public class SigmaMockTestBase {
         recognizedMap.put("CorpuscularObject", RECOGNIZED_CORPUSCULAR_OBJECTS);
 
         List<String> recognizedCaseRolesCapitalized = Lists.newArrayList();
-        for(String str : RECOGNIZED_CASE_ROLES) {
+        for (String str : RECOGNIZED_CASE_ROLES) {
             recognizedCaseRolesCapitalized.add(Character.toUpperCase(str.charAt(0)) + str.substring(1));
         }
 
@@ -144,20 +146,21 @@ public class SigmaMockTestBase {
         termFormatMap.put("Entity", "entity");
     }
 
-    /**
+    /****************************************************************
      * Mock of KB.
      */
-    protected class KBMock extends KB     {
+    protected class KBMock extends KB {
+
          public KBMock(String dummyStr) {
             super(dummyStr);
             kbCache = new KBcacheMock(this);
         }
 
         @Override
-        public boolean isSubclass(String c1, String c2)    {
+        public boolean isSubclass(String c1, String c2) {
             List<String> list = recognizedMap.get(c2);
 
-            if (list != null && list.contains(c1))    {
+            if (list != null && list.contains(c1)) {
                 return true;
             }
 
@@ -175,7 +178,7 @@ public class SigmaMockTestBase {
         }
     }
 
-    /**
+    /****************************************************************
      * Mock of KBcache.
      */
     protected class KBcacheMock extends KBcache {
@@ -196,8 +199,11 @@ public class SigmaMockTestBase {
         }
     }
 
+    /****************************************************************
+     */
     @BeforeClass
     public static void setUp() {
+
         NLGUtils.readKeywordMap(KB_PATH);
 
         Hashtable<String,String> hash = new Hashtable<>();
@@ -212,10 +218,10 @@ public class SigmaMockTestBase {
         WordNet.wn.verbSynsetHash = hash;
     }
 
+    /****************************************************************
+     */
     @AfterClass
     public static void tearDown() {
         WordNet.wn.verbSynsetHash = oldWordNetSynSetTable;
     }
-
-
 }
