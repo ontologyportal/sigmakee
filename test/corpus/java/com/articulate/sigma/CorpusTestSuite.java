50ccf1b0cfea853ef78255eff5ea32e1ad5a0fb8
apease <apease@articulatesoftware.com>
2017-05-21 14:20:47 -0700

    test suite class

---
+++ test/corpus/java/com/articulate/sigma/CorpusTestSuite.java
@@ -0,0 +1,21 @@
+package com.articulate.sigma;
+
+import com.google.common.collect.Sets;
+import com.articulate.sigma.inference.InferenceTest;
+import com.articulate.sigma.inference.InferenceWikiTest;
+import org.junit.Test;
+import com.articulate.sigma.nlg.HtmlParaphraseIntegrationTest;
+import java.util.ArrayList;
+import java.util.Set;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import static org.junit.Assert.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        InferenceTest.class,
+})
+public class CorpusTestSuite extends IntegrationTestBase {
+
+}
