476204ff93cca8dd626020e59a2c61d3fc152a96
apease <apease@articulatesoftware.com>
2017-08-01 09:28:34 +0100

    fixed spurious error in argumentsToArrayList(), added variable for collecting warnings as well as errors

--- src/java/com/articulate/sigma/Formula.java
+++ src/java/com/articulate/sigma/Formula.java
@@ -108,7 +108,10 @@ public class Formula implements Comparable {
     
     // An ArrayList of String messages with a message that has a reserved character of ':' 
     // dividing the message from a formula or term that will be hyperlinked and formmated
-	public ArrayList<String> errors = new ArrayList<String>();
+	public TreeSet<String> errors = new TreeSet<String>();
+
+    /** Warnings found during execution. */
+    public TreeSet<String> warnings = new TreeSet<String>();
 
     /** The formula. */
     public String theFormula;
@@ -134,7 +137,7 @@ public class Formula implements Comparable {
         return;
     }
 
-	public ArrayList<String> getErrors() {
+	public TreeSet<String> getErrors() {
 		return this.errors;
 	}
 	
@@ -1405,8 +1408,15 @@ public class Formula implements Comparable {
     public ArrayList<String> argumentsToArrayList(int start) {
 
         if (theFormula.indexOf('(',1) != -1) {
-            System.out.println("Error in Formula.argumentsToArrayList(): complex formula: " + this);
-            return null;
+            ArrayList<String> erList = complexArgumentsToArrayList(0);
+            for (String s : erList) {
+                if (s.indexOf('(') != -1 && !StringUtil.quoted(s)) {
+                    String err = "Error in Formula.argumentsToArrayList() complex formula: " + this.toString();
+                    errors.add(err);
+                    System.out.println(err);
+                    return null;
+                }
+            }
         }
         int index = start;
         ArrayList<String> result = new ArrayList<String>();
@@ -2839,119 +2849,30 @@ public class Formula implements Comparable {
     /** ***************************************************************
      * A test method.
      */
+    public static void testArg2ArrayList() {
+
+        System.out.println("testArg2ArrayList(): ");
+        String f = "(termFormat EnglishLanguage experimentalControlProcess \"experimental control (process)\")";
+        Formula form = new Formula(f);
+        System.out.println(form.argumentsToArrayList(0));
+    }
+
+    /** ***************************************************************
+     */
+    public Formula negate() {
+
+        return new Formula("(not " + theFormula + ")");
+    }
+
+    /** ***************************************************************
+     * A test method.
+     */
     public static void main(String[] args) {
 
         // testIsSimpleClause();
         //testReplaceVar();
-        testBigArgs();
-    }
-
-    public Formula negate() {
-        return new Formula("(not " + theFormula + ")");
-    }
-
-    /** *****************************************************************
-     */
-    private static class VariableMapping {
-        String var1;
-        String var2;
-
-        public VariableMapping(String v1, String v2) {
-            var1 = v1;
-            var2 = v2;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-
-            VariableMapping that = (VariableMapping) o;
-
-            if (var1 != null ? !var1.equals(that.var1) : that.var1 != null) {
-                return false;
-            }
-            if (var2 != null ? !var2.equals(that.var2) : that.var2 != null) {
-                return false;
-            }
-
-            return true;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = var1 != null ? var1.hashCode() : 0;
-            result = 31 * result + (var2 != null ? var2.hashCode() : 0);
-            return result;
-        }
-
-        public static List<Set<VariableMapping>> intersect(List<Set<VariableMapping>> mapList1, List<Set<VariableMapping>> mapList2) {
-            List<Set<VariableMapping>> intersection = new LinkedList<Set<VariableMapping>>();
-            if (mapList1 == null || mapList2 == null) {
-                return null;
-            }
-            for (Set<VariableMapping> set1 : mapList1) {
-                for (Set<VariableMapping> set2 : mapList2) {
-                    Set<VariableMapping> newSet = VariableMapping.unify(set1, set2);
-                    if(newSet != null && !intersection.contains(newSet)) {
-                        intersection.add(newSet);
-                    }
-                }
-            }
-            if (intersection.isEmpty()) {
-                //keeping the convention of null meaning imposibility
-                intersection = null;
-            }
-            return intersection;
-        }
-
-        public static List<Set<VariableMapping>> union(List<Set<VariableMapping>> mapList1, List<Set<VariableMapping>> mapList2) {
-            List<Set<VariableMapping>> union = new LinkedList<Set<VariableMapping>>();
-            if(mapList1 != null) {
-                for (Set<VariableMapping> set1 : mapList1) {
-                    union.add(set1);
-                }
-            }
-            if (mapList2 != null) {
-                for (Set<VariableMapping> set2 : mapList2) {
-                    if ( !union.contains(set2)) {
-                        union.add(set2);
-                    }
-                }
-            }
-            return union;
-        }
-
-        private static Set<VariableMapping> unify(Set<VariableMapping> set1, Set<VariableMapping> set2) {
-            Set<VariableMapping> result = new HashSet<VariableMapping>();
-            for(VariableMapping element:set1) {
-                result.add(element);
-            }
-            for(VariableMapping element:set2) {
-                //testing the element does not collide with an existing element
-                for(VariableMapping e:result){
-                    boolean leftVarsEqual = e.var1.equals(element.var1);
-                    boolean rightVarsEqual = e.var2.equals(element.var2);
-                    if ((leftVarsEqual && !rightVarsEqual) || (!leftVarsEqual && rightVarsEqual)) {
-                        return null;
-                    }
-                }
-                result.add(element);
-            }
-            return result;
-        }
-
-        @Override
-        public String toString() {
-            return "VariableMapping{" +
-                    "var1='" + var1 + '\'' +
-                    ", var2='" + var2 + '\'' +
-                    '}';
-        }
+        //testBigArgs();
+        testArg2ArrayList();
     }
 }
 
