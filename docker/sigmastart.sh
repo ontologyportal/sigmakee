b70903c2ad5a7b34e833d0aab1722bd63b4280e4
apease <apease@articulatesoftware.com>
2018-07-06 15:00:10 -0700

    updated to latest tomcat

--- docker/sigmastart.sh
+++ docker/sigmastart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-su -l sumo -c /home/sumo/Programs/apache-tomcat-8.5.31/bin/startup.sh
+su -l sumo -c /home/sumo/Programs/apache-tomcat-9.0.10/bin/startup.sh
 
 RV=$?
 echo "Return value is: $RV"
@@ -8,5 +8,5 @@ echo "Return value is: $RV"
 sleep 5
 
 # Added this since the process must be running in the foreground.  I would suggest changing to the log for the application.
-su -l sumo -c "tail -f /home/sumo/Programs/apache-tomcat-8.5.31/logs/*.log"
+su -l sumo -c "tail -f /home/sumo/Programs/apache-tomcat-9.0.10/logs/*.log"
 
