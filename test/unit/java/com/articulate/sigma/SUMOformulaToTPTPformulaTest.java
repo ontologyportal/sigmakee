9dfeb5b71637bac33ffe01b0965cbe09a88139f7
apease <apease@articulatesoftware.com>
2018-07-10 11:57:50 -0700

    added tests

--- test/unit/java/com/articulate/sigma/SUMOformulaToTPTPformulaTest.java
+++ test/unit/java/com/articulate/sigma/SUMOformulaToTPTPformulaTest.java
@@ -11,8 +11,10 @@ import static org.junit.Assert.assertEquals;
  */
 public class SUMOformulaToTPTPformulaTest {
 
+    /** ***************************************************************
+     */
     @Test
-    public void TesttptpParseSUOKIFString() {
+    public void TesttptpParseSUOKIFString1() {
 
         String kifstring, expectedRes, actualRes;
 
@@ -20,10 +22,18 @@ public class SUMOformulaToTPTPformulaTest {
         kifstring = "(=> " +
                 "(instance ?X P)" +
                 "(instance ?X Q))";
-        expectedRes= "( ( ! [V__X] : (s__instance(V__X,s__P) => s__instance(V__X,s__Q)) ) )";
+        expectedRes = "( ( ! [V__X] : (s__instance(V__X,s__P) => s__instance(V__X,s__Q)) ) )";
         actualRes = SUMOformulaToTPTPformula.tptpParseSUOKIFString(kifstring, false);
+        System.out.println(actualRes);
         assertEquals(expectedRes, actualRes);
+    }
 
+    /** ***************************************************************
+     */
+    @Test
+    public void TesttptpParseSUOKIFString2() {
+
+        String kifstring, expectedRes, actualRes;
         // test2: passed
         kifstring = "(=> " +
                 "(or" +
@@ -32,9 +42,16 @@ public class SUMOformulaToTPTPformulaTest {
                 "(instance ?X ?T))";
         expectedRes = "( ( ! [V__X,V__T] : ((s__instance(V__X,s__Q) | s__instance(V__X,s__R)) => s__instance(V__X,V__T)) ) )";
         actualRes = SUMOformulaToTPTPformula.tptpParseSUOKIFString(kifstring, false);
+        System.out.println(actualRes);
         assertEquals(expectedRes, actualRes);
+    }
 
+    /** ***************************************************************
+     */
+    @Test
+    public void TesttptpParseSUOKIFString3() {
 
+        String kifstring, expectedRes, actualRes;
         // test3: passed
         kifstring = "(or " +
                 "(not " +
@@ -42,6 +59,43 @@ public class SUMOformulaToTPTPformulaTest {
                 "(instance ?X R))";
         expectedRes = "( ( ! [V__X] : ((~ s__instance(V__X,s__Q)) | s__instance(V__X,s__R)) ) )";
         actualRes = SUMOformulaToTPTPformula.tptpParseSUOKIFString(kifstring, false);
+        System.out.println(actualRes);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    /** ***************************************************************
+     */
+    @Test
+    public void TesttptpParseSUOKIFString4() {
+
+        String kifstring, expectedRes, actualRes;
+        // test3: passed
+        kifstring = "(<=>\n" +
+                "    (instance ?NUMBER NegativeRealNumber)\n" +
+                "    (and\n" +
+                "        (lessThan ?NUMBER 0)\n" +
+                "        (instance ?NUMBER RealNumber)))";
+        expectedRes = "( ( ! [V__NUMBER] : (s__instance(V__NUMBER,s__NegativeRealNumber) <=> (less(V__NUMBER,n__0) & s__instance(V__NUMBER,s__RealNumber))) ) )";
+        actualRes = SUMOformulaToTPTPformula.tptpParseSUOKIFString(kifstring, false);
+        System.out.println(actualRes);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    /** ***************************************************************
+     */
+    @Test
+    public void TesttptpParseSUOKIFString5() {
+
+        String kifstring, expectedRes, actualRes;
+        // test3: passed
+        kifstring = "(<=>\n" +
+                "    (instance ?NUMBER NegativeRealNumber)\n" +
+                "    (and\n" +
+                "        (lessThan ?NUMBER 0.001)\n" +    // nonsense just to test number hiding
+                "        (instance ?NUMBER RealNumber)))";
+        expectedRes = "( ( ! [V__NUMBER] : (s__instance(V__NUMBER,s__NegativeRealNumber) <=> (less(V__NUMBER,n__0_001) & s__instance(V__NUMBER,s__RealNumber))) ) )";
+        actualRes = SUMOformulaToTPTPformula.tptpParseSUOKIFString(kifstring, false);
+        System.out.println(actualRes);
         assertEquals(expectedRes, actualRes);
     }
 }
