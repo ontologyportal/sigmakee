7fd95b8cec6ae79b27d979d3b5e77dbcce30e06e
apease <apease@articulatesoftware.com>
2018-08-27 05:37:27 -0700

    many changes to improve translation to TFF

--- src/java/com/articulate/sigma/trans/SUMOKBtoTFAKB.java
+++ src/java/com/articulate/sigma/trans/SUMOKBtoTFAKB.java
@@ -23,18 +23,40 @@ public class SUMOKBtoTFAKB extends SUMOKBtoTPTPKB {
 
     /** *************************************************************
      */
-    public String translateSort(String s) {
+    public static String translateSort(String s) {
 
+        //System.out.println("translateSort(): s: '" + s + "'");
+        if (s.equals("$i") || s.equals("$tType"))
+            return s;
+        if (s.equals("Integer"))
+            return "$int";
+        if (s.equals("RealNumber"))
+            return "$real";
+        return "$i";
+    }
+
+    /** *************************************************************
+     */
+    public static String translateName(String s) {
+
+        System.out.println("% translateName(): " + s);
         int ttype = s.charAt(0);
         if (Character.isDigit(ttype))
             ttype = StreamTokenizer_s.TT_NUMBER;
-        return SUMOformulaToTPTPformula.translateWord(s,ttype,false);
+        String result = SUMOformulaToTPTPformula.translateWord(s,ttype,false);
+        if (result.endsWith("+"))
+            result = result.replace("+","_c");
+        return result;
     }
 
     /** *************************************************************
      */
     public void writeRelationSort(String t, PrintWriter pw, String sanitizedKBName) {
 
+        if (Formula.isLogicalOperator(t))
+            return;
+        if (Formula.isMathFunction(t))
+            return;
         ArrayList<String> sig = kb.kbCache.signatures.get(t);
         if (sig == null || sig.size() == 0) {
             pw.println("% Error in writeSorts(): " + t);
@@ -42,6 +64,8 @@ public class SUMOKBtoTFAKB extends SUMOKBtoTPTPKB {
             Thread.dumpStack();
         }
         StringBuffer sigBuf = new StringBuffer();
+        if (kb.isFunction(t))
+            sigBuf.append(" " + translateSort(sig.get(0)) + " *");
         for (String s : sig.subList(1,sig.size()))
             sigBuf.append(" " + translateSort(s) + " *");
         if (sigBuf.length() == 0) {
@@ -51,29 +75,62 @@ public class SUMOKBtoTFAKB extends SUMOKBtoTPTPKB {
             Thread.dumpStack();
         }
         String sigStr = sigBuf.toString().substring(0,sigBuf.length()-1);
+        String relname = translateName(t);
+        if (relname.endsWith("__m"))
+            relname = relname.substring(0,relname.length()-3);
         if (kb.isFunction(t)) {
             String range = sig.get(0);
-            pw.println("tff(" + StringUtil.initialLowerCase(t) + ".sig,type," + translateSort(t) + " : ( " + sigStr + " ) > " + translateSort(range) + " ).");
+            pw.println("tff(" + StringUtil.initialLowerCase(t) + "_sig,type," + relname + " : ( " + sigStr + " ) > " + translateSort(range) + " ).");
         }
         else
-            pw.println("tff(" + StringUtil.initialLowerCase(t) + ".sig,type," + translateSort(t) + " : ( " + sigStr + " ) > $o ).");
+            pw.println("tff(" + StringUtil.initialLowerCase(t) + "_sig,type," + relname + " : ( " + sigStr + " ) > $o ).");
 
+    }
+
+    /** *************************************************************
+     */
+    private void writeTermSort(PrintWriter pw, String t) {
+
+        HashSet<String> parents = kb.immediateParents(t);
+        if (parents == null || parents.size() == 0) {
+            parents = new HashSet<>();
+            if (!kb.isInstance(t))
+                parents.add("$tType");
+            else
+                parents.add("$i");
+        }
+        for (String c : parents) {
+            if (!kb.isInstance(t)) {
+                pw.println("tff(" + StringUtil.initialLowerCase(t) + "_type,type," +
+                        translateSort(t) + ": $tType ).");
+//                        translateSort(t) + ": " + translateSort(c) + " ).");
+                String cl = translateSort(c);
+                if (!c.equals("$tType"))
+                    cl = translateSort(c) + "_c";
+                pw.println("tff(" + StringUtil.initialLowerCase(t) + "_type,type," +
+                        translateSort(t) + "_c : $tType ).");
+                        //translateSort(t) + "_c : " + cl + " ).");
+            }
+            else
+                pw.println("tff(" + StringUtil.initialLowerCase(t) + "_type,type," +
+                        translateSort(t) + ": $tType ).");
+                        //translateSort(t) + ": " + translateSort(c) + " ).");
+        }
     }
 
     /** *************************************************************
      */
     public void writeSorts(PrintWriter pw, String sanitizedKBName) {
 
+        SUMOtoTFAform.setNumericFunctionInfo();
         for (String t : kb.getTerms()) {
             pw.println("% writeSorts(): " + t);
             if (Formula.isLogicalOperator(t) || t.equals("equal"))
                 continue;
             if (kb.isRelation(t))
                 writeRelationSort(t,pw,sanitizedKBName);
-            else if (!kb.isInstance(t))
-                pw.println("tff(" + StringUtil.initialLowerCase(t) + "_type,type," + translateSort(t) + ": $tType ).");
-            else
-                pw.println("tff(" + StringUtil.initialLowerCase(t) + "_type,type," + translateSort(t) + ": $i ).");
+            //else
+                //writeTermSort(pw,t);
         }
     }
 
