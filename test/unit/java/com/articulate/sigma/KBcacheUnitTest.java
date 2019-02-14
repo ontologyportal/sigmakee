69d7845f0987310aead7caf497531d338b7600eb
apease <apease@articulatesoftware.com>
2019-02-14 14:56:53 -0800

    Separated one test method out into all its component tests as individual methods

--- test/unit/java/com/articulate/sigma/KBcacheUnitTest.java
+++ test/unit/java/com/articulate/sigma/KBcacheUnitTest.java
@@ -23,6 +23,7 @@ package com.articulate.sigma;
  */
 import com.google.common.collect.Sets;
 import org.junit.Test;
+import org.junit.BeforeClass;
 
 import java.util.*;
 
@@ -30,10 +31,13 @@ import static org.junit.Assert.*;
 
 public class KBcacheUnitTest {
 
-    @Test
-    public void testBasic() {
+    public static KB kb = new KB("TestKB");
 
-        KB kb = new KB("TestKB");
+    /** ***************************************************************
+     */
+    @BeforeClass
+    public static void setup() {
+
         kb.kbCache = new KBcache(kb);
         KIF kif = new KIF();
         kif.parseStatement("(subAttribute Attorney Lawyer)");
@@ -57,48 +61,128 @@ public class KBcacheUnitTest {
         kif.parseStatement("(instance ResidentFn Function)");
         kif.parseStatement("(subclass Function Relation)");
         kb.merge(kif,"");
+        for (Formula f : kb.formulaMap.values())
+            f.sourceFile = "test"; // without a source file kbCache assumes it's a cached formula and ignores it
         kb.kbCache.buildCaches();
-        String rel = "rel";
         kb.kbCache.showState();
+    }
+
+    /** ***************************************************************
+     */
+    @Test
+    public void testRelations() {
+
         System.out.println("Test relations");
-        HashSet<String> expected = new HashSet<>(Arrays.asList("subAttribute","rel", "relsub", "subclass",
-                "subrelation","CitizenryFn","ResidentFn"));
+        HashSet<String> expected = new HashSet<>(Arrays.asList("subAttribute", "rel", "relsub", "subclass",
+                "subrelation", "CitizenryFn", "ResidentFn"));
         HashSet<String> actual = kb.kbCache.relations;
         assertEquals(expected, actual);
-        //transRels
+    }
+
+    /** ***************************************************************
+     */
+    @Test
+    public void transRels() {
+
         System.out.println("Test transRels");
-        expected = new HashSet<>(Arrays.asList("relsub","subclass","subAttribute","subrelation"));
-        actual = kb.kbCache.transRels;
+        HashSet<String> expected = new HashSet<>(Arrays.asList("relsub", "subclass", "subAttribute", "subrelation"));
+        HashSet<String> actual = kb.kbCache.transRels;
         assertEquals(expected, actual);
-        // parents
-        //expected = new HashSet<>(Arrays.asList("rel", "relsub"));
-        //actual = kb.kbCache.parents.get(rel);
-        //assertEquals(expected, actual);
-        // children
-        //expected = new HashSet<>(Arrays.asList("rel", "relsub"));
-        //actual = kb.kbCache.children.get(rel);
-        //assertEquals(expected, actual);
-        // signatures
-        //expected = new HashSet<>(Arrays.asList("rel", "relsub"));
-        //actual = kb.kbCache.signatures.get(rel);
-        //assertEquals(expected, actual);
-        // valences
-        //expected = new HashSet<>(Arrays.asList("rel", "relsub"));
-        //actual = kb.kbCache.valences;
-        //assertEquals(expected, actual);
+    }
 
-        // insts
-        //System.out.println("Test insts");
-        //expected = new HashSet<>(Arrays.asList("rel", "relsub"));
-        //actual = kb.kbCache.insts;
-        //assertEquals(expected, actual);
+    /** ***************************************************************
+     */
+    @Test
+    public void testParents() {
 
-        //instances
-        //expected = new HashSet<>(Arrays.asList("rel", "relsub"));
-        //actual = kb.kbCache.instances;
-        //assertEquals(expected, actual);
+        System.out.println("Test parents");
+        HashSet<String> expected = new HashSet<>(Arrays.asList("Relation", "Entity"));
+        HashSet<String> actual = kb.kbCache.getParentClassesOfInstance("rel");
+        assertEquals(expected, actual);
+    }
 
+    /** ***************************************************************
+     */
+    @Test
+    public void testChildren() {
+
+        System.out.println("Test children");
+        HashSet<String> expected = new HashSet<>(Arrays.asList("relsub"));
+        HashSet<String> actual = kb.kbCache.children.get("subrelation").get("rel");
+        assertEquals(expected, actual);
+    }
+
+    /** ***************************************************************
+     */
+    @Test
+    public void testSignatures() {
+
+        System.out.println("Test signatures");
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("", "Object", "Oject"));
+        ArrayList<String> actual = kb.kbCache.signatures.get("rel");
+        assertEquals(expected.subList(1,2), actual.subList(1,2));
+    }
+
+    /** ***************************************************************
+     */
+    @Test
+    public void testValences() {
+
+        System.out.println("Test valences");
+        int expected = 2;
+        int actual = kb.kbCache.valences.get("rel");
+        assertEquals(expected, actual);
+    }
+
+    /** ***************************************************************
+     */
+    @Test
+    public void testInsts() {
+
+        System.out.println("Test insts");
+        assertTrue(kb.kbCache.insts.contains("rel"));
+    }
+
+    /** ***************************************************************
+     */
+    @Test
+    public void testInstances() {
+
+        System.out.println("Test instances");
+        HashSet<String> expected = new HashSet<>(Arrays.asList("subAttribute", "rel", "relsub", "subclass",
+                "subrelation", "CitizenryFn", "ResidentFn"));
+        HashSet<String> actual = kb.kbCache.instances.get("Relation");
+        assertEquals(expected, actual);
+    }
+
+    /** ***************************************************************
+     */
+    @Test
+    public void testTransInsts() {
+
+        System.out.println("Test testTransInsts");
         assertTrue(kb.kbCache.transInstOf("Attorney", "Attribute"));
+    }
+
+    /** ***************************************************************
+     */
+    @Test
+    public void testIsChildOf() {
+
+        System.out.println("Test testIsChildOf");
         assertTrue(kb.isChildOf("CitizenryFn", "Function"));
     }
+
+    /** ***************************************************************
+     */
+    @Test
+    public void testInstancesForType() {
+
+        System.out.println("Test testInstancesForType");
+        HashSet<String> expected = new HashSet<>(Arrays.asList("subAttribute", "rel", "relsub", "subclass",
+                "subrelation", "CitizenryFn", "ResidentFn"));
+        HashSet<String> actual = kb.kbCache.getInstancesForType("Relation");
+        assertEquals(expected,actual);
+    }
+
 }
