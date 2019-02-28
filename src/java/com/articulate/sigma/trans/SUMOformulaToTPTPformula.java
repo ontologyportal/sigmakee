330351466958d3033c5c9b5337a1a9629bd6d8a6
apease <apease@articulatesoftware.com>
2019-02-28 07:27:42 -0800

    Change hardcoded initialization for "fof" to allow new "tff" translation type

--- src/java/com/articulate/sigma/trans/SUMOformulaToTPTPformula.java
+++ src/java/com/articulate/sigma/trans/SUMOformulaToTPTPformula.java
@@ -19,7 +19,6 @@ public class SUMOformulaToTPTPformula {
      */
     public SUMOformulaToTPTPformula () {
 
-        lang = "fof";
     }
 
     /** ***************************************************************
