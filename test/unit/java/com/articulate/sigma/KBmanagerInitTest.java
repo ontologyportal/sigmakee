6b808ce974211ee70c58a4cb9fbb80a98f6b534c
apease <apease@articulatesoftware.com>
2019-05-18 13:34:34 -0700

    make allowed init time more generous

--- test/unit/java/com/articulate/sigma/KBmanagerInitTest.java
+++ test/unit/java/com/articulate/sigma/KBmanagerInitTest.java
@@ -26,7 +26,7 @@ public class KBmanagerInitTest extends UnitTestBase {
      */
     @Test
     public void testInitializationTime()   {
-        assertTrue("Actual time = " + UnitTestBase.totalKbMgrInitTime, UnitTestBase.totalKbMgrInitTime < 200000);
+        assertTrue("Actual time = " + UnitTestBase.totalKbMgrInitTime, UnitTestBase.totalKbMgrInitTime < 250000);
         // Just in case something whacky is going on, make sure it's greater than some minimum, too.
         assertTrue("Actual time = " + UnitTestBase.totalKbMgrInitTime, UnitTestBase.totalKbMgrInitTime > 10000);
     }
