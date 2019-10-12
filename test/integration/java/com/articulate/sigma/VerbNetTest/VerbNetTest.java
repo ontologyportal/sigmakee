09536e39e51da617009852c5dc21ca2d164aa59b
apease <apease@articulatesoftware.com>
2019-10-12 14:38:44 -0700

    create new test

---
+++ test/integration/java/com/articulate/sigma/VerbNetTest/VerbNetTest.java
@@ -0,0 +1,40 @@
+package com.articulate.sigma.VerbNet;
+
+import com.articulate.sigma.*;
+import com.articulate.sigma.wordNet.*;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.*;
+import java.util.*;
+import static org.junit.Assert.*;
+
+public class VerbNetTest extends IntegrationTestBase {
+
+
+    /** *************************************************************
+     */
+    @Test
+    public void testTerm() {
+
+        String term = "SocialInteraction";
+        TreeMap<String,String> tm = WordNet.wn.getWordsFromTerm(term);
+        System.out.println("testTerm(): words: " + tm);
+        String verbs = VerbNet.formatVerbs(tm);
+        System.out.println("testTerm(): verbs: " + verbs);
+        assertTrue(!StringUtil.emptyString(verbs));
+    }
+
+    /** *************************************************************
+     */
+    @Test
+    public void testWordList() {
+
+        TreeMap<String, ArrayList<String>> tm = WordNet.wn.getSenseKeysFromWord("object");
+        System.out.println("testWordList(): senses: " + tm);
+        String verbs = VerbNet.formatVerbsList(tm);
+        System.out.println("testWordList(): verbs: " + verbs);
+        assertTrue(!StringUtil.emptyString(verbs));
+    }
+}
