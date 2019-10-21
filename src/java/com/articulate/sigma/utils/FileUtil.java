91f7e886d17ed22b78293719425c3b5cc9a38a4d
apease <apease@articulatesoftware.com>
2019-10-21 12:23:46 -0700

    new utility to extract KIF statements matching a given pattern to a separate file and generate a new file without them

--- src/java/com/articulate/sigma/utils/FileUtil.java
+++ src/java/com/articulate/sigma/utils/FileUtil.java
@@ -2,12 +2,13 @@ package com.articulate.sigma.utils;
 
 import com.google.common.collect.Lists;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtil {
+
+    public static boolean includeBlanks = false;
 
     /****************************************************************
      * This method reads in a text file, breaking it into single line documents
@@ -25,7 +26,7 @@ public class FileUtil {
         try {
             BufferedReader bf = new BufferedReader(new FileReader(f));
             while ((line = bf.readLine()) != null) {
-                if (line == null || line.equals(""))
+                if (!includeBlanks && (line == null || line.equals("")))
                     continue;
                 documents.add(line);
             }
@@ -35,5 +36,96 @@ public class FileUtil {
             System.out.println("Unable to read line in file. Last line successfully read was: " + line);
         }
         return documents;
+    }
+
+    /****************************************************************
+     * Read a KIF file extracting of a certain type in one
+     * and putting them in another.  Write a new copy of the original
+     * file without the statements, as well as the new file, leaving
+     * the original untouched.
+     */
+    public static void splitStatements(String filename, String pattern) {
+
+        String fnameMinus = filename + ".minus";
+        String fnameNew = filename + ".new";
+        PrintWriter pwMinus = null;
+        PrintWriter pwNew = null;
+        includeBlanks = true;
+        try {
+            pwNew = new PrintWriter(new FileWriter(fnameNew, false));
+            pwMinus = new PrintWriter(new FileWriter(fnameMinus, false));
+            ArrayList<String> lines = (ArrayList) readLines(filename, false);
+            System.out.println("splitSttaements(): read file " + filename + " with " + lines.size() + " lines");
+            for (int i = 0; i < lines.size(); i++) {
+                String s = lines.get(i);
+                System.out.println(s);
+                if (s.contains(pattern) && !s.startsWith(";")) {
+                    pwNew.println(s);
+                    int parenLevel = 0;
+                    boolean inQuote = false;
+                    do {
+                        for (int j = 0; j < s.length(); j++) {
+                            if (s.charAt(j) == '(' && !inQuote)
+                                parenLevel++;
+                            if (s.charAt(j) == ')' && !inQuote)
+                                parenLevel--;
+                            if (s.charAt(j) == '"')
+                                inQuote = !inQuote;
+                        }
+                        if (parenLevel > 0) {
+                            i++;
+                            s = lines.get(i);
+                            pwNew.println(s);
+                        }
+                    } while (parenLevel > 0);
+                }
+                else
+                    pwMinus.println(s);
+            }
+        }
+        catch (Exception ex) {
+            System.out.println("Error with " + filename + ": " + ex.getMessage());
+            ex.printStackTrace();
+        }
+        finally {
+            try {
+                if (pwNew != null)
+                    pwNew.close();
+                if (pwMinus != null)
+                    pwMinus.close();
+            }
+            catch (Exception ioe) {
+                ioe.printStackTrace();
+                System.out.println(ioe.getMessage());
+            }
+        }
+    }
+
+    /** ***************************************************************
+     * A test method.
+     */
+    public static void printHelp() {
+
+        System.out.println("Sigma Knowledge Engineering Environment - FileUtil");
+        System.out.println("  options:");
+        System.out.println("  -h - show this help screen");
+        System.out.println("  -s <fname> \"<pattern>\" - split statements");
+    }
+
+    /** ***************************************************************
+     */
+    public static void main(String[] args) {
+
+        if (args == null) {
+            printHelp();
+        }
+        else {
+            if (args != null && args.length > 0 && args[0].equals("-h")) {
+                printHelp();
+            }
+            if (args != null && args.length > 2 && args[0].equals("-s")) {
+                splitStatements(args[1],args[2]);
+            }
+        }
     }
 }
