8e9ea75e1ac90d99eda8e1c203a7b80618b56574
apease <apease@articulatesoftware.com>
2019-12-02 17:05:56 -0800

    add TPTP translation tests

--- test/integration/java/com/articulate/sigma/IntegrationTestSuite.java
+++ test/integration/java/com/articulate/sigma/IntegrationTestSuite.java
@@ -3,6 +3,7 @@ package com.articulate.sigma;
 import com.google.common.collect.Sets;
 import org.junit.Test;
 import com.articulate.sigma.nlg.HtmlParaphraseIntegrationTest;
+import com.articulate.sigma.trans.*;
 import java.util.ArrayList;
 import java.util.Set;
 import org.junit.runner.RunWith;
@@ -20,6 +21,7 @@ import static org.junit.Assert.*;
         KbIntegrationTest.class,
         KBmanagerInitIntegrationTest.class,
         PredVarInstIntegrationTest.class,
+        SUMOformulaToTPTPformulaTest.class,
         //HtmlParaphraseIntegrationTest.class,
 })
 public class IntegrationTestSuite extends IntegrationTestBase {
