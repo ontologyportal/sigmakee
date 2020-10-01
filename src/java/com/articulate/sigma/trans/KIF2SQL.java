b7786767479fda606bf86ef479b34d97a97b367f
apease <apease@articulatesoftware.com>
2020-10-01 16:02:14 -0700

    handle additional tables, add debugging, handle apostrophes and long strings

--- src/java/com/articulate/sigma/trans/KIF2SQL.java
+++ src/java/com/articulate/sigma/trans/KIF2SQL.java
@@ -29,6 +29,8 @@ Articulate Software
 
     public static KIF2SQL ot = new KIF2SQL();
     public static boolean initNeeded = true;
+    public static boolean debug = false;
+    public static int maxForDebug = 10;
     public KB kb;
     /** Relations in SUMO that have a corresponding relation in
      *  OWL and therefore require special treatment. */
@@ -72,11 +74,13 @@ Articulate Software
 
    /** ***************************************************************
      */
-    private static String processArg(String doc) {
+    private static String processArg(String doc, int limit) {
 
         String result = doc;
         result = result.replaceAll("'","''");
         result = removeQuotes(result);
+        if (result.length() > limit)
+            result = result.substring(0,limit);
         return result;
     }
 
@@ -109,7 +113,7 @@ Articulate Software
                         case ']': rel = "antiInstanceRelation"; break;
                     }
                 }
-                pw.println(" INSERT INTO edges (source, rel, target) values ('" + term + "', '" + rel + "', 'WN30-" + synset + "');");
+                pw.println(" INSERT INTO edges (source, rel, target) values ('" + term + "', '" + rel + "', '" + processArg("WN30-" + synset,50) + "');");
             }
         }
     }
@@ -119,8 +123,8 @@ Articulate Software
     private void writeBinary(PrintWriter pw, Formula form) {
 
         String rel = form.getStringArgument(0);
-        String arg1 = processArg(form.getStringArgument(1));
-        String arg2 = processArg(form.getStringArgument(2));
+        String arg1 = processArg(form.getStringArgument(1),50);
+        String arg2 = processArg(form.getStringArgument(2),50);
         pw.println("INSERT INTO edges (source, rel, target) values ('" + arg1 + "', '" + rel + "', '" + arg2 + "');");
     }
 
@@ -129,9 +133,9 @@ Articulate Software
     private void writeTernary(PrintWriter pw, Formula form) {
 
         String rel = form.getStringArgument(0);
-        String arg1 = processArg(form.getStringArgument(1));
-        String arg2 = processArg(form.getStringArgument(2));
-        String arg3 = processArg(form.getStringArgument(3));
+        String arg1 = processArg(form.getStringArgument(1),50);
+        String arg2 = processArg(form.getStringArgument(2),50);
+        String arg3 = processArg(form.getStringArgument(3),50);
         pw.println("INSERT INTO ternary (rel, arg1, arg2, arg3) values ('" + rel + "', '" + arg1 + "', '" + arg2 + "', '" + arg3 + "');");
     }
 
@@ -140,11 +144,11 @@ Articulate Software
     private void writeQuaternary(PrintWriter pw, Formula form) {
 
         String rel = form.getStringArgument(0);
-        String arg1 = processArg(form.getStringArgument(1));
-        String arg2 = processArg(form.getStringArgument(2));
-        String arg3 = processArg(form.getStringArgument(3));
-        String arg4 = processArg(form.getStringArgument(4));
-        pw.println("INSERT INTO ternary (rel, arg1, arg2, arg3, arg4) values ('" + rel + "', '" + arg1 + "', '" + arg2 + "', '" + arg3 + "', '" + arg4 + "');");
+        String arg1 = processArg(form.getStringArgument(1),50);
+        String arg2 = processArg(form.getStringArgument(2),50);
+        String arg3 = processArg(form.getStringArgument(3),50);
+        String arg4 = processArg(form.getStringArgument(4),50);
+        pw.println("INSERT INTO quaternary (rel, arg1, arg2, arg3, arg4) values ('" + rel + "', '" + arg1 + "', '" + arg2 + "', '" + arg3 + "', '" + arg4 + "');");
     }
 
     /** ***************************************************************
@@ -152,12 +156,12 @@ Articulate Software
     private void writeQuinntary(PrintWriter pw, Formula form) {
 
         String rel = form.getStringArgument(0);
-        String arg1 = processArg(form.getStringArgument(1));
-        String arg2 = processArg(form.getStringArgument(2));
-        String arg3 = processArg(form.getStringArgument(3));
-        String arg4 = processArg(form.getStringArgument(4));
-        String arg5 = processArg(form.getStringArgument(5));
-        pw.println("INSERT INTO ternary (rel, arg1, arg2, arg3, arg4, arg5) values ('" + rel + "', '" + arg1 + "', '" + arg2 + "', '" + arg3 + "', '" + arg4 +  "', '" + arg5 + "');");
+        String arg1 = processArg(form.getStringArgument(1),50);
+        String arg2 = processArg(form.getStringArgument(2),50);
+        String arg3 = processArg(form.getStringArgument(3),50);
+        String arg4 = processArg(form.getStringArgument(4),50);
+        String arg5 = processArg(form.getStringArgument(5),50);
+        pw.println("INSERT INTO quintary (rel, arg1, arg2, arg3, arg4, arg5) values ('" + rel + "', '" + arg1 + "', '" + arg2 + "', '" + arg3 + "', '" + arg4 +  "', '" + arg5 + "');");
     }
 
     /** ***************************************************************
@@ -167,7 +171,7 @@ Articulate Software
         Iterator it = kb.formulaMap.values().iterator();
         for (Formula f : kb.formulaMap.values()) {
             if (f.isRule())
-                pw.println("INSERT INTO nodes (id, label) values ('axiom-" + f.createID() + "', '" + f.getFormula() + "');");
+                pw.println("INSERT INTO doc (lang, term, doc)  values ('SUOKIF', 'axiom-" + f.createID() + "', '" + processArg(f.getFormula(),500) + "');");
             else if (f.isSimpleClause(kb)){
                 if (f.argumentsToArrayListString(1).size() == 2)
                     writeBinary(pw,f);
@@ -192,7 +196,7 @@ Articulate Software
                 String lang = form.getStringArgument(2);
                 String documentation = form.getStringArgument(3);
                 if (documentation != null)
-                    pw.println("INSERT INTO ternary (rel, lang, id, label) values ('documentation', '" + lang + "', '" + term + "', '" + StringUtil.wordWrap(processArg(documentation)) + "');");
+                    pw.println("INSERT INTO doc (lang, term, doc) values ('" + lang + "', '" + term + "', '" + processArg(documentation,500) + "');");
             }
         }
     }
@@ -209,8 +213,11 @@ Articulate Software
      */
     public void writeKB(PrintWriter pw) {
 
+        int counter = 0;
         Set<String> kbterms = kb.getTerms();
         for (String term : kbterms) {
+            if (debug && counter++ > maxForDebug)
+                continue;
             writeSUMOTerm(pw,term);
             writeDocumentation(pw,term);
             writeWordNetLink(pw,term);
@@ -246,7 +253,7 @@ Articulate Software
                 "Somebody ----s somebody PP",
                 "Somebody ----s something PP",
                 "Somebody ----s PP",
-                "Somebody's (body part) ----s",
+                "Somebody''s (body part) ----s",
                 "Somebody ----s somebody to INFINITIVE",
                 "Somebody ----s somebody INFINITIVE",
                 "Somebody ----s that CLAUSE",
@@ -274,14 +281,14 @@ Articulate Software
      */
     private void writeWordNetSynset(PrintWriter pw, String synset) {
 
-        System.out.println("INFO in KIF2SQL.writeWordNetSynset(): " + synset);
+        //System.out.println("INFO in KIF2SQL.writeWordNetSynset(): " + synset);
         if (synset.startsWith("WN30-"))
             synset = synset.substring(5);
         ArrayList al = (ArrayList) WordNet.wn.synsetsToWords.get(synset);
         if (al != null) {
             for (int i = 0; i < al.size(); i++) {
                 String word = (String) al.get(i);
-                pw.println("INSERT INTO wordnet (source, rel, target) values ('WN30-" + synset + "', 'word', '" + word + "');");
+                pw.println("INSERT INTO wordnet (source, rel, target) values ('WN30-" + synset + "', 'word', '" + processArg(word,50) + "');");
             }
             String doc = null;
             switch (synset.charAt(0)) {
@@ -290,8 +297,7 @@ Articulate Software
                 case '3': doc = (String) WordNet.wn.adjectiveDocumentationHash.get(synset.substring(1)); break;
                 case '4': doc = (String) WordNet.wn.adverbDocumentationHash.get(synset.substring(1)); break;
             }
-            doc = processArg(doc);
-            pw.println("INSERT INTO wordnet (source, rel, target) values ('WN30-" + synset + "', 'doc', '" + doc + "');");
+            pw.println("INSERT INTO doc (lang, term, doc) values ('EnglishLanguage', 'WN30-" + synset + "', '" + processArg(doc,500) + "');");
             al = (ArrayList) WordNet.wn.relations.get(synset);
             if (al != null) {
                 for (int i = 0; i < al.size(); i++) {
@@ -311,13 +317,13 @@ Articulate Software
         while (it.hasNext()) {
             String plural = (String) it.next();
             String singular = (String) WordNet.wn.exceptionNounHash.get(plural);
-            pw.println("INSERT INTO wordnet (source, rel, target) values ('" + plural + "', 'pluralOf', '" + singular + "');");
+            pw.println("INSERT INTO wordnet (source, rel, target) values ('" + processArg(plural,50) + "', 'pluralOf', '" + processArg(singular,50) + "');");
         }
         it = WordNet.wn.exceptionVerbHash.keySet().iterator();
         while (it.hasNext()) {
             String past = (String) it.next();
             String infinitive = (String) WordNet.wn.exceptionVerbHash.get(past);
-            pw.println("INSERT INTO wordnet (source, rel, target) values ('" + infinitive + "', 'infinitiveOf', '" + past + "');");
+            pw.println("INSERT INTO wordnet (source, rel, target) values ('" + processArg(infinitive,50) + "', 'infinitiveOf', '" + processArg(past,50) + "');");
         }
     }
 
@@ -333,7 +339,9 @@ Articulate Software
         if (senses != null) {
             for (int i = 0; i < senses.size(); i++) {
                 String sense = (String) senses.get(i);
-                pw.println("INSERT INTO wordnet (source, rel, target) values ('" + word + "', 'senseKey', 'WN30WordSense-" + sense + "');");
+                if (sense.length() > 50)
+                    sense = sense.substring(0,50);
+                pw.println("INSERT INTO wordnet (source, rel, target) values ('" + processArg(word,50) + "', 'senseKey', '" + processArg("WN30WordSense-" + sense,50) + "');");
             }
         }
         else
@@ -344,9 +352,10 @@ Articulate Software
      */
     private void writeWordsToSenses(PrintWriter pw) throws IOException {
 
-        Iterator it = WordNet.wn.wordsToSenseKeys.keySet().iterator();
-        while (it.hasNext()) {
-            String word = (String) it.next();
+        int counter = 0;
+        for (String word : WordNet.wn.wordsToSenseKeys.keySet()) {
+            if (debug && counter++ > maxForDebug)
+                continue;
             writeOneWordToSenses(pw,word);
         }
     }
@@ -355,20 +364,21 @@ Articulate Software
      */
     private void writeSenseIndex(PrintWriter pw) throws IOException {
 
-        Iterator it = WordNet.wn.senseIndex.keySet().iterator();
-        while (it.hasNext()) {
-            String sense = (String) it.next();
+        int counter = 0;
+        for (String sense : WordNet.wn.senseIndex.keySet()) {
+            if (debug && counter++ > maxForDebug)
+                continue;
             String synset = (String) WordNet.wn.senseIndex.get(sense);
             String pos = WordNetUtilities.getPOSfromKey(sense);
             String word = WordNetUtilities.getWordFromKey(sense);
             String posNum = WordNetUtilities.posLettersToNumber(pos);
-            pw.println("INSERT INTO wordnet (source, rel, target) values ('" + sense + "', 'keyTosense', 'WN30-" + posNum + synset + "');");
+            pw.println("INSERT INTO wordnet (source, rel, target) values ('" + processArg(sense,50) + "', 'keyTosense', 'WN30-" + posNum + synset + "');");
             if (posNum.equals("2")) {
                 ArrayList frames = (ArrayList) WordNet.wn.verbFrames.get(synset + "-" + word);
                 if (frames != null) {
                     for (int i = 0; i < frames.size(); i++) {
                         String frame = (String) frames.get(i);
-                        pw.println("INSERT INTO wordnet (source, rel, target) values ('" + sense + "', 'frame', 'WN30VerbFrame-" + frame + "');");
+                        pw.println("INSERT INTO wordnet (source, rel, target) values ('" + processArg(sense,50) + "', 'frame', 'WN30VerbFrame-" + frame + "');");
                     }
                 }
             }
@@ -382,9 +392,10 @@ Articulate Software
         System.out.println("INFO in KIF2SQL.writeWordNet()");
         // Get POS-prefixed synsets.
         Iterator it = WordNet.wn.synsetsToWords.keySet().iterator();
-        while (it.hasNext()) {
-            String synset = (String) it.next();
-            writeWordNetSynset(pw,synset);
+        int counter = 0;
+        for (String synset : WordNet.wn.synsetsToWords.keySet()) {
+            if (!debug || counter++ < maxForDebug)
+                writeWordNetSynset(pw,synset);
         }
         writeWordNetExceptions(pw);
         writeVerbFrames(pw);
@@ -427,9 +438,10 @@ Articulate Software
         else {
             System.out.println("INFO in KIF2SQL.main()");
             KBmanager.getMgr().initializeOnce();
+            String kbDir = KBmanager.getMgr().getPref("kbDir");
             String kbName = KBmanager.getMgr().getPref("sumokbname");
             KB kb = KBmanager.getMgr().getKB(kbName);
-            String path = kbName + ".sql";
+            String path = kbDir + File.separator + kbName + ".sql";
             System.out.println("INFO in KIF2SQL.write(): writing " + path);
             FileWriter fw = new FileWriter(path);
             PrintWriter pw = new PrintWriter(fw);
