c26afb52ceee4d0efddbcce0d6f122ca95b5d002
apease <apease@articulatesoftware.com>
2020-12-15 14:07:26 -0800

    move addToMap() here

--- src/java/com/articulate/sigma/utils/MapUtils.java
+++ src/java/com/articulate/sigma/utils/MapUtils.java
@@ -10,12 +10,26 @@ import java.util.Set;
 public class MapUtils {
 
     /** ***************************************************************
+     */
+    public static void addToMapMap(HashMap<String, HashMap<String, HashSet<String>>> mapmap, String superkey, String key, String element) {
+
+        HashMap<String, HashSet<String>> map = mapmap.get(superkey);
+        if (map == null)
+            map = new HashMap<String, HashSet<String>>();
+        mapmap.put(superkey,map);
+        HashSet<String> set = map.get(key);
+        if (set == null)
+            set = new HashSet<String>();
+        set.add(element);
+        map.put(key,set);
+    }
+
+    /** ***************************************************************
      * utility method to add a String element to a HashMap of String
      * keys and a value of an HashSet of Strings
      */
     public static void addToMap(HashMap<String, HashSet<String>> map, String key, String element) {
 
-        //System.out.println("FormulaPreprocessor.addToMap(): key,element: " + key + ", " + element);
         HashSet<String> al = map.get(key);
         if (al == null)
             al = new HashSet<String>();
