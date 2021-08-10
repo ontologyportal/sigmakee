e232aa6a91eb555476ffe38f6ab931b8232d0f9a
apease <apease@articulatesoftware.com>
2021-08-10 13:18:50 -0700

    add tests for collapseConnectives()

--- test/integration/java/com/articulate/sigma/trans/TPTP2SUMOTest.java
+++ test/integration/java/com/articulate/sigma/trans/TPTP2SUMOTest.java
@@ -3,8 +3,10 @@ package com.articulate.sigma.trans;
 import TPTPWorld.TPTPFormula;
 import TPTPWorld.TPTPParser;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import com.articulate.sigma.*;
+import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
@@ -20,9 +22,11 @@ public class TPTP2SUMOTest {
 
     /** *************************************************************
      */
+    @Ignore
     @Test
     public void testPartition() {
 
+        System.out.println("TPTP2SUMOTest.testPartition()");
         String input = "fof(f658,plain,(" +
                 "sQ4_eqProxy(s__BobTheGolfer,s__JohnTheGolfer) | " +
                 "sQ4_eqProxy(s__JohnsGolfGame,s__BobsGolfGame) | " +
@@ -43,5 +47,114 @@ public class TPTP2SUMOTest {
         catch (Exception e) {
             System.out.println("e: " + e);
         }
+    }
+
+    /** *************************************************************
+     */
+    @Ignore
+    @Test
+    public void testCollapse() {
+
+        System.out.println("TPTP2SUMOTest.testPartition()");
+        Formula f = new Formula("(and (and (foo A B) (foo B B)) (bar C))");
+        String result = TPTP2SUMO.collapseConnectives(f).toString();
+        String expected = "(and\n" +
+                "  (foo A B)\n" +
+                "  (foo B B)\n" +
+                "  (bar C))";
+        System.out.println("result: " + result);
+        assertEquals(expected,result);
+    }
+
+    /** *************************************************************
+     */
+    @Ignore
+    @Test
+    public void testCollapse2() {
+
+        System.out.println("TPTP2SUMOTest.testPartition2()");
+        Formula f = new Formula("(=> (and (and (foo A B) (foo B B)) (bar C)) (blah F G))");
+        String result = TPTP2SUMO.collapseConnectives(f).toString();
+        String expected = "(=>\n" +
+                "  (and\n" +
+                "    (foo A B)\n" +
+                "    (foo B B)\n" +
+                "    (bar C))\n" +
+                "  (blah F G))";
+        System.out.println("result: " + result);
+        assertEquals(expected,result);
+    }
+
+    /** *************************************************************
+     */
+    @Test
+    public void testCollapse3() {
+
+        KBmanager.prefOverride.put("loadLexicons","false");
+        KBmanager.getMgr().initializeOnce();
+        System.out.println("TPTP2SUMOTest.testPartition3()");
+        Formula f = new Formula("(forall (?X155 ?X156 ?X157 ?X158)\n" +
+                "  (=>\n" +
+                "    (and\n" +
+                "      (instance ?X158 Organism)\n" +
+                "      (instance ?X155 Organism))\n" +
+                "    (=>\n" +
+                "      (and\n" +
+                "        (and\n" +
+                "          (and\n" +
+                "            (and\n" +
+                "              (and\n" +
+                "                (and\n" +
+                "                  (and\n" +
+                "                    (instance ?X157 Golf)\n" +
+                "                    (instance ?X156 Golf))\n" +
+                "                  (not\n" +
+                "                    (equal ?X155 ?X158)))\n" +
+                "                (not\n" +
+                "                  (equal ?X156 ?X157)))\n" +
+                "              (plays ?X157 ?X155))\n" +
+                "            (plays ?X156 ?X158))\n" +
+                "          (inhabits ?X155 UnitedKingdom))\n" +
+                "        (inhabits ?X158 UnitedKingdom))\n" +
+                "      (exists (?X159)\n" +
+                "        (and\n" +
+                "          (and\n" +
+                "            (and\n" +
+                "              (and\n" +
+                "                (plays ?X159 ?X155)\n" +
+                "                (plays ?X159 ?X158))\n" +
+                "              (instance ?X159 Golf))\n" +
+                "            (located ?X159 UnitedKingdom))\n" +
+                "          (instance ?X159 TournamentSport))))))");
+        String result = TPTP2SUMO.collapseConnectives(f).toString();
+        String expected = "(forall (?A1 ?G1 ?G2 ?A2)\n" +
+                "  (=>\n" +
+                "    (and\n" +
+                "      (instance ?A2 Organism)\n" +
+                "      (instance ?A1 Organism))\n" +
+                "(=>\n" +
+                "  (and\n" +
+                "    (inhabits ?A1 UnitedKingdom)\n" +
+                "    (inhabits ?A2 UnitedKingdom)\n" +
+                "    (plays ?G1 ?A1)\n" +
+                "    (plays ?G2 ?A2)\n" +
+                "    (not \n" +
+                "      (equal ?G1 ?G2))\n" +
+                "    (not \n" +
+                "      (equal ?A1 ?A2))\n" +
+                "    (instance ?G1 Golf)\n" +
+                "    (instance ?G2 Golf))\n" +
+                "  (exists (?G3)\n" +
+                "    (and\n" +
+                "      (instance ?G3 TournamentSport)\n" +
+                "      (located ?G3 UnitedKingdom)\n" +
+                "      (instance ?G3 Golf)\n" +
+                "      (plays ?G3 ?A1)\n" +
+                "      (plays ?G3 ?A2))))))";
+        System.out.println("result: " + result);
+        System.out.println("expected: " + expected);
+        Formula fresult = new Formula(result);
+        Formula fexpected = new Formula(expected);
+        assertTrue(fexpected.deepEquals(fresult));
     }
 }
