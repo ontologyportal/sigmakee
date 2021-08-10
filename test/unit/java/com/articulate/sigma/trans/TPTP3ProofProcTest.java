93b9e54d77a17bcec9fd56cbf52b468a14f02674
apease <apease@articulatesoftware.com>
2021-08-10 13:20:22 -0700

    add new test, improve test result printing

--- test/unit/java/com/articulate/sigma/trans/TPTP3ProofProcTest.java
+++ test/unit/java/com/articulate/sigma/trans/TPTP3ProofProcTest.java
@@ -43,6 +43,9 @@ public class TPTP3ProofProcTest extends UnitTestBase {
     @Test
     public void testGetPrologArgs1() {
 
+        System.out.println("=============================");
+        String label = "testGetPrologArgs1";
+        System.out.println("TPTP3ProofProcTest: " + label);
         String input = "fof(c_0_5, axiom, (s__subclass(s__Artifact,s__Object)), c_0_3).";
         String expected = "[fof, c_0_5,  axiom,  (s__subclass(s__Artifact,s__Object)),  c_0_3]";
         test(input,expected,"testGetPrologArgs1");
@@ -53,6 +56,9 @@ public class TPTP3ProofProcTest extends UnitTestBase {
     @Test
     public void testGetPrologArgs2() {
 
+        System.out.println("=============================");
+        String label = "testGetPrologArgs2";
+        System.out.println("TPTP3ProofProcTest: " + label);
         String input = "fof(c_0_2, negated_conjecture,(~(?[X1]:(s__subclass(X1,s__Object)&~$answer(esk1_1(X1)))))," +
                 "inference(assume_negation,[status(cth)],[inference(add_answer_literal,[status(thm)],[c_0_0, theory(answers)])])).";
         String expected = "[fof, c_0_2,  negated_conjecture, (~(?[X1]:(s__subclass(X1,s__Object)&~$answer(esk1_1(X1))))), " +
@@ -64,6 +70,10 @@ public class TPTP3ProofProcTest extends UnitTestBase {
      */
     @Test
     public void testGetPrologArgs3() {
+
+        System.out.println("=============================");
+        String label = "testGetPrologArgs3";
+        System.out.println("TPTP3ProofProcTest: " + label);
         String input = "cnf(c_0_14,negated_conjecture,($false), " +
                     "inference(eval_answer_literal,[status(thm)], " +
                     "[inference(spm,[status(thm)],[c_0_12, c_0_13, theory(equality)]), theory(answers)]), ['proof']).";
@@ -77,6 +87,9 @@ public class TPTP3ProofProcTest extends UnitTestBase {
     @Test
     public void testGetPrologArgs4() {
 
+        System.out.println("=============================");
+        String label = "testGetPrologArgs4";
+        System.out.println("TPTP3ProofProcTest: " + label);
         String input = "fof(f185,conjecture,(" +
                     "  ? [X15] : s__subclass(X15,s__Entity))," +
                     "  file('/home/apease/.sigmakee/KBs/temp-comb.tptp',unknown)).";
@@ -91,7 +104,9 @@ public class TPTP3ProofProcTest extends UnitTestBase {
     public void testParseProofStep () {
 
         System.out.println("========================");
-        String ps1 = "fof(c_0_5, axiom, (s__subclass(s__Artifact,s__Object)), c_0_3).";
+        String label = "testParseProofStep";
+        System.out.println("TPTP3ProofProcTest: " + label);
+        String ps1 = "fof(c_0_5, axiom, (s__subclass(s__Artifact,s__Object)), file('/home/apease/.sigmakee/KBs/temp-comb.tptp', kb_SUMO_1234)).";
         String ps2 = "fof(c_0_2, negated_conjecture,(~(?[X1]:(s__subclass(X1,s__Object)&~$answer(esk1_1(X1)))))," +
                 "inference(assume_negation,[status(cth)],[inference(add_answer_literal,[status(thm)],[c_0_0, theory(answers)])])).";
         String ps3 = "cnf(c_0_14,negated_conjecture,($false), " +
@@ -99,19 +114,36 @@ public class TPTP3ProofProcTest extends UnitTestBase {
         String ps4 = "fof(f185,conjecture,(" +
                 "  ? [X15] : s__subclass(X15,s__Entity))," +
                 "  file('/home/apease/.sigmakee/KBs/temp-comb.tptp',unknown)).";
+        String ps5 = "fof(f768,axiom,(! [X155,X156,X157,X158] : " +
+                "((s__instance(X158,s__Organism) & " +
+                "s__instance(X155,s__Organism)) => " +
+                "((s__instance(X157,s__Golf) & " +
+                "s__instance(X156,s__Golf) & " +
+                "X155 != X158 & X156 != X157 & " +
+                "s__plays(X157,X155) & " +
+                "s__plays(X156,X158) & " +
+                "s__inhabits(X155,s__UnitedKingdom) & " +
+                "s__inhabits(X158,s__UnitedKingdom)) => " +
+                "? [X159] : (s__plays(X159,X155) & s__plays(X159,X158) & " +
+                "s__instance(X159,s__Golf) & s__located(X159,s__UnitedKingdom) & " +
+                "s__instance(X159,s__TournamentSport))))),  file('/home/apease/.sigmakee/KBs/temp-comb.tptp',kb_SUMO_768)).";
         TPTP3ProofProcessor tpp = new TPTP3ProofProcessor();
         tpp.idTable.put("c_0_0", Integer.valueOf(0));
         tpp.idTable.put("c_0_3", Integer.valueOf(1));
         tpp.idTable.put("c_0_12", Integer.valueOf(2));
         tpp.idTable.put("c_0_13", Integer.valueOf(3));
-        System.out.println(tpp.parseProofStep(ps1));
+ //       System.out.println(tpp.parseProofStep(ps1));
         System.out.println();
-        System.out.println(tpp.parseProofStep(ps2));
+ //       System.out.println(tpp.parseProofStep(ps2));
         System.out.println();
-        System.out.println(tpp.parseProofStep(ps3));
+ //       System.out.println(tpp.parseProofStep(ps3));
         System.out.println();
         tpp.idTable.put("f185", Integer.valueOf(4));
-        System.out.println(tpp.parseProofStep(ps4));
+ //       System.out.println(tpp.parseProofStep(ps4));
+        System.out.println();
+        String result = tpp.parseProofStep(ps5).toString();
+        System.out.println(result);
+        assertTrue(result.contains("inhabits"));
     }
 
     /** ***************************************************************
@@ -183,6 +215,7 @@ public class TPTP3ProofProcTest extends UnitTestBase {
 
         System.out.println("========================");
         String label = "testProcessAnswersFromProof";
+        System.out.println("TPTP3ProofProcTest: " + label);
         ArrayList<String> input = new ArrayList();
         input.add("% SZS status Theorem for temp-comb");
         input.add("% SZS answers Tuple [[s__TransitFn__m]|_] for temp-comb");
@@ -206,7 +239,7 @@ public class TPTP3ProofProcTest extends UnitTestBase {
         tpp.parseProofOutput(input,query,kb,sb);
         tpp.processAnswersFromProof(new StringBuffer("X"),query);
         String actual = tpp.bindingMap.toString();
-        String expected = "{?X=TransitFn}";
+        String expected = "{X0=TransitFn}";
         System.out.println("Actual: " + actual);
         System.out.println("Expected: " + expected);
         if (!StringUtil.emptyString(actual) && actual.equals(expected))
