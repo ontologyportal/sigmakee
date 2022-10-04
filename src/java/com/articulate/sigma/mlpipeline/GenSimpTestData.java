8ac72abec3dabff22f8d77ef3ccd7e83dae1c344
apease <apease@articulatesoftware.com>
2022-10-04 17:21:56 +0200

    routines to generate lots of ground facts

---
+++ src/java/com/articulate/sigma/mlpipeline/GenSimpTestData.java
@@ -0,0 +1,315 @@
+package com.articulate.sigma.mlpipeline;
+
+import com.articulate.sigma.*;
+import com.articulate.sigma.nlg.LanguageFormatter;
+import com.articulate.sigma.nlg.NLGUtils;
+import com.articulate.sigma.utils.StringUtil;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.*;
+
+public class GenSimpTestData {
+
+    public static boolean debug = false;
+    public static KB kb;
+    public static Random rand = new Random();
+    public static boolean skip = false;
+    public static HashSet<String> skipTypes = new HashSet<>();
+    public static final int instLimit = 10;
+
+    /** ***************************************************************
+     * handle the case where the argument type is a subclass
+     */
+    public static String handleClass(String t, HashMap<String, ArrayList<String>> instMap) {
+
+        String arg = "";
+        String bareClass = t.substring(0, t.length() - 1);
+        if (debug) System.out.println("handleClass(): bareClass: " + bareClass);
+        if (bareClass.equals("Class"))
+            skip = true;
+        else {
+            HashSet<String> children = kb.kbCache.getChildClasses(bareClass);
+            ArrayList<String> cs = new ArrayList<>();
+            cs.addAll(children);
+            if (children == null || children.size() == 0)
+                skip = true;
+            else {
+                int rint = rand.nextInt(cs.size());
+                arg = cs.get(rint);
+            }
+        }
+        return arg;
+    }
+
+    /** ***************************************************************
+     * generate new SUMO statements for relations using the set of
+     * available instances for each argument type and output English
+     * paraphrase
+     */
+    public static ArrayList<Formula> genFormulas(String rel, ArrayList<String> sig,
+                                                 HashMap<String, ArrayList<String>> instMap) {
+
+        ArrayList<StringBuffer> forms = new ArrayList<>();
+        for (int i = 1; i < sig.size(); i++) {
+            String currT = sig.get(i);
+            if (currT.endsWith("+"))
+                return new ArrayList<Formula>(); // bail out if there is a subclass argument
+        }
+
+        StringBuffer form = new StringBuffer();
+        form.append("(" + rel + " ");
+        forms.add(form);
+
+        for (int i = 1; i < sig.size(); i++) {
+            ArrayList<StringBuffer> newforms = new ArrayList<>();
+            String currT = sig.get(i);
+            if (debug) System.out.println("genFormula() currT: " + currT);
+            if (instMap.get(currT) == null || instMap.get(currT).size() < 1)
+                return new ArrayList<Formula>();
+            int max = instMap.get(currT).size();
+            if (max > instLimit)
+                max = instLimit;
+            for (int j = 0; j < max; j++) {
+                String arg = "";
+                arg = instMap.get(currT).get(j);
+                for (StringBuffer sb : forms) {
+                    StringBuffer f = new StringBuffer(sb);
+                    f.append(arg + " ");
+                    newforms.add(f);
+                }
+            }
+            forms = newforms;
+        }
+
+        ArrayList<Formula> formsList = new ArrayList<>();
+        for (StringBuffer sb : forms) {
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append(")");
+            Formula f = new Formula(sb.toString());
+            formsList.add(f);
+        }
+        return formsList;
+    }
+
+    /** ***************************************************************
+     * handle quantities
+     */
+    public static void handleQuantity(String t, HashMap<String, ArrayList<String>> instMap) {
+
+        TreeSet<String> instances = kb.getAllInstances(t);
+        if (instances.size() < 1) {
+            System.out.println("handleQuantity(): no instances for " + t);
+            return;
+        }
+        ArrayList<String> arInsts = new ArrayList<>();
+        arInsts.addAll(instances);
+        int rint = rand.nextInt(instances.size());
+        String inst = arInsts.get(rint); // get an instance of a quantity
+        float num = rand.nextFloat() * 100;
+        String f = "(MeasureFn " + num + " " + inst + ")";
+        if (instMap.containsKey(t)) {
+            ArrayList<String> insts = instMap.get(t);
+            insts.add(f);
+        }
+        else {
+            ArrayList<String> insts = new ArrayList<>();
+            insts.add(f);
+            instMap.put(t,insts);
+        }
+    }
+
+    /** ***************************************************************
+     * handle the case where the argument type is not a subclass
+     */
+    public static void handleNonClass(String t, HashMap<String, ArrayList<String>> instMap) {
+
+        System.out.println("handleNonClass(): t: " + t);
+        HashSet<String> hinsts = kb.kbCache.getInstancesForType(t);
+        if (hinsts.contains("statementPeriod"))
+            System.out.println("handleNonClass(): hinsts: " + hinsts);
+        ArrayList<String> insts = new ArrayList<>();
+        insts.addAll(hinsts);
+        if (debug) System.out.println("handleNonClass(): insts: " + insts);
+        if (insts.size() > 0) {
+            if (instMap.containsKey(t)) {
+                ArrayList<String> oldinsts = instMap.get(t);
+                oldinsts.addAll(insts);
+            }
+            else
+                instMap.put(t,insts);
+        }
+        else {
+            String term = t + "1";
+            if (debug) System.out.println("handleNonClass(2): t: " + t);
+            String lang = "EnglishLanguage";
+            insts.add(term);
+            if (debug) System.out.println("handleNonClass(): insts(2): " + insts);
+            //System.out.println("handleNonClass(): term format size: " + kb.getTermFormatMap(lang).keySet().size());
+            //System.out.println("handleNonClass(): containsKey: " + kb.getTermFormatMap(lang).containsKey(t));
+            //System.out.println("handleNonClass(): termFormat: " + kb.getTermFormatMap(lang).get(t));
+            String fString = "a " + kb.getTermFormatMap(lang).get(t); // kb.getTermFormat(lang,t);
+            String form = "(termFormat EnglishLanguage " + term + " \"" + fString + "\")";
+            HashMap<String, String> langTermFormatMap = kb.getTermFormatMap(lang);
+            langTermFormatMap.put(term, fString);
+            //System.out.println(form);
+            kb.tell(form);
+            instMap.put(t, insts);
+        }
+        if (debug) System.out.println("handleNonClass(): instMap: " + instMap);
+    }
+
+    /** ***************************************************************
+     * generate new SUMO statements for relations and output English
+     * paraphrase
+     */
+    public static String toEnglish(String form) {
+
+        return NLGUtils.htmlParaphrase("", form, kb.getFormatMap("EnglishLanguage"),
+                kb.getTermFormatMap("EnglishLanguage"), kb, "EnglishLanguage") + "\n";
+    }
+
+    /** ***************************************************************
+     * generate new SUMO termFormat statements for constants in a file
+     */
+    public static void genTermFormatFromNames(String fname) {
+
+        try {
+            File constituent = new File(fname);
+            String canonicalPath = constituent.getCanonicalPath();
+            KIF kif = new KIF(canonicalPath);
+            kif.setParseMode(KIF.RELAXED_PARSE_MODE);
+            kif.readFile(fname);
+            for (Formula f : kif.formulaMap.values()) {
+                if (f.car().equals("instance")) {
+                    String t = f.getStringArgument(1);
+                    String s = StringUtil.camelCaseToSep(t);
+                    System.out.println("(termFormat EnglishLanguage " + t + " \"" + s + "\")");
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** ***************************************************************
+     * generate new SUMO termFormat and instance statements for names
+     */
+    public static void genHumans() {
+
+        ArrayList<ArrayList<String>> fn = DB.readSpreadsheet("/home/apease/workspace/sumo" +
+                File.separator +"WordNetMappings/FirstNames.csv", null, false, ',');
+        for (ArrayList<String> ar : fn) {
+            String firstName = ar.get(0);
+            String g = ar.get(1);
+            if (firstName != null) {
+                String gender = "Male";
+                if (g.toUpperCase().equals("F"))
+                    gender = "Female";
+                System.out.println("(instance " + firstName + " Human)");
+                System.out.println("(attribute " + firstName + " " + gender + ")");
+                System.out.println("(names \"" + firstName + "\" " + firstName + ")");
+            }
+        }
+    }
+
+    /** ***************************************************************
+     * generate new SUMO statements for relations and output English
+     * paraphrase
+     */
+    public static void genStatements(HashMap<String, String> formatMap) {
+
+        for (String rel : kb.kbCache.relations) {
+            skip = false;
+            if (formatMap.get(rel) != null && !kb.isFunction(rel)) {
+                boolean skip = false;
+                if (debug) System.out.println("genStatements()  rel: " + rel);
+                ArrayList<String> sig = kb.kbCache.getSignature(rel);
+                HashMap<String, ArrayList<String>> instMap = new HashMap<>();
+                if (debug) System.out.println("sig: " + sig);
+                for (String t : sig) {
+                    if (skipTypes.contains(t))
+                        skip = true;
+                    if (StringUtil.emptyString(t) || skipTypes.contains(t))
+                        continue;
+                    if (debug) System.out.println("genStatements() t: " + t);
+                    if (!t.endsWith("+") && !kb.isSubclass(t,"Quantity")) {
+                        handleNonClass(t,instMap);
+                    }
+                    else if (kb.isSubclass(t,"Quantity")) {
+                        if (debug) System.out.println("genStatements(): found quantity for : " + rel);
+                        handleQuantity(t, instMap);
+                    }
+                }
+                if (!skip) {
+                    ArrayList<Formula> forms = genFormulas(rel,sig,instMap);
+                    for (Formula f : forms) {
+                        String form = f.getFormula();
+                        if (!StringUtil.emptyString(form)) {
+                            System.out.println(form);
+                            String actual = toEnglish(form);
+                            System.out.println(StringUtil.filterHtml(actual));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /** ***************************************************************
+     * generate English for all ground relation statements
+     */
+    public static void handleGroundStatements(HashMap<String, String> formatMap ) {
+
+        HashSet<Formula> forms = new HashSet<>();
+        forms.addAll(kb.formulaMap.values());
+        for (Formula f : forms) {
+            if (f.isGround() && formatMap.containsKey(f.relation) && !StringUtil.emptyString(f.toString()))
+                System.out.println(toEnglish(f.toString()));
+        }
+    }
+
+    /** ***************************************************************
+     * init and call main routine.
+     */
+    public static void generate() {
+
+        System.out.println("GenSimpTestData.generate()");
+        KBmanager.getMgr().initializeOnce();
+        //resultLimit = 0; // don't limit number of results on command line
+        kb = KBmanager.getMgr().getKB(KBmanager.getMgr().getPref("sumokbname"));
+        System.out.println("generate(): # relations: " + kb.kbCache.relations.size());
+        HashMap<String, String> formatMap = kb.getFormatMap("EnglishLanguage");
+        skipTypes.addAll(Arrays.asList("Formula") );
+        handleGroundStatements(formatMap);
+        genStatements(formatMap);
+    }
+
+    /** ***************************************************************
+     */
+    public static void testTypes() {
+
+        System.out.println("GenSimpTestData.testTypes()");
+        KBmanager.getMgr().initializeOnce();
+        kb = KBmanager.getMgr().getKB(KBmanager.getMgr().getPref("sumokbname"));
+        String t = "Object";
+        HashSet<String> hinsts = kb.kbCache.getInstancesForType(t);
+        System.out.println(hinsts);
+        System.out.println("Object has instance BinaryPredicate: " + hinsts.contains("BinaryPredicate"));
+        System.out.println("signature of 'half': " + kb.kbCache.getSignature("half"));
+    }
+
+    /** ***************************************************************
+     * init and call main routine.
+     */
+    public static void main(String args[]) {
+
+        //testTypes();
+        generate();
+        //genTermFormatFromNames("/home/apease/workspace/sumo/WorldAirports.kif");
+        //genHumans();
+    }
+}
