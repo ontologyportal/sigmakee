e8398eb4c070ff0d7ef5795353fc35e3a189fe32
apease <apease@articulatesoftware.com>
2022-10-15 08:58:27 +0200

    may changes to generate more/different NL/logic pairs

--- src/java/com/articulate/sigma/mlpipeline/GenSimpTestData.java
+++ src/java/com/articulate/sigma/mlpipeline/GenSimpTestData.java
@@ -1,7 +1,9 @@
 package com.articulate.sigma.mlpipeline;
 
 import com.articulate.sigma.*;
+import com.articulate.sigma.nlg.LanguageFormatter;
 import com.articulate.sigma.nlg.NLGUtils;
+import com.articulate.sigma.utils.AVPair;
 import com.articulate.sigma.utils.StringUtil;
 
 import java.io.*;
@@ -16,6 +18,7 @@ public class GenSimpTestData {
     public static HashSet<String> skipTypes = new HashSet<>();
     public static final int instLimit = 500;
     public static PrintWriter pw = null;
+    public static final int loopMax = 2;
 
     /** ***************************************************************
      * handle the case where the argument type is a subclass
@@ -195,13 +198,29 @@ public class GenSimpTestData {
     /** ***************************************************************
      * generate new SUMO termFormat and instance statements for names
      */
-    public static void genHumans() {
+    public static Collection<AVPair> readHumans() {
 
+        Collection<AVPair> result = new HashSet<>();
         ArrayList<ArrayList<String>> fn = DB.readSpreadsheet("/home/apease/workspace/sumo" +
-                File.separator +"WordNetMappings/FirstNames.csv", null, false, ',');
+                File.separator + "WordNetMappings/FirstNames.csv", null, false, ',');
         for (ArrayList<String> ar : fn) {
             String firstName = ar.get(0);
             String g = ar.get(1);
+            AVPair avp = new AVPair(firstName, g);
+            result.add(avp);
+        }
+        return result;
+    }
+
+    /** ***************************************************************
+     * generate new SUMO termFormat and instance statements for names
+     */
+    public static void genHumans() {
+
+        Collection<AVPair> avps = readHumans();
+        for (AVPair avp : avps) {
+            String firstName = avp.attribute;
+            String g = avp.value;
             if (firstName != null) {
                 String gender = "Male";
                 if (g.toUpperCase().equals("F"))
@@ -213,6 +232,25 @@ public class GenSimpTestData {
         }
     }
 
+    /** ***************************************************************
+     * generate missing SUMO termFormat statements
+     */
+    public static void genMissingTermFormats() {
+
+        System.out.println("GenSimpTestData.genMissingTermFormats(): start");
+        KBmanager.getMgr().initializeOnce();
+        kb = KBmanager.getMgr().getKB(KBmanager.getMgr().getPref("sumokbname"));
+        for (String s : kb.terms) {
+            ArrayList<Formula> res = kb.askWithRestriction(0,"termFormat",2,s);
+            if (res == null || res.size() == 0) {
+                boolean inst = false;
+                if (kb.isInstance(s))
+                    inst = true;
+                String news = StringUtil.camelCaseToSep(s,true,inst);
+                System.out.println("(termFormat EnglishLanguage " + s + " \"" + news + "\")");
+            }
+        }
+    }
     /** ***************************************************************
      * generate new SUMO statements for relations and output English
      * paraphrase
@@ -314,6 +352,9 @@ public class GenSimpTestData {
         kb = KBmanager.getMgr().getKB(KBmanager.getMgr().getPref("sumokbname"));
         String s = "(=> (and (valence ?REL ?NUMBER) (instance ?REL Predicate)) (forall (@ROW) (=> (?REL @ROW) (equal (ListLengthFn (ListFn @ROW)) ?NUMBER))))";
         String actual = toEnglish(s);
+        System.out.println("Strike:" + kb.getTermFormat("EnglishLanguage","BaseballStrike"));
+        System.out.println("Strike2:" + kb.getTermFormatMap("EnglishLanguage").get("BaseballStrike"));
+        System.out.println("Strike3:" + LanguageFormatter.translateWord(kb.getTermFormatMap("EnglishLanguage"),"BaseballStrike"));
         System.out.println(actual);
     }
 
@@ -333,9 +374,183 @@ public class GenSimpTestData {
 
     /** ***************************************************************
      */
-    public static void genActions() {
+    public class Preposition {
+        public String procType = null;
+        public String prep = null;
+        public String noun = null;
+    }
 
+    /** ***************************************************************
+     * @return objects
+     */
+    public void addArguments(Collection<String> col, boolean dob, boolean incPrep,
+                             HashSet<Preposition> objects) {
 
+        for (String s : col) {
+            Preposition p = this.new Preposition();
+            p.prep = "";
+            p.noun = s;
+            objects.add(p);
+        }
+    }
+
+    /** ***************************************************************
+     * Create action sentences from a subject, preposition, direct object,
+     * preposition and indirect object.  Indirect object and its preposition
+     * can be left out.  Actions will eventually be past and future tense or
+     * wrapped in modals.
+     */
+    public void initActions() {
+
+        System.out.println("GenSimpTestData.initActions(): start");
+        KBmanager.getMgr().initializeOnce();
+        kb = KBmanager.getMgr().getKB(KBmanager.getMgr().getPref("sumokbname"));
+        System.out.println("GenSimpTestData.initActions(): finished loading KBs");
+        //artifact, organic object
+        TreeSet<String> socRole = kb.getAllInstances("SocialRole");
+        TreeSet<String> artInst = kb.getAllInstances("Artifact");
+        HashSet<String> artClass = kb.kbCache.getChildClasses("Artifact");
+        HashSet<String> intProc = kb.kbCache.getChildClasses("DualObjectProcess");
+        TreeSet<String> orgInst = kb.getAllInstances("OrganicObject");
+        HashSet<String> orgClass = kb.kbCache.getChildClasses("OrganicObject");
+        HashSet<Preposition> objects = new HashSet<>();
+        addArguments(artInst, false, false, objects);
+        addArguments(artClass, false, false, objects);
+        addArguments(orgInst, false, false, objects);
+        addArguments(orgClass, false, false, objects);
+        HashSet<Preposition> indirect = new HashSet<>();
+        //  get capabilities from axioms like
+        //  (=> (instance ?GUN Gun) (capability Shooting instrument ?GUN))
+        ArrayList<Formula> forms = kb.ask("cons",0,"capability");
+        for (Formula f : forms) {
+            String ant = FormulaUtil.antecedent(f);
+            Formula fant = new Formula(ant);
+            if (fant.isSimpleClause(kb) && fant.car().equals("instance")) {
+                String antClass = fant.getStringArgument(2);
+                String cons = FormulaUtil.consequent(f);
+                Formula fcons = new Formula(cons);
+                if (fcons.isSimpleClause(kb)) {
+                    String consClass = fcons.getStringArgument(1);
+                    String rel = fcons.getStringArgument(2);
+                    Preposition p = this.new Preposition();
+                    p.procType = consClass;
+                    p.prep = rel;
+                    p.noun = antClass;
+                    indirect.add(p);
+                }
+            }
+        }
+        System.out.println("GenSimpTestData.initActions() finished collecting terms");
+        Collection<AVPair> humans = readHumans();
+        genActions(humans,socRole,intProc,objects,indirect);
+    }
+
+    /** ***************************************************************
+     * also return true if there's no termFormat for the process
+     */
+    public boolean compoundVerb(String term) {
+
+        String word = kb.getTermFormat("EnglishLanguage",term);
+        if (word == null || word.contains(" ")) {
+            System.out.println("compoundVerb(): or null: " + word + " " + term);
+            return true;
+        }
+        return false;
+    }
+
+    /** ***************************************************************
+     */
+    public String verbForm(String term) {
+
+        String word = kb.getTermFormat("EnglishLanguage",term);
+        if (word == null) {
+            System.out.println("verbForm(): no term format for " + term);
+            return null;
+        }
+        if (word.endsWith("ing"))
+            return "is " + word;
+        return word + "s";
+    }
+
+    /** ***************************************************************
+     */
+    public String nounForm(String term) {
+
+        String word = kb.getTermFormat("EnglishLanguage",term);
+        boolean subst = kb.isSubclass(term,"Substance");
+        if (word == null) {
+            System.out.println("nounForm(): no term format for " + term);
+            return null;
+        }
+        if (kb.isInstance(term))
+            return "the " + word;
+        if (subst)
+            return "some " + word;
+        if (word.matches("^[aeiouh].*"))
+            return "an " + word;
+        return "a " + word;
+    }
+
+    /** ***************************************************************
+     * Create action sentences from a subject, preposition, direct object,
+     * preposition and indirect object.  Indirect object and its preposition
+     * can be left out.  Actions will eventually be past and future tense or
+     * wrapped in modals.
+     */
+    public void genActions(Collection<AVPair> humans,
+                           Collection<String> roles,
+                           Collection<String> intProc,
+                           Collection<Preposition> direct,
+                           Collection<Preposition> indirect) {
+
+        System.out.println("GenSimpTestData.genActions()");
+        int humCount = 0;
+        for (AVPair avp : humans) {
+            if (humCount++ > loopMax) break;
+            int procCount = 0;
+            for (String proc : intProc) {
+                if (compoundVerb(proc)) { continue; } // too hard grammatically for now to have compound verbs
+                if (procCount++ > loopMax) break;
+                int directCount = 0;
+                for (Preposition dprep : direct) {
+                    if (directCount++ > loopMax) break;
+                    int indCount = 0;
+                    boolean onceWithoutInd = false;
+                    for (Preposition indprep : indirect) {
+                        if (indCount++ > loopMax) break;
+                        StringBuffer english = new StringBuffer();
+                        StringBuffer prop = new StringBuffer();
+                        english.append(avp.attribute + " ");
+                        prop.append("(exists (?H ?P ?DO ?IO) (and ");
+                        prop.append("(instance ?H Human) ");
+                        prop.append("(names \"" + avp.attribute + "\" ?H) ");
+                        english.append(verbForm(proc) + " ");
+                        prop.append("(instance ?P " + proc + ") (agent ?P ?H) ");
+                        english.append(nounForm(dprep.noun) + " ");
+                        prop.append("(instance ?DO " + dprep.noun + ") (patient ?P ?DO) ");
+                        if (kb.isSubclass(proc,indprep.procType)) {
+                            english.append("with " + nounForm(indprep.noun));
+                            prop.append("(instance ?IO " + indprep.noun + ") (instrument ?P ?IO)))");
+                            onceWithoutInd = false;
+                        }
+                        else {
+                            prop.append("))");
+                            indCount = 0;
+                            onceWithoutInd = true;
+                            if (!onceWithoutInd) {
+                                System.out.println(english.toString());
+                                System.out.println(prop.toString() + "\n");
+                                onceWithoutInd = true;
+                            }
+                        }
+                        if (!onceWithoutInd) {
+                            System.out.println(english.toString());
+                            System.out.println(prop.toString() + "\n");
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /** ***************************************************************
@@ -352,7 +567,10 @@ public class GenSimpTestData {
         }
         //testTypes();
         //generate();
-        allAxioms();
+        //allAxioms();
+        GenSimpTestData gstd = new GenSimpTestData();
+        gstd.initActions();
+        //genMissingTermFormats();
         //testNLG();
         //genTermFormatFromNames("/home/apease/workspace/sumo/WorldAirports.kif");
         //genHumans();
