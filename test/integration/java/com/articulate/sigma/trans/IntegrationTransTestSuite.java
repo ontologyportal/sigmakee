4540c9b10f8f8316955307df33391ba5ebb76cf8
git <tdnorbra@nps.edu>
2024-10-16 14:18:28 -0700

    [Terry N.] add test suite for pkg

---
+++ test/integration/java/com/articulate/sigma/trans/IntegrationTransTestSuite.java
@@ -0,0 +1,23 @@
+package com.articulate.sigma.trans;
+
+import com.articulate.sigma.nlg.*;
+import com.articulate.sigma.*;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//This software is released under the GNU Public License
+//<http://www.gnu.org/copyleft/gpl.html>.
+// Copyright 2019 Infosys
+// adam.pease@infosys.com
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    SUMOtoTFAKBTest.class,
+    SUMOtoTFAformTest.class,
+    THFtest.class,
+    TPTP2SUMOTest.class
+})
+public class IntegrationTransTestSuite extends UnitTestBase {
+
+}
