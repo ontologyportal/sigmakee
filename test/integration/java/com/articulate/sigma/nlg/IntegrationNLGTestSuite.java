16448317e22b6dd991540a0950fbbfeef9787783
git <tdnorbra@nps.edu>
2024-10-16 14:18:13 -0700

    [Terry N.] add test suite for pkg

---
+++ test/integration/java/com/articulate/sigma/nlg/IntegrationNLGTestSuite.java
@@ -0,0 +1,20 @@
+package com.articulate.sigma.nlg;
+
+import com.articulate.sigma.VerbNet.*;
+import com.articulate.sigma.*;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//This software is released under the GNU Public License
+//<http://www.gnu.org/copyleft/gpl.html>.
+// Copyright 2019 Infosys
+// adam.pease@infosys.com
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    HtmlParaphraseIntegrationTest.class
+})
+public class IntegrationNLGTestSuite extends UnitTestBase {
+
+}
