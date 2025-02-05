79c54244e8580d77277b2c5e7f1136e51a8fa0e8
git <tdnorbra@nps.edu>
2025-02-04 19:44:05 -0800

    [Terry N.] add annotation

--- src/java/com/articulate/sigma/StreamTokenizer_s.java
+++ src/java/com/articulate/sigma/StreamTokenizer_s.java
@@ -2,11 +2,11 @@
 copyright Teknowledge (c) 2003 and reused under the terms of the GNU license.
 This software is released under the GNU Public License <http://www.gnu.org/copyleft/gpl.html>.
 Users of this code also consent, by use of this code, to credit Articulate Software
-and Teknowledge in any writings, briefings, publications, presentations, or 
-other representations of any software which incorporates, builds on, or uses this 
+and Teknowledge in any writings, briefings, publications, presentations, or
+other representations of any software which incorporates, builds on, or uses this
 code.  Please cite the following article in any publication with references:
 
-Pease, A., (2003). The Sigma Ontology Development Environment, 
+Pease, A., (2003). The Sigma Ontology Development Environment,
 in Working Notes of the IJCAI-2003 Workshop on Ontology and Distributed Systems,
 August 9, Acapulco, Mexico.
 */
@@ -781,6 +781,7 @@ public class StreamTokenizer_s {
      * @see     java.io.StreamTokenizer_s#sval
      * @see     java.io.StreamTokenizer_s#ttype
      */
+    @Override
     public String toString() {
 	String ret;
 	switch (ttype) {
