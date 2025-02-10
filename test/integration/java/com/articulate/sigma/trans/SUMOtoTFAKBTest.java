82d0989b4037dc400c74f5673bfa3fb59ae837df
git <tdnorbra@nps.edu>
2025-02-09 22:36:29 -0800

    [Terry N.] use syserr for errors

--- test/integration/java/com/articulate/sigma/trans/SUMOtoTFAKBTest.java
+++ test/integration/java/com/articulate/sigma/trans/SUMOtoTFAKBTest.java
@@ -45,7 +45,7 @@ public class SUMOtoTFAKBTest extends IntegrationTestBase {
         if (expectedRes.equals(result))
             System.out.println("testPartition(): Success!");
         else
-            System.out.println("testPartition(): fail");
+            System.err.println("testPartition(): fail");
         assertTrue(expectedRes.equals(result));
     }
 
@@ -64,7 +64,7 @@ public class SUMOtoTFAKBTest extends IntegrationTestBase {
         if (expectedRes.equals(result))
             System.out.println("testDynamicSortDef(): Success!");
         else
-            System.out.println("testDynamicSortDef(): fail");
+            System.err.println("testDynamicSortDef(): fail");
         assertTrue(expectedRes.equals(result));
     }
 
@@ -84,7 +84,7 @@ public class SUMOtoTFAKBTest extends IntegrationTestBase {
                 "s__instance(s__intelligenceQuotient__m, s__Predicate) & " +
                 "s__intelligenceQuotient__2Re(V__ROW1, V__ROW2)) => " +
                 "s__instance(s__ListOrderFn__2InFn(s__ListFn__2ReFn(V__ROW1, V__ROW2), V__NUMBER), V__CLASS))";
-        HashSet<String> result = stfa.missingSorts(new Formula(f));
+        Set<String> result = stfa.missingSorts(new Formula(f));
         String expectedRes = "tff(listFn__2ReFn_sig,type,s__ListFn__2ReFn : (  $i * $real  ) > $i ).";
         String resultStr = "";
         if (result != null && !result.isEmpty())
@@ -94,7 +94,7 @@ public class SUMOtoTFAKBTest extends IntegrationTestBase {
         if (result != null && result.contains(expectedRes))
             System.out.println("testMissingSort(): Success!");
         else
-            System.out.println("testMissingSort(): fail");
+            System.err.println("testMissingSort(): fail");
         assertTrue(result.contains(expectedRes));
     }
 }
