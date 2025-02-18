f07cea7cd84aef379dcf6499b2abb7f3ec2e2f1c
git <tdnorbra@nps.edu>
2025-02-17 18:16:24 -0800

    [Terry N.] var dec. out of loops

--- src/java/com/articulate/sigma/BasicXMLparser.java
+++ src/java/com/articulate/sigma/BasicXMLparser.java
@@ -2,11 +2,11 @@
 copyright Teknowledge (c) 2003 and reused under the terms of the GNU license.
 This software is released under the GNU Public License <http://www.gnu.org/copyleft/gpl.html>.
 Users of this code also consent, by use of this code, to credit Articulate Software
-and Teknowledge in any writings, briefings, publications, presentations, or 
-other representations of any software which incorporates, builds on, or uses this 
+and Teknowledge in any writings, briefings, publications, presentations, or
+other representations of any software which incorporates, builds on, or uses this
 code.  Please cite the following article in any publication with references:
 
-Pease, A., (2003). The Sigma Ontology Development Environment, 
+Pease, A., (2003). The Sigma Ontology Development Environment,
 in Working Notes of the IJCAI-2003 Workshop on Ontology and Distributed Systems,
 August 9, Acapulco, Mexico.
 */
@@ -15,9 +15,11 @@ package com.articulate.sigma;
 
 import java.io.IOException;
 import java.io.LineNumberReader;
+import java.io.Reader;
 import java.io.StringReader;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.List;
 
 /** ***************************************************************
  * Parses simple XML into a hierarchy of BasicXMLelement (s).  Used
@@ -30,7 +32,7 @@ import java.util.ArrayList;
 public class BasicXMLparser {
 
      /** An ArrayList of BasicXMLelement(s). */
-    public ArrayList<BasicXMLelement> elements = new ArrayList<BasicXMLelement>();
+    public List<BasicXMLelement> elements = new ArrayList<>();
 
     /** ***************************************************************
      * Constructor that parses an XML-formatted string, with one tag per
@@ -38,9 +40,9 @@ public class BasicXMLparser {
      */
     public BasicXMLparser(String xml) {
 
-        StringReader sr = new StringReader(xml);
-        LineNumberReader lnr = new LineNumberReader(sr);
-        try {
+
+        try (Reader sr = new StringReader(xml);
+             LineNumberReader lnr = new LineNumberReader(sr)) {
             BasicXMLelement el = new BasicXMLelement();
             parse(lnr,el);
             elements = el.subelements;
@@ -61,51 +63,55 @@ public class BasicXMLparser {
      * Assume that each line has only one tag.
      */
     private void parse(LineNumberReader lnr, BasicXMLelement element) throws ParseException, IOException {
-        
-        String newTag = null;
-        boolean inQuote = false;
+
+        String newTag;
+        boolean inQuote;
+        String line, endTagString, nameString, valueString;
+        int tagStart, tagEnd, name, value;
+        BasicXMLelement newElement;
+        char valueEnd;
         while (lnr.ready()) {
-            String line = lnr.readLine();
+            line = lnr.readLine();
             inQuote = false;
             if (line == null) {
                 System.out.println("INFO in BasicXMLparser.parse: Exiting with line = null.");
                 return;
             }
             line = line.trim();
-            int tagStart = line.indexOf('<');
+            tagStart = line.indexOf('<');
             if (tagStart != 0) {                        // It's not a tag
-                element.contents = element.contents + (new String(line.trim()));
+                element.contents = element.contents + (line.trim());
                 System.out.println("INFO in BasicXMLparser.parse: Adding contents: " + line);
                 continue;
             }
             if (line.charAt(tagStart+1) == '/') {      // Found a closing tag
-                String endTagString = line.substring(tagStart+2,line.indexOf('>'));
+                endTagString = line.substring(tagStart+2,line.indexOf('>'));
                 if (element.tagname == null)
-                    throw new ParseException("Error in BasicXMLparser.parse(): Closing tag " + line + " without open tag, at line: ", lnr.getLineNumber());                
-                if (endTagString.equalsIgnoreCase(element.tagname)) 
-                    return;                
+                    throw new ParseException("Error in BasicXMLparser.parse(): Closing tag " + line + " without open tag, at line: ", lnr.getLineNumber());
+                if (endTagString.equalsIgnoreCase(element.tagname))
+                    return;
                 else
                     throw new ParseException("Error in BasicXMLparser.parse(): Close tag " + endTagString + " doesn't match open tag " + element.tagname, lnr.getLineNumber());
             }
             else {                                    // An opening or combined open/close tag - like <foo/>
-                int tagEnd = tagStart + 1;
-                while (tagEnd < line.length() && Character.isJavaIdentifierPart(line.charAt(tagEnd))) 
+                tagEnd = tagStart + 1;
+                while (tagEnd < line.length() && Character.isJavaIdentifierPart(line.charAt(tagEnd)))
                     tagEnd++;
                 newTag = line.substring(line.indexOf('<') + 1,tagEnd);
-                BasicXMLelement newElement = new BasicXMLelement();
+                newElement = new BasicXMLelement();
                 newElement.tagname = newTag;
                 if (line.charAt(tagEnd) == ' ') {     // The tag has attributes
                     do {
                         tagEnd++;
-                        int name = tagEnd;
-                        while (tagEnd < line.length() && Character.isJavaIdentifierPart(line.charAt(tagEnd))) 
+                        name = tagEnd;
+                        while (tagEnd < line.length() && Character.isJavaIdentifierPart(line.charAt(tagEnd)))
                             tagEnd++;
-                        String nameString = line.substring(name,tagEnd);
-                        if (line.charAt(tagEnd) != '=') 
+                        nameString = line.substring(name,tagEnd);
+                        if (line.charAt(tagEnd) != '=')
                             throw new ParseException("Error in BasicXMLparser.parse(): Name without value: " + nameString, lnr.getLineNumber());
                         tagEnd++;
-                        int value = tagEnd;
-                        char valueEnd = ' ';
+                        value = tagEnd;
+                        valueEnd = ' ';
                         if (line.charAt(value) == '\'') {
                             value++;
                             tagEnd++;
@@ -118,15 +124,15 @@ public class BasicXMLparser {
                             valueEnd = '\"';
                             inQuote = !inQuote;
                         }
-                        while (tagEnd < line.length() && line.charAt(tagEnd) != valueEnd && 
+                        while (tagEnd < line.length() && line.charAt(tagEnd) != valueEnd &&
                                (line.charAt(tagEnd) != '>' || inQuote)) {
                             tagEnd++;
-                            if (line.charAt(tagEnd) == '\"' || line.charAt(tagEnd) == '\'') 
+                            if (line.charAt(tagEnd) == '\"' || line.charAt(tagEnd) == '\'')
                                 inQuote = !inQuote;
                         }
-                        String valueString = line.substring(value,tagEnd);
+                        valueString = line.substring(value,tagEnd);
 
-                        if (line.charAt(tagEnd) == valueEnd) 
+                        if (line.charAt(tagEnd) == valueEnd)
                             tagEnd++;
                         newElement.attributes.put(nameString,valueString);
                     } while (line.charAt(tagEnd) == ' ');
@@ -136,24 +142,24 @@ public class BasicXMLparser {
                     parse(lnr,newElement);
                     element.subelements.add(newElement);
                 }
-                else if (line.charAt(tagEnd) == '/' && 
+                else if (line.charAt(tagEnd) == '/' &&
                          (line.charAt(tagEnd + 1) == '>' && !inQuote)) {
                     newTag = "";
                     element.subelements.add(newElement);
                 }
             }
         }
-        return;
     }
-   
+
     /** ***************************************************************
      * Convert the XML hierarchy to a String.
      */
     public String toString() {
 
-        StringBuffer result = new StringBuffer();
+        StringBuilder result = new StringBuilder();
+        BasicXMLelement element;
         for (int i = 0; i < elements.size(); i++) {
-            BasicXMLelement element = elements.get(i);
+            element = elements.get(i);
             result = result.append(element.toString());
         }
         return result.toString();
@@ -188,7 +194,7 @@ public class BasicXMLparser {
         System.out.println(bp.elements.size());
         System.out.println(bp.toString());
 
-        /* 
+        /*
     <queryResponse>
       <answer result='yes' number='1'>
         <bindingSet type='definite'>
