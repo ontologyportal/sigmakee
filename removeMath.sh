03dffa081610199b511f07d119283ff8edf4ce3b
git <tdnorbra@nps.edu>
2025-02-24 21:10:29 -0800

    [Terry N.] break out nix and macos functions

--- removeMath.sh
+++ removeMath.sh
@@ -1,7 +1,36 @@
 #!/bin/bash -e
 
-# Prepending the string expression with an empty string: '' lets it work on
-# macOS as well as other *nix O/S's
-sed -i '' 's/.*$to_real.*//g' $1
-sed -i '' 's/.*$remainder_t.*//g' $1
-sed -i '' 's/.*$quotient_e.*//g' $1
+UNAME=$( command -v uname)
+
+case $( "${UNAME}" | tr '[:upper:]' '[:lower:]') in
+  linux*)
+    nix
+    ;;
+  darwin*)
+    mac
+    ;;
+  msys*|cygwin*|mingw*)
+    # or possible 'bash on windows'
+    nix
+    ;;
+  nt|win*)
+    nix
+    ;;
+  *)
+    printf 'unknown o/s\n'
+    ;;
+esac
+
+nix() {
+    sed -i 's/.*$to_real.*//g' $1
+    sed -i 's/.*$remainder_t.*//g' $1
+    sed -i 's/.*$quotient_e.*//g' $1
+}
+
+mac() {
+    # Prepending the string expression with an empty string: '' lets it work on
+    # macOS by mitigating unknown label errors
+    sed -i '' 's/.*$to_real.*//g' $1
+    sed -i '' 's/.*$remainder_t.*//g' $1
+    sed -i '' 's/.*$quotient_e.*//g' $1
+}
