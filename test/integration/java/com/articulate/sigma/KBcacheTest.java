1baa89a0631b203d8718ee12e0c396219b229828
apease <apease@articulatesoftware.com>
2025-04-14 09:22:44 -0700

    added RealNumber to expected return set for testBuildChildrenRealNumber()
    Signed-off-by: apease <apease@articulatesoftware.com>

--- test/integration/java/com/articulate/sigma/KBcacheTest.java
+++ test/integration/java/com/articulate/sigma/KBcacheTest.java
@@ -76,7 +76,7 @@ public class KBcacheTest extends IntegrationTestBase {
         Set<String> expected = new TreeSet<>(Arrays.asList("RationalNumber","Integer","EvenInteger",
                 "OddInteger","PrimeNumber","NonnegativeInteger","PositiveInteger","NegativeInteger",
                 "IrrationalNumber","NonnegativeRealNumber","PositiveRealNumber","PositiveInteger",
-                "NegativeRealNumber","NegativeInteger","BinaryNumber"));
+                "NegativeRealNumber","NegativeInteger","BinaryNumber","RealNumber"));
 
         Set<String> actual = new TreeSet<>(cache.getChildClasses(parent));
 
