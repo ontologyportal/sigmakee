ebe8692f3041f7e9d44c49b314477a7e7e6b577c
git <tdnorbra@nps.edu>
2025-04-15 13:49:58 -0700

    [Terry N.] javadoc

--- src/java/com/articulate/sigma/KButilities.java
+++ src/java/com/articulate/sigma/KButilities.java
@@ -118,6 +118,10 @@ public class KButilities implements ServletContextListener {
     }
 
     /** *************************************************************
+     * Checks for consistent typing in the given formula
+     *
+     * @param kb the knowledge base to check typing against
+     * @param f the formula to check
      */
     public static boolean hasCorrectTypes(KB kb, Formula f) {
 
@@ -152,6 +156,10 @@ public class KButilities implements ServletContextListener {
     }
 
     /** *************************************************************
+     * Checks for overall validity of the given formula
+     *
+     * @param kb the knowledge base to check typing against
+     * @param form the formula to check
      */
     public static boolean isValidFormula(KB kb, String form) {
 
