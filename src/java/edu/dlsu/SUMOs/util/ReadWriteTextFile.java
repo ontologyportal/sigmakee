2748e265bb4a9fe247bda8ef36804f043081cfc4
git <tdnorbra@nps.edu>
2025-04-23 20:09:04 -0700

    [Terry N.] use try w/ resources

--- src/java/edu/dlsu/SUMOs/util/ReadWriteTextFile.java
+++ src/java/edu/dlsu/SUMOs/util/ReadWriteTextFile.java
@@ -12,54 +12,49 @@ import java.io.Writer;
 public class ReadWriteTextFile {
 
   /**
-  * Fetch the entire contents of a text file, and return it in a String.
-  * This style of implementation does not throw Exceptions to the caller.
-  *
-  * @param aFile is a file which already exists and can be read.
-  */
-  static public String getContents(File aFile) {
+   * Fetch the entire contents of a text file, and return it in a String.
+   * This style of implementation does not throw Exceptions to the caller.
+   *
+   * @param aFile is a file which already exists and can be read.
+   */
+  public static String getContents(File aFile) {
+
     //...checks on aFile are elided
     StringBuilder contents = new StringBuilder();
-    
-    try {
-      //use buffering, reading one line at a time
+
+    try ( //use buffering, reading one line at a time
       //FileReader always assumes default encoding is OK!
-      BufferedReader input =  new BufferedReader(new FileReader(aFile));
-      try {
-        String line = null; //not declared within while loop
-        /*
-        * readLine is a bit quirky :
-        * it returns the content of a line MINUS the newline.
-        * it returns null only for the END of the stream.
-        * it returns an empty String if two newlines appear in a row.
-        */
-        while (( line = input.readLine()) != null){
-          contents.append(line);
-          contents.append(System.getProperty("line.separator"));
-        }
-      }
-      finally {
-        input.close();
+      BufferedReader input = new BufferedReader(new FileReader(aFile))) {
+      String line; //not declared within while loop
+      /*
+       * readLine is a bit quirky :
+       * it returns the content of a line MINUS the newline.
+       * it returns null only for the END of the stream.
+       * it returns an empty String if two newlines appear in a row.
+       */
+      while ((line = input.readLine()) != null) {
+        contents.append(line);
+        contents.append(System.getProperty("line.separator"));
       }
     }
     catch (IOException ex){
       ex.printStackTrace();
     }
-    
+
     return contents.toString();
   }
 
   /**
-  * Change the contents of text file in its entirety, overwriting any
-  * existing text.
-  *
-  * This style of implementation throws all exceptions to the caller.
-  *
-  * @param aFile is an existing file which can be written to.
-  * @throws IllegalArgumentException if param does not comply.
-  * @throws FileNotFoundException if the file does not exist.
-  * @throws IOException if problem encountered during write.
-  */
+   * Change the contents of text file in its entirety, overwriting any
+   * existing text.
+   *
+   * This style of implementation throws all exceptions to the caller.
+   *
+   * @param aFile is an existing file which can be written to.
+   * @throws IllegalArgumentException if param does not comply.
+   * @throws FileNotFoundException if the file does not exist.
+   * @throws IOException if problem encountered during write.
+   */
   static public void setContents(File aFile, String aContents)
                                  throws FileNotFoundException, IOException {
     if (aFile == null) {
@@ -75,14 +70,10 @@ public class ReadWriteTextFile {
       throw new IllegalArgumentException("File cannot be written: " + aFile);
     }
 
-    //use buffering
-    Writer output = new BufferedWriter(new FileWriter(aFile));
-    try {
+    try ( //use buffering
+      Writer output = new BufferedWriter(new FileWriter(aFile))) {
       //FileWriter always assumes default encoding is OK!
-      output.write( aContents );
-    }
-    finally {
-      output.close();
+      output.write( aContents);
     }
   }
 
@@ -93,5 +84,5 @@ public class ReadWriteTextFile {
     setContents(testFile, "The content of this file has been overwritten...");
     //System.out.println("New file contents: " + getContents(testFile));
   }
-} 
+}
 
