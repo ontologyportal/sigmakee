6d3f1141d8d938720f39bc07a9f75f0064c08621
git <tdnorbra@nps.edu>
2025-07-01 21:26:26 -0700

    [Terry N.] observe interface convention

--- src/java/com/articulate/sigma/DisplayFilter.java
+++ src/java/com/articulate/sigma/DisplayFilter.java
@@ -2,11 +2,11 @@
 copyright Teknowledge (c) 2003 and reused under the terms of the GNU license.
 This software is released under the GNU Public License <http://www.gnu.org/copyleft/gpl.html>.
 Users of this code also consent, by use of this code, to credit Articulate Software
-and Teknowledge in any writings, briefings, publications, presentations, or 
-other representations of any software which incorporates, builds on, or uses this 
+and Teknowledge in any writings, briefings, publications, presentations, or
+other representations of any software which incorporates, builds on, or uses this
 code.  Please cite the following article in any publication with references:
 
-Pease, A., (2003). The Sigma Ontology Development Environment, 
+Pease, A., (2003). The Sigma Ontology Development Environment,
 in Working Notes of the IJCAI-2003 Workshop on Ontology and Distributed Systems,
 August 9, Acapulco, Mexico.
 */
@@ -19,7 +19,6 @@ package com.articulate.sigma;
  * allowed the calling context.
  */
 public interface DisplayFilter {
-
 
     /** ***************************************************************
      * Returns true if suoKifTerm may be displayed or included in the
@@ -36,7 +35,7 @@ public interface DisplayFilter {
      *
      * @return true or false
      */
-    public boolean isLegalForDisplay (DocGen dg, String suoKifTerm);
+    boolean isLegalForDisplay (DocGen dg, String suoKifTerm);
 
 }
 
