806db08f0d990c68e0b7ea4af75440e7673c8a39
git <tdnorbra@nps.edu>
2025-08-07 08:21:32 -0700

    [Terry N.] use Formula constants

--- src/java/com/articulate/sigma/SInE.java
+++ src/java/com/articulate/sigma/SInE.java
@@ -3,18 +3,18 @@
 <http://www.gnu.org/copyleft/gpl.html>.  Users of this code also consent,
 by use of this code, to credit Articulate Software in any
 writings, briefings, publications, presentations, or other representations
-of any software which incorporates, builds on, or uses this code.  
+of any software which incorporates, builds on, or uses this code.
 
 Please cite the following when describing SInE
 Hoder, K., (2008) The SUMO Inference Engine (SInE).  Master's thesis,
 Charles University, Prague.  See also http://www.cs.man.ac.uk/~hoderk/sine/
-                                           
+
 Please cite the following article in any publication with references
 when addressing Sigma in general:
 
 Pease, A., (2003). The Sigma Ontology Development Environment, in Working
 Notes of the IJCAI-2003 Workshop on Ontology and Distributed Systems,
-August 9, Acapulco, Mexico. See also http://sigmakee.sourceforge.net 
+August 9, Acapulco, Mexico. See also http://sigmakee.sourceforge.net
 */
 
 package com.articulate.sigma;
@@ -25,11 +25,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -57,7 +57,7 @@ public class SInE extends InferenceEngine {
         }
         return res;
     }
-    
+
     /** *************************************************************
      */
     public static class SInEFactory extends EngineFactory {
@@ -70,57 +70,58 @@ public class SInE extends InferenceEngine {
         @Override
         public InferenceEngine createFromKBFile(String kbFileName) {
                 return SInE.getNewInstance(kbFileName);
-        }      
+        }
     }
-    
+
     /** *************************************************************
      */
     public static EngineFactory getFactory() {
             return new SInEFactory();
     }
-    
+
     static Pattern symbolPattern=Pattern.compile("(?<!\\w|\\?|\\@)[a-zA-Z][_a-zA-Z0-9-]*");
     static Pattern quotedPattern=Pattern.compile("\"[^\"]*\"");
-    
-    static Set<String> nonSymbols=new HashSet<String>();
-    static {
-        nonSymbols.add("and");
-        nonSymbols.add("or");
-        nonSymbols.add("not");
-        nonSymbols.add("forall");
-        nonSymbols.add("exists");
-    }    
 
-    ArrayList<String> formulas;
+    static Set<String> nonSymbols=new HashSet<>();
+    static {
+        nonSymbols.add(Formula.AND);
+        nonSymbols.add(Formula.OR);
+        nonSymbols.add(Formula.XOR);
+        nonSymbols.add(Formula.NOT);
+        nonSymbols.add(Formula.UQUANT);
+        nonSymbols.add(Formula.EQUANT);
+    }
+
+    List<String> formulas;
     /***
      * These formulas will be always selected.
      * Actually formulas which contain no symbols are put here.
      */
-    ArrayList<String> mandatoryFormulas;
+    List<String> mandatoryFormulas;
     Hashtable<String, Set<String>> formSymbols;
     Hashtable<String, Integer> degrees;
-    
+
     /***
-     * Relation between symbols and formulas which represents the fact that 
+     * Relation between symbols and formulas which represents the fact that
      * a formula (axiom) defines meaning of a symbol.
-     * 
+     *
      * The meaning of strings is following:<br>
      * Map<Symbol, List<Formula>>
      */
     Map<String, List<String>> requirements;
     EngineFactory underlyingEngineFactory;
-    
+
     /** *************************************************************
      */
     private SInE(String kbFileName, EngineFactory ef) throws Exception {
 
         underlyingEngineFactory = ef;
-        formSymbols = new Hashtable<String, Set<String>>();
-        formulas = new ArrayList<String>();
-        mandatoryFormulas = new ArrayList<String>();
-        degrees = new Hashtable<String, Integer>();
-        requirements = new Hashtable<String, List<String>>();
-    
+        formSymbols = new Hashtable<>();
+        formulas = new ArrayList<>();
+        mandatoryFormulas = new ArrayList<>();
+        degrees = new Hashtable<>();
+        requirements = new Hashtable<>();
+
         String error = null;
         //System.out.println("INFO in SInE(): initializing");
         File kbFile = null;
@@ -133,13 +134,13 @@ public class SInE extends InferenceEngine {
                                              + "\n<br/>" + error + "\n<br/>");
             }
         }
-        
+
         if (error == null) {
             if (!kifFormat) {
                 //System.out.println("INFO in SInE(): non KIF");
-                ArrayList<String> forms = loadNonKif(kbFile);
+                List<String> forms = loadNonKif(kbFile);
                 if (!forms.isEmpty()) {
-                    formulas.ensureCapacity(forms.size());
+                    ((ArrayList)formulas).ensureCapacity(forms.size());
                     loadFormulas(forms);
                 }
             }
@@ -148,7 +149,7 @@ public class SInE extends InferenceEngine {
                 kif.setParseMode(KIF.RELAXED_PARSE_MODE);
                 kif.readFile(kbFile.getCanonicalPath());
                 if (!kif.formulaMap.keySet().isEmpty()) {
-                    formulas.ensureCapacity(kif.formulaMap.keySet().size());
+                    ((ArrayList)formulas).ensureCapacity(kif.formulaMap.keySet().size());
                     loadFormulas(kif.formulaMap.keySet());
                 }
             }
@@ -163,29 +164,29 @@ public class SInE extends InferenceEngine {
 
     /** *************************************************************
      */
-    public SInE(Iterable<String> formulaSource) { 
+    public SInE(Iterable<String> formulaSource) {
 
         //underlyingEngineFactory = Vampire.getFactory();
-        formSymbols = new Hashtable<String, Set<String>>();
-        formulas = new ArrayList<String>();
-        mandatoryFormulas = new ArrayList<String>();
-        degrees = new Hashtable<String, Integer>();
-        requirements = new Hashtable<String, List<String>>();
-    
+        formSymbols = new Hashtable<>();
+        formulas = new ArrayList<>();
+        mandatoryFormulas = new ArrayList<>();
+        degrees = new Hashtable<>();
+        requirements = new Hashtable<>();
+
         loadFormulas(formulaSource);
     }
 
     /** *************************************************************
      * Loads formulas from given source.
      */
-    public ArrayList<String> loadNonKif(File kbFile) {
+    public List<String> loadNonKif(File kbFile) {
 
-        ArrayList<String> result = new ArrayList<>();
-        LineNumberReader lr = null;
+        List<String> result = new ArrayList<>();
         String line = null;
-        try {
-            FileReader fr = new FileReader(kbFile);
-            lr = new LineNumberReader(fr);
+        LineNumberReader lnr = null;
+        try (Reader fr = new FileReader(kbFile);
+            LineNumberReader lr = new LineNumberReader(fr)) {
+            lnr = lr;
             while ((line = lr.readLine()) != null) {
                 if (!StringUtil.emptyString(line) && !line.startsWith("%")) {
                     result.add(line);
@@ -194,8 +195,8 @@ public class SInE extends InferenceEngine {
             }
         }
         catch (Exception ex1) {
-            System.out.println("ERROR in ENTER SInE.loadNonKif(" + kbFile + ")");
-            System.out.println("  approx. line # == " + lr.getLineNumber());
+            System.err.println("Error in ENTER SInE.loadNonKif(" + kbFile + ")");
+            System.out.println("  approx. line # == " + lnr.getLineNumber());
             System.out.println("  line == " + line);
             ex1.printStackTrace();
         }
@@ -205,37 +206,33 @@ public class SInE extends InferenceEngine {
 
     /** *************************************************************
      * Loads formulas from given source.
-     * 
+     *
      * @param formulaSource Iterable object that contains strings representing formulas.
      */
     public void loadFormulas(Iterable<String> formulaSource) {
 
-        Iterator it = formulaSource.iterator();
-    
-        // First we go through all formulas, and compute degree of each symbol.
-        while (it.hasNext()) {
-            String f = (String) it.next();            
-            formulas.add(f);            
+        for (String f : formulaSource) {
+            formulas.add(f);
             for (String sym : getSymbols(f)) {
                 Integer prev = degrees.get(sym);
-                if (prev != null) 
+                if (prev != null)
                     degrees.put(sym, prev+1);
-                else 
-                    degrees.put(sym, 1);                
+                else
+                    degrees.put(sym, 1);
             }
         }
         //System.out.println("SInE.loadFormulas(): stored " + formulas.size() + " formulas ");
-        
+
         // Now we associate each formula with its lowest-degree symbols.
         for (String form : formulas) {
             Set<String> symbols = getSymbols(form);
-            if (symbols.size() == 0) {
+            if (symbols.isEmpty()) {
                 mandatoryFormulas.add(form);
                 continue;
             }
-                        
+
             int minDeg = 5000000;
-            ArrayList<String> minDegSyms = new ArrayList<String>();
+            List<String> minDegSyms = new ArrayList<>();
             for (String sym : symbols) {
                 int deg = degrees.get(sym);
                 if (deg < minDeg) {
@@ -251,7 +248,7 @@ public class SInE extends InferenceEngine {
             for (String sym : minDegSyms) {
                 List<String> reqForms = requirements.get(sym);
                 if (reqForms == null) {
-                    reqForms = new ArrayList<String>();
+                    reqForms = new ArrayList<>();
                     reqForms.add(form);
                     requirements.put(sym, reqForms);
                 }
@@ -262,10 +259,10 @@ public class SInE extends InferenceEngine {
         }
         //System.out.println("SInE.loadFormulas(): computed " + requirements.size() + " formulas ");
     }
-      
+
     /** *************************************************************
      * Returns all symbols occurring in given formula.
-     * 
+     *
      * @param form Formula to get symbols from.
      * @return Symbols occurring in given formula.
      */
@@ -276,12 +273,12 @@ public class SInE extends InferenceEngine {
         //System.out.println("SInE.getSymbols(): form: " + form);
         Set<String> res = formSymbols.get(form);
         if (res == null) {
-            res = new HashSet<String>();            
-            String quotesCollapsedForm = quotedPattern.matcher(form).replaceAll("\"\"");            
+            res = new HashSet<>();
+            String quotesCollapsedForm = quotedPattern.matcher(form).replaceAll("\"\"");
             Matcher symMatch = symbolPattern.matcher(quotesCollapsedForm);
-            while (symMatch.find()) 
-                res.add(symMatch.group());           
-            res.removeAll(nonSymbols);            
+            while (symMatch.find())
+                res.add(symMatch.group());
+            res.removeAll(nonSymbols);
             formSymbols.put(form, res);
         }
         //System.out.println("SInE.getSymbols(): symbols: " + res);
@@ -293,58 +290,58 @@ public class SInE extends InferenceEngine {
     public Set<String> getSymbols(Collection<String> forms) {
 
         //System.out.println("SInE.getSymbols(): forms " + forms.size());
-        Set<String> syms = new HashSet<String>();
-        for (String form : forms) 
+        Set<String> syms = new HashSet<>();
+        for (String form : forms)
             syms.addAll(getSymbols(form));
         //System.out.println("SInE.getSymbols(): symbols " + syms.size());
-        return syms; 
+        return syms;
     }
-    
+
     /** *************************************************************
      * Returns formulas that are directly required by given symbols
-     * (in the sense of requirements map).   
-     * 
+     * (in the sense of requirements map).
+     *
      * @param symbols Symbols whose required formulas will be found.
      * @return Formulas required by symbols.
      */
     public Set<String> get1RequiredFormulas(Collection<String> symbols) {
 
         //System.out.println("SInE.get1RequiredFormulas(): symbols: " + symbols);
-        Set<String> reqForms = new HashSet<String>();
+        Set<String> reqForms = new HashSet<>();
         for (String sym : symbols) {
             List<String> symReqForms = requirements.get(sym);
             if (symReqForms == null)
                 continue;
-            for (String form : symReqForms) 
-                reqForms.add(form);            
+            for (String form : symReqForms)
+                reqForms.add(form);
         }
         return reqForms;
     }
-    
+
     /** *************************************************************
      * Returns all symbols transitively required by given symbols
      * (in the sense of requirements map).
-     * 
+     *
      * @param symbols Collection of symbols to be closed under requirements relation.
      * @return Closure of given collection of symbols under requirements relation.
      */
     public Set<String> getRequiredSymbols(Collection<String> symbols) {
 
-        Set<String> reqSyms = new HashSet<String>(symbols);            
-        int prevSize = 0;
+        Set<String> reqSyms = new HashSet<>(symbols);
+        int prevSize;
         do {
-            prevSize = reqSyms.size();                    
-            Set<String> reqForms = get1RequiredFormulas(reqSyms);                    
+            prevSize = reqSyms.size();
+            Set<String> reqForms = get1RequiredFormulas(reqSyms);
             reqSyms.addAll(getSymbols(reqForms));
         } while (reqSyms.size() > prevSize);
         System.out.println("SInE.getRequiredSymbols(): " + reqSyms.size());
         return reqSyms;
     }
-    
+
     /** *************************************************************
      * Returns formulas that are transitively required by given symbols
      * (in the sense of requirements map).
-     * 
+     *
      * @param symbols Symbols whose required formulas will be found.
      * @return Formulas transitively required by symbols.
      */
@@ -356,7 +353,7 @@ public class SInE extends InferenceEngine {
 
     /** *************************************************************
      * Performs axiom selection for given query.
-     * 
+     *
      * @param form Formula, according to which axioms will be selected.
      * @return Selected formulas.
      */
@@ -365,40 +362,38 @@ public class SInE extends InferenceEngine {
         Set<String> symbols = getSymbols(form);
         //System.out.println("performSelection(): form: " + form);
         //System.out.println("performSelection(): symbols: " + symbols);
-        symbols.addAll(getSymbols(mandatoryFormulas));            
-        Set<String> res = getRequiredFormulas(symbols);            
+        symbols.addAll(getSymbols(mandatoryFormulas));
+        Set<String> res = getRequiredFormulas(symbols);
         res.addAll(mandatoryFormulas);
-        
+
         return res;
     }
-    
+
     /** *************************************************************
      */
     @Override
     public String submitQuery(String formula, int timeLimit, int bindingsLimit)
                     throws IOException {
-        
+
         long t1 = System.currentTimeMillis();
-        Set<String> selectedFormulas = performSelection(formula);        
+        Set<String> selectedFormulas = performSelection(formula);
         long t_elapsed = (System.currentTimeMillis() - t1);
 
         System.out.println("INFO in SInE.submitQuery(): "
                            + (t_elapsed / 1000.0)
                            + " seconds to perform axiom selection");
         System.out.println("INFO in SInE.submitQuery(): "
-                + selectedFormulas.size() + " formula(s) selected out of " + formulas.size()); 
-
+                + selectedFormulas.size() + " formula(s) selected out of " + formulas.size());
         // print all selected formulas for debugging
-        Iterator it = selectedFormulas.iterator();
-        while (it.hasNext()) {
-            String f = (String) it.next();
+
+        for (String f : selectedFormulas) {
             System.out.println(f);
         }
 
-        InferenceEngine eng = underlyingEngineFactory.createWithFormulas(selectedFormulas);           
-        String res = eng.submitQuery(formula, timeLimit, bindingsLimit);            
+        InferenceEngine eng = underlyingEngineFactory.createWithFormulas(selectedFormulas);
+        String res = eng.submitQuery(formula, timeLimit, bindingsLimit);
         eng.terminate();
-        
+
         return res;
     }
 
@@ -410,7 +405,7 @@ public class SInE extends InferenceEngine {
         formulas.add(formula);
         //Formulas asserted through this method will always be used.
         mandatoryFormulas.add(formula);
-        
+
         return null;
     }
 
